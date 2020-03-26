3988c133ed79a225584e6920c41aa4da987608bd
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-03-26 16:38:42 +0000

    updated

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1162,7 +1162,7 @@ namespace RapidTrackingSingleThread
             if (url.ToLower().Contains("%2f") || url.ToLower().Contains("%2e"))
                 url = GetRedirectedUrl(WebUtility.UrlDecode(WebUtility.HtmlDecode(url)).Trim());
 
-            return WebUtility.HtmlEncode(url.Replace("\x00", "%00")).Replace("\\\\u003d", "=").Replace('\u0002', ' ').Replace('\u0018', ' ').Trim();  //26-03-2020
+            return WebUtility.HtmlEncode(url.Replace("\x00", "%00")).Replace("\\\\u003d", "=").Replace('\u0002', ' ').Replace('\u0018', ' ').Trim();  //26-03-2020 updated converting hexadecimal codes
         }
 
     }

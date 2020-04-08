8d5da6cce763d7b87e20c13f84b83c7fc2bca436
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-04-08 19:20:17 +0100

    update

--- RapidTrackingReceiving/iOS.cs
+++ RapidTrackingReceiving/iOS.cs
@@ -465,7 +465,9 @@ namespace Oxylabs_BulkKeywords
                 if (col == null)
                     col = crNode.SelectNodes(".//div[@id='tadsb']/ol/li"); // 21-02-2020 included selector for the text ads block
                 if (col == null)
-                    col = doc.DocumentNode.SelectNodes("//div[@id='tads']/ol/li");
+                    col = doc.DocumentNode.SelectNodes("//div[@id='tads']/div/ol/li"); //08-04-2020
+                if (col == null)
+                    col = doc.DocumentNode.SelectNodes("//div[@class='WcsE3 dJMePd']/div");  // 01-04-2020
                 if (col != null)
                 {
                     s.Append("<block type=\"adwords\" url=\"\">");

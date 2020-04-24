6fe1ad3891ba5bf86c541b9bfb4ae237af846e85
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-04-24 23:41:56 +0100

    updated selectors for carousel

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -1752,6 +1752,12 @@ namespace RapidTrackingSingleThread
                 nd = node.SelectSingleNode(".//div[@class='Y37F6d Nn2Stf']");  // 21-04-2020
             if (nd != null)
             {
+                // 24-04-2020
+                nd = node.SelectSingleNode(".//h2");
+                if (nd != null)
+                    if (nd.InnerText.ToLower().Trim() == "recipes" || nd.InnerText.ToLower().Trim() == "ricette")
+                        return "Carousel";
+                // 24-04-2020
                 nd = node.SelectSingleNode(".//div[@class='kp-blk nGydZ Wnoohf OJXvsb']");  // 20-03-2020
                 if (nd == null)
                     return "KnowledgePanel";

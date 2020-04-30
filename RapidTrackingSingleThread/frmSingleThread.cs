6e8ed9579f919667cf035ef1d01f3cbb7e667a39
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-04-30 15:22:20 +0100

    updated

--- RapidTrackingSingleThread/frmSingleThread.cs
+++ RapidTrackingSingleThread/frmSingleThread.cs
@@ -290,7 +290,7 @@ namespace RapidTrackingSingleThread
                 //lstKWs.Items.Add("106:romeo and juliet tickets");
                 //lstKWs.Items.Add("160:malmö ff");
                 //lstKWs.Items.Add("102:terry crews");
-                 lstKWs.Items.Add("58:55d bus schedule");
+                 lstKWs.Items.Add("106:55d bus schedule");
             });
             return;
 

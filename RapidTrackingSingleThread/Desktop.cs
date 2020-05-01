aa2ea6877e04a98cf32cdd05f1327e21841f8a31
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-05-01 10:56:33 +0100

    update 3

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -39,7 +39,7 @@ namespace RapidTrackingSingleThread
             if (nodeCol == null)
                 nodeCol = doc.DocumentNode.SelectNodes("//div[@id='ires']/ol/div");
             if (nodeCol == null)
-                nodeCol = doc.DocumentNode.SelectNodes("//div[@id='rso']/div"); //13-03-2020            
+                nodeCol = doc.DocumentNode.SelectNodes("//div[@id='rso']/div|//div[@id='rso']/g-section-with-header");//13-03-2020  //01-05-2020         
 
             foreach (HtmlNode node in nodeCol)
             {

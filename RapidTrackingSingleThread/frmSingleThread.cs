dbe498cd8c518211e80fac7712b60c06516862fa
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-05-18 20:27:48 +0100

    update

--- RapidTrackingSingleThread/frmSingleThread.cs
+++ RapidTrackingSingleThread/frmSingleThread.cs
@@ -94,8 +94,8 @@ namespace RapidTrackingSingleThread
                             string html = obj["results"][0]["content"].Value<string>();
                             string jobid = src[2];
                             string device = src[3];
-                            File.WriteAllText(@"D:\source\"+jobid+"_"+keyword+".html", html, Encoding.UTF8);
-                            //File.WriteAllText(@"C:\inetpub\wwwroot\"+jobid+"_withOut filter_"+".html", html, Encoding.UTF8);
+                            File.WriteAllText(@"C:\inetpub\wwwroot\html\" + jobid + "_" + keyword + ".html", html, Encoding.UTF8);
+                            //File.WiteAllText(@"C:\inetpub\wwwroot\"+jobid+"_withOut filter_"+".html", html, Encoding.UTF8);
                             result = true;
                             doc = new HtmlAgilityPack.HtmlDocument();
                             doc.LoadHtml(html);
@@ -177,24 +177,24 @@ namespace RapidTrackingSingleThread
       
         private void SendToAPI(string seid, string kw, string res, string jobid)
        {
-            if (res == string.Empty)
-            {
-                XmlDocument xd = new XmlDocument();
-                res = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-                res += "<searchResult searchEngine =\"" + seid + "\" keyword=\"" + kw + "\" date =\"" + DateTime.Today.ToString("yyyy-MM-dd") + "\">";
-                res += "<section col = \"main\" /> <section col=\"right\" /> </searchResult> ";
-                xd.LoadXml(res);
-                xd.Save(xmlPath);
-            }
-            else
-            {
+            //if (res == string.Empty)
+            //{
+            //    XmlDocument xd = new XmlDocument();
+            //    res = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+            //    res += "<searchResult searchEngine =\"" + seid + "\" keyword=\"" + kw + "\" date =\"" + DateTime.Today.ToString("yyyy-MM-dd") + "\">";
+            //    res += "<section col = \"main\" /> <section col=\"right\" /> </searchResult> ";
+            //    xd.LoadXml(res);
+            //    xd.Save(xmlPath);
+            //}
+            //else
+            //{
                 XmlDocument xd = new XmlDocument();
                 res = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + res;
                 xd.LoadXml(res);
                 xd.Save(xmlPath);
                // File.WriteAllText(@"D:\source\" + seid + "_" + jobid + "_" + kw + ".xml", res);
 
-            }
+            //}
             //SendToURL
 
 
@@ -291,9 +291,9 @@ namespace RapidTrackingSingleThread
                 //lstKWs.Items.Add("106:romeo and juliet tickets");
                 //lstKWs.Items.Add("160:malmö ff");
                 //lstKWs.Items.Add("102:terry crews");
-                 //lstKWs.Items.Add("1:cherries");
+                lstKWs.Items.Add("106:fat freddys drop tour");
             });
-            //return;
+            return;
 
             try
             {

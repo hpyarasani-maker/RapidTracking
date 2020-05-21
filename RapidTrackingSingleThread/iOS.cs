4ef56c43c693068670e41228cad364264fc7dd78
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-05-21 18:05:02 +0100

    update app comments

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -635,7 +635,7 @@ namespace RapidTrackingSingleThread
                             continue;
                         }
                         //21-02-2020  included selector for the Apps Block
-                        if (nd.SelectSingleNode(".//div[@class='ki5rnd']|.//div[@class='yR4jwc']") != null) //20-05-2020
+                        if (nd.SelectSingleNode(".//div[@class='ki5rnd']|.//div[@class='yR4jwc']") != null) //20-05-2020 included selector for app block
                         {
                             s.Append(GetApps(nd));
                             continue;

b4e8522919a8518d5f1d8ff75ac6e8ee816fbad0
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-05-23 18:40:39 +0100

    update

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -127,7 +127,7 @@ namespace RapidTrackingSingleThread
                             HtmlNodeCollection nc = node.SelectNodes(".//div[@id='kp-wp-tab-overview']/div"); //22-01-2020
                             if (nc == null)
                                 nc = node.SelectNodes(".//div[@class='WvKfwe']/div|.//div[@class='WvKfwe a3spGf']/div|.//div[@class='a3spGf WvKfwe']/div");  //15-04-2020      //23-05-2020
-                            if (nc == null)
+                            if (nc == null) //|.//div[@class='a3spGf WvKfwe']/div //23-05-2020
                                 nc = node.SelectNodes(".//div[@class='Kot7x eXEBMb Znsfnf']/div[@class='GhpATe pttBJc']"); //15-04-2020
                             if (nc == null)
                                 nc = node.SelectNodes(".//div[@class='UDZeY']/div|.//div[@class='vC5Ym']/div|.//div[@class='kp-blk cUnQKe Wnoohf OJXvsb']|.//div[@class='kp-blk c2xzTb OJXvsb']"); // 23-12-2019       //23-05-2020

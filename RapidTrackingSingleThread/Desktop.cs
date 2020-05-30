1c5a54a60695335a9c46fac20e5e232de273c2ae
UKUser <UKUser@HEMACHANDER>
2020-05-30 12:10:20 +0100

    udpated

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1172,7 +1172,8 @@ namespace RapidTrackingSingleThread
         private bool IsOrganic(HtmlNode node)
         {
             return (node.SelectSingleNode(".//h3[@class='r']") != null || node.SelectSingleNode(".//div[@class='r']") != null
-                || node.SelectSingleNode(".//div[@class='zTpPx']") != null || node.SelectSingleNode(".//div[@class='zTpPx']/g-link/a") != null);    // 28-05-2020   // 13-03-2020
+                || node.SelectSingleNode(".//div[@class='zTpPx']") != null || node.SelectSingleNode(".//div[@class='zTpPx']/g-link/a") != null    // 28-05-2020   // 13-03-2020
+                || node.SelectSingleNode(".//h3[@class='r dO0Ag']") != null);   //29-05-2020
         }
         //Surendra comments
         //07-11-2019

9925842fc2281e5f46bf4b6d0c73d53c6194ae77
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-06-01 17:00:35 +0100

    updated knowledge panel

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -1314,6 +1314,8 @@ namespace RapidTrackingSingleThread
                 nd = node.SelectSingleNode(".//div[@class='Uyhxfe ZdjxGf']");  // 30-03-2020
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[@class='Y37F6d Nn2Stf']");  // 21-04-2020
+            if (nd == null)
+                nd = node.SelectSingleNode(".//div[@class='HnYYW FIdh1']");  // 01-06-2020
             if (nd != null)
             {
                 string hdr = "";

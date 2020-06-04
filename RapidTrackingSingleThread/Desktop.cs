bcad240f7cff0d1b74e7d1d7af92d760a7c00597
UKUser <UKUser@HEMACHANDER>
2020-06-04 13:14:00 +0100

    update

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -907,7 +907,8 @@ namespace RapidTrackingSingleThread
                     if (n == null)
                         n = nd.SelectSingleNode(".//div[@class='nDgy9d']");     // changes on 02-07-2019
                     if (n == null)
-                        n = nd.SelectSingleNode(".//div[@class='mCBkyc jBgGLd']|.//div[@class='mCBkyc nDgy9d']");   //01-05-2020
+                        n = nd.SelectSingleNode(".//div[@class='mCBkyc jBgGLd']|.//div[@class='mCBkyc nDgy9d']|.//div[@class='mCBkyc oz3cqf vH5Lmd nDgy9d']|.//div[@class='mCBkyc oz3cqf vH5Lmd jBgGLd']"); //04-06-2020   //01-05-2020
+
                     if (n != null)
                         title = n.InnerText;
                     else

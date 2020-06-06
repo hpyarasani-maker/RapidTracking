d667fa35d54a2e132cdf506a273e0fc0f9d9b8ee
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-06-06 22:10:58 +0100

    Revert "updated"
    This reverts commit 21d48d53cb7a0f85cb6535163f7235e93d18ea19.

--- RapidTrackingSingleThread/frmSingleThread.cs
+++ RapidTrackingSingleThread/frmSingleThread.cs
@@ -292,7 +292,7 @@ namespace RapidTrackingSingleThread
                 //lstKWs.Items.Add("160:malmö ff");
                 //lstKWs.Items.Add("102:terry crews");
                 //lstKWs.Items.Add("102:the uninhabitable earth summary");
-                lstKWs.Items.Add("102:chicken korma");
+                lstKWs.Items.Add("106:donald trump");
             });
             return;
 

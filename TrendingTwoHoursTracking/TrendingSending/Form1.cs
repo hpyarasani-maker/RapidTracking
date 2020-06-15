b8b11ecc94da59a6a48b20f0bdc4eda43d89738d
UKUser <UKUser@HEMACHANDER>
2020-06-15 14:07:37 +0100

    updated

--- TrendingTwoHoursTracking/TrendingSending/Form1.cs
+++ TrendingTwoHoursTracking/TrendingSending/Form1.cs
@@ -109,8 +109,8 @@ namespace TrendingSending
             //return;
 
             Cursor.Current = Cursors.WaitCursor;
-            //string myDate = date_picker.Text;
-            string myDate = "2019-10-26";
+            string myDate = date_picker.Text;
+            //string myDate = "2019-10-26";
 
             //string strQry = "exec [dbo].[GetBulkTrendingDesktop_1] '" + myDate + "'";
             string strQry = "exec [dbo].[GetBulkTrendingMobile_1] '" + myDate + "'"; 
@@ -205,7 +205,7 @@ namespace TrendingSending
                     progress_lbl.Refresh();
                     lblIP.Text = ts.TotalSeconds.ToString();
                 });
-                Thread.Sleep(60000);
+                Thread.Sleep(70000);
                 //Thread.Sleep(30000);
             }
         }

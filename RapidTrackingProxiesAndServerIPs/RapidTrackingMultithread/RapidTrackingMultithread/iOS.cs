2a11a3ad3592d867a5f1cab868eb295c9f4fb41c
UKUser <UKUser@HEMACHANDER>
2020-06-17 16:35:58 +0100

    updated

--- RapidTrackingProxiesAndServerIPs/RapidTrackingMultithread/RapidTrackingMultithread/iOS.cs
+++ RapidTrackingProxiesAndServerIPs/RapidTrackingMultithread/RapidTrackingMultithread/iOS.cs
@@ -473,7 +473,7 @@ namespace RapidTrackingMultithread
                 if (col == null)
                     col = crNode.SelectNodes(".//div[@id='tadsb']/ol/li"); // 21-02-2020 included selector for the text ads block
                 if (col == null)
-                    col = doc.DocumentNode.SelectNodes("//div[@id='tads']/div/ol/li"); //08-04-2020
+                    col = doc.DocumentNode.SelectNodes("//div[@id='tads']/div/ol/li|//div[@jsname='hWE2jd']"); //17-06-2020 //08-04-2020
                 //if (col == null)
                 //    col = doc.DocumentNode.SelectNodes("//div[@class='WcsE3 dJMePd']/div");  // 01-04-2020 commented on 16-06-2020
                 if (col == null)

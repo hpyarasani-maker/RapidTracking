495e0de388d35a99141ffbe0949ac8bec6a3d939
UKUser <UKUser@HEMACHANDER>
2020-06-23 16:57:58 +0100

    updated

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -1340,6 +1340,8 @@ namespace RapidTrackingSingleThread
                 nd = node.SelectSingleNode(".//div[@class='Y37F6d Nn2Stf']");  // 21-04-2020
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[@class='HnYYW FIdh1']");  // 01-06-2020
+            if (nd == null)
+                nd = node.SelectSingleNode(".//div[@class='MQv7ze']");  // 23-06-2020
             if (nd != null)
             {
                 string hdr = "";

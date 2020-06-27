1b784314695200cfd082dec7c0b62e17f3b62adb
UKUser <UKUser@HEMACHANDER>
2020-06-27 16:03:32 +0100

    updated

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1218,7 +1218,7 @@ namespace RapidTrackingSingleThread
         {
             return (node.SelectSingleNode(".//h3[@class='r']") != null || node.SelectSingleNode(".//div[@class='r']") != null
                 || node.SelectSingleNode(".//div[@class='zTpPx']") != null || node.SelectSingleNode(".//div[@class='zTpPx']/g-link/a") != null    // 28-05-2020   // 13-03-2020
-                || node.SelectSingleNode(".//h3[@class='r dO0Ag']") != null);   //29-05-2020
+                || node.SelectSingleNode(".//h3[@class='r dO0Ag']") != null || node.SelectSingleNode(".//div[@class='DOqJne']") != null); //27-06-2020    //29-05-2020
         }
         //Surendra comments
         //07-11-2019

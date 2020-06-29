450e474fccf574b2dd82711301d45e7bd0745d65
UKUser <UKUser@HEMACHANDER>
2020-06-29 16:37:27 +0100

    updated

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -281,7 +281,7 @@ namespace RapidTrackingSingleThread
             StringBuilder s = new StringBuilder();
 
             // for carousel
-            HtmlNode crNode = doc.DocumentNode.SelectSingleNode("//div[@id='extabar']");//|//div[@class='W3btD']"); //29-06-2020
+            HtmlNode crNode = doc.DocumentNode.SelectSingleNode("//div[@id='extabar']|//div[@id='appbar']");  //29-06-2020
             if (crNode != null)
             {
                 if (crNode.SelectSingleNode(".//div[@id='kx']") != null  || crNode.SelectSingleNode(".//g-scrolling-carousel") != null) //|| crNode.SelectSingleNode(".//div[@role='heading']") != null) //29-06-2020

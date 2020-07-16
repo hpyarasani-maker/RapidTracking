73e7a4dc57fe947a183aeb87252d96b5023be5bd
UKUser <UKUser@HEMACHANDER>
2020-07-16 13:44:13 +0100

    updated selector for recipes

--- RapidTrackingJobIDResults/iOS.cs
+++ RapidTrackingJobIDResults/iOS.cs
@@ -1892,7 +1892,7 @@ namespace RapidTrackingJobIDResults
                 // 24-04-2020
                 nd = node.SelectSingleNode(".//h2");
                 if (nd != null)
-                    if (nd.InnerText.ToLower().Trim() == "recipes" || nd.InnerText.ToLower().Trim() == "ricette" || nd.InnerText.ToLower().Trim() == "recetas" || nd.InnerText.ToLower().Trim() == "recept" || nd.InnerText.ToLower().Trim() == "resep")//15-07-2020 //17-06-2020 //22-05-2020 receipes language included
+                    if (nd.InnerText.ToLower().Trim() == "recipes" || nd.InnerText.ToLower().Trim() == "ricette" || nd.InnerText.ToLower().Trim() == "recetas" || nd.InnerText.ToLower().Trim() == "recept" || nd.InnerText.ToLower().Trim() == "resep" || nd.InnerText.ToLower().Trim() == "receitas") //16-07-2020//15-07-2020 //17-06-2020 //22-05-2020 receipes language included
                         return "Carousel";
                 // 24-04-2020
 

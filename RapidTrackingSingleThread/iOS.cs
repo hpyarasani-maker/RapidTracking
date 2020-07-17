7ef04baad307cad16487f2bb3c9cd79c628389d1
UKUser <UKUser@HEMACHANDER>
2020-07-17 15:29:03 +0100

    updated selector for images block

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -1492,7 +1492,7 @@ namespace RapidTrackingSingleThread
             if (nds == null)
                 nds = node.SelectNodes(".//div[@class='eR2XS']/g-inner-card/div/a/g-img[@class='SeXxHf']/img"); //08-06-2020
             if (nds == null)
-                nds = node.SelectNodes(".//div[@class='OixsOd']/a");  //13-07-2020 selector included for images
+                nds = node.SelectNodes(".//div[@class='OixsOd']/a|.//div[@class='eA0Zlc qN5nNb ivg-i CxtVFd']/g-img[@class='BA0A6c']/img");  //17-07-2020  //13-07-2020 selector included for images
             if (nds != null)
 
                 foreach (HtmlNode nd in nds)

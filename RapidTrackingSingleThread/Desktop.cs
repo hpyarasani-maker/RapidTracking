40e64d7cd983158c2ca8d629beae00b45612887f
UKUser <UKUser@HEMACHANDER>
2020-08-05 16:46:45 +0100

    updated

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1114,9 +1114,8 @@ namespace RapidTrackingSingleThread
             }
             // end of map changes.
 
-            nd = node.SelectSingleNode(".//div[@id='kx']|.//div[@id='rrg']");
-            if (nd == null)
-                nd = node.SelectSingleNode(".//div[@class='s9lrV aZVgnb']/a/span[@class='U0nHk mfMhoc']");  //05-08-2020 included selector for carousel
+            nd = node.SelectSingleNode(".//div[@id='kx']|.//div[@id='rrg']|.//div[@class='oHJrJb']");//05-08-2020
+           
             if (nd != null)
             {
                 return "Carousel";

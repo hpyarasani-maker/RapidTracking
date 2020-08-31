78e01ba34b8720cea796f58502cd1d85f03fb4ee
UKUser <UKUser@HEMACHANDER>
2020-08-31 18:41:11 +0100

    update

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -1856,9 +1856,17 @@ namespace RapidTrackingSingleThread
                     foreach (HtmlNode nd in App2)
                     {
                         HtmlNode App3 = nd.ChildNodes[0];
+                        //31-08-2020
+                        HtmlNode titleNode = nd.SelectSingleNode(".//div[@class='dlErff']");
+                        string title;
+                        if (titleNode != null)
+                            title = titleNode.InnerText.Trim();
+                        else
+                            title = nd.InnerText.TrimStart();
 
-                        s.Append("<item url=\"" + SetUrl(App3.Attributes["href"].Value) + "\" title=\"" + SetTitle(nd.InnerText.TrimStart()) + "\" />");
-                        //end of 06-08-2019
+                        s.Append("<item url=\"" + SetUrl(App3.Attributes["href"].Value) + "\" title=\"" + SetTitle(title) + "\" />");
+                        //end 31-08-2020
+                       
                     }
                 }
                 s.Append("</block>");

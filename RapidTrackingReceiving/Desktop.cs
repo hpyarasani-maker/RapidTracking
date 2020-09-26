4f802dd1bb7abf717c5c29aa23e20b899c8028da
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-09-26 11:52:25 +0100

    update

--- RapidTrackingReceiving/Desktop.cs
+++ RapidTrackingReceiving/Desktop.cs
@@ -1438,28 +1438,7 @@ namespace Oxylabs_BulkKeywords
 
             return string.Empty;
         }
-        //23-09-2020 included method to find and fix hexdecimal chars
-        public string SanitizeXmlString(string xml)
-        {
-
-            if (xml == null)
-            {
-                throw new ArgumentNullException("xml");
-            }
-
-            StringBuilder buffer = new StringBuilder(xml.Length);
-
-            foreach (char c in xml)
-            {
-                if (XmlSanitizingStream.IsLegalXmlChar(c))
-                {
-                    buffer.Append(c);
-                }
-            }
-
-            return buffer.ToString();
-        }
-        //end 23-09-2020
+       
     }
 }
 
--- RapidTracking_Lessthan20_Multithreading/iOS.cs
+++ RapidTracking_Lessthan20_Multithreading/iOS.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Globalization;
+using System.IO;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -9,7 +10,7 @@ using System.Web;
 
 namespace TrackingTrending
 {
-    class iOS 
+    class iOS
     {
         int orgLinks;
         string html;
@@ -18,7 +19,7 @@ namespace TrackingTrending
         {
             count = 0;
 
-            if (doc == null)  return string.Empty;           
+            if (doc == null) return string.Empty;
 
             orgLinks = 0;
             html = doc.DocumentNode.OuterHtml;
@@ -41,7 +42,7 @@ namespace TrackingTrending
             //if (nodeCol == null)
             //    nodeCol = doc.DocumentNode.SelectNodes("//div[@id='ires']/ol/div");
 
-            if (nodeCol == null) return string.Empty; 
+            if (nodeCol == null) return string.Empty;
 
             string ndText = "";
 
@@ -152,7 +153,7 @@ namespace TrackingTrending
                             break;
                         }
                     }
-                    catch(WebException ex) { return ex.Message.ToString(); }
+                    catch (WebException ex) { return ex.Message.ToString(); }
                 }
             }
 
@@ -171,7 +172,7 @@ namespace TrackingTrending
                 count = orgLinks;
                 return sb.ToString();
             }
-            
+
             return string.Empty;
 
         }
@@ -2519,7 +2520,7 @@ namespace TrackingTrending
             if (url.Contains("%")) //18-09-2020
                 url = GetRedirectedUrl(WebUtility.UrlDecode(WebUtility.HtmlDecode(url)).Trim());
 
-            return WebUtility.HtmlEncode(url.Replace("\x00", "%00")).Replace("\\\\u003d", "=").Replace('\u0002', ' ').Replace('\u0018', ' ').Replace('\f', ' ').Trim(); //23-09-2020 applied method to URL//22-04-2020
+            return WebUtility.HtmlEncode(SanitizeXmlString(url).Replace("\x00", "%00")).Replace("\\\\u003d", "=").Replace('\u0002', ' ').Replace('\u0018', ' ').Replace('\f', ' ').Trim(); //23-09-2020 applied method to URL//22-04-2020
         }
         //27-08-2020
         private string GetRedirectedUrl_TextAds(string url)
@@ -2566,7 +2567,7 @@ namespace TrackingTrending
                 if (url.Contains("%")) //18-09-2020
                     url = GetRedirectedUrl_TextAds(WebUtility.UrlDecode(WebUtility.HtmlDecode(url)).Trim());
 
-                return WebUtility.HtmlEncode(url.Replace("\x00", "%00")).Replace("\\\\u003d", "=").Replace('\u0002', ' ').Replace('\u0018', ' ').Replace('\f', ' ').Trim();//23-09-2020 applied method to URL
+                return WebUtility.HtmlEncode(SanitizeXmlString(url).Replace("\x00", "%00")).Replace("\\\\u003d", "=").Replace('\u0002', ' ').Replace('\u0018', ' ').Replace('\f', ' ').Trim();//23-09-2020 applied method to URL
             }
 
             return string.Empty;
@@ -2594,5 +2595,79 @@ namespace TrackingTrending
         }
         //end 23-09-2020
     }
-}
 
+
+    public class XmlSanitizingStream : StreamReader
+    {
+        public XmlSanitizingStream(Stream streamToSanitize)
+        : base(streamToSanitize, true)
+        { }
+
+        /// <summary>
+        /// Whether a given character is allowed by XML 1.0.
+        /// </summary>
+        public static bool IsLegalXmlChar(int character)
+        {
+            return
+            (
+                 character == 0x9 /* == '\t' == 9   */          ||
+                 character == 0xA /* == '\n' == 10  */          ||
+                 character == 0xD /* == '\r' == 13  */          ||
+                (character >= 0x20 && character <= 0xD7FF) ||
+                (character >= 0xE000 && character <= 0xFFFD) ||
+                (character >= 0x10000 && character <= 0x10FFFF)
+            );
+        }
+        private const int EOF = -1;
+
+        public override int Read()
+        {
+            // Read each char, skipping ones XML has prohibited
+
+            int nextCharacter;
+
+            do
+            {
+                // Read a character
+
+                if ((nextCharacter = base.Read()) == EOF)
+                {
+                    // If the char denotes end of file, stop
+                    break;
+                }
+            }
+
+            // Skip char if it's illegal, and try the next
+
+            while (!XmlSanitizingStream.
+                    IsLegalXmlChar(nextCharacter));
+
+            return nextCharacter;
+        }
+
+        public override int Peek()
+        {
+            // Return next legal XML char w/o reading it 
+
+            int nextCharacter;
+
+            do
+            {
+                // See what the next character is 
+                nextCharacter = base.Peek();
+            }
+            while
+            (
+                // If it's illegal, skip over 
+                // and try the next.
+
+                !XmlSanitizingStream.IsLegalXmlChar(nextCharacter) &&
+                (nextCharacter = base.Read()) != EOF
+            );
+
+            return nextCharacter;
+
+        }
+    }
+
+}

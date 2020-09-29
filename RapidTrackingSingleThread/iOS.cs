17b2948103859a3c3e868da540ccef790366cc46
UKUser <UKUser@HEMACHANDER>
2020-09-29 17:01:39 +0100

    updated

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2207,7 +2207,7 @@ namespace RapidTrackingSingleThread
                 || node.SelectSingleNode(".//div[@class='vkc_np kkww4d']") != null    // changed on 05-07-2019
                 || node.SelectSingleNode(".//div[@class='UDZeY fAgajc']") != null     // 13-03-2020
                 || node.SelectSingleNode(".//div[@class='wQu7gc']") != null   //08-07-2020 mising answered card
-                || node.SelectSingleNode(".//div[@class='kp-blk OJXvsb']") != null)  //26-08-2020 included selector for answered 
+                || node.SelectSingleNode(".//div[@class='kp-blk OJXvsb']") != null  //26-08-2020 included selector for answered 
                 || node.SelectSingleNode(".//w-answer|.//a[@class='duf3 wrSo4']") != null) //28-09-2020
 
                 return "AnswerCard";

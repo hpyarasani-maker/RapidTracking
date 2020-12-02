07485c4917a6467693bd01ca3f91971af5a29f74
UKUser <UKUser@HEMACHANDER>
2020-12-02 13:00:47 +0000

    updated search params for seid = 474 in receiving project

--- RapidTrackingReceiving/SearchParams.cs
+++ RapidTrackingReceiving/SearchParams.cs
@@ -1623,6 +1623,10 @@ namespace Oxylabs_BulkKeywords
             {
                 seid =473, domain ="lt", geo_location="Lithuania", locale= "LT-LT", uule = "w+CAIQICIJTGl0aHVhbmlh", device="desktop"
             },
+            new SearchProperties() //included seid on 02-12-2020
+            {
+                seid =474, domain ="lt", geo_location="Lithuania", locale= "LT-LT", uule = "w+CAIQICIJTGl0aHVhbmlh", device="mobile_android"
+            },
             new SearchProperties()
             {
                 seid =475, domain ="com", geo_location="Florida,United States", locale="en-us", uule="w+CAIQICIVRmxvcmlkYSxVbml0ZWQgU3RhdGVz", device="desktop"

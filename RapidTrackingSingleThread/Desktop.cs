b0c382052d4ef7b23cbd8b7754936a633bbd8477
UKUser <UKUser@HEMACHANDER>
2020-12-07 15:54:59 +0000

    updated

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1433,7 +1433,7 @@ namespace RapidTrackingSingleThread
                         && node.SelectSingleNode(".//div[@class='Brgz0 tw-res']|.//div[@class='kp-blk cUnQKe Wnoohf OJXvsb']") == null)
                         bVal = false;
 
-                    if (node.SelectSingleNode(".//div[contains(@class,'kp-blk')]") != null) //0-12-2020 for answercard block
+                    if (node.SelectSingleNode(".//div[contains(@class,'kp-blk')]") != null) //07-12-2020 for answercard block
                         return true;
 
                     if (node.SelectSingleNode(".//div[@class='g']") != null) //04-12-2020 select for class links

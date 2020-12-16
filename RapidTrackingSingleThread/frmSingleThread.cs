5b1521cdd3fe9044f1effccb5ec9b28c0d0b3e6b
UKUser <UKUser@HEMACHANDER>
2020-12-16 17:28:55 +0000

    updated

--- RapidTrackingSingleThread/frmSingleThread.cs
+++ RapidTrackingSingleThread/frmSingleThread.cs
@@ -294,9 +294,9 @@ namespace RapidTrackingSingleThread
                 //lstKWs.Items.Add("160:malmö ff");
                 //lstKWs.Items.Add("102:terry crews");
                 //lstKWs.Items.Add("102:the uninhabitable earth summary");
-                //lstKWs.Items.Add("1:donald trump");
+                lstKWs.Items.Add("139:donald trump");
             });
-            //return;
+            return;
 
             try
             {

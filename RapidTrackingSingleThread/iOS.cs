8b0775571fcc7f5c1fa4841fe31044b5bee300bf
UKUser <UKUser@HEMACHANDER>
2020-12-18 18:45:32 +0000

    update

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -211,7 +211,12 @@ namespace RapidTrackingSingleThread
                     foreach (HtmlNode nd in col)
                     {
                         var url = nd.Attributes["href"].Value.Trim();
-                        url = GetRedirectedUrl(url);
+                        //18-12-2020
+                        var url1 = GetRedirectedUrl(url);
+                        if (string.IsNullOrEmpty(url1))
+                            url1 = GetProductListUrl(url);
+                        url = url1;
+                        //end 18-12-2020
                         s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(nd.InnerText) + "\" />");
                     }
                 }

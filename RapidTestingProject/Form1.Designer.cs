f0668db74f0d62a54a813d3bbf0c90041315bd0d
UKUser <UKUser@HEMACHANDER>
2021-01-12 16:52:35 +0000

    update new testing project

--- RapidTestingProject/Form1.Designer.cs
+++ RapidTestingProject/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtError = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lstKWs = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(327, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "#";
+            // 
+            // txtError
+            // 
+            this.txtError.Location = new System.Drawing.Point(330, 275);
+            this.txtError.Multiline = true;
+            this.txtError.Name = "txtError";
+            this.txtError.Size = new System.Drawing.Size(510, 210);
+            this.txtError.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(330, 49);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(510, 108);
+            this.textBox1.TabIndex = 5;
+            // 
+            // lstKWs
+            // 
+            this.lstKWs.FormattingEnabled = true;
+            this.lstKWs.Location = new System.Drawing.Point(5, 13);
+            this.lstKWs.Name = "lstKWs";
+            this.lstKWs.Size = new System.Drawing.Size(310, 472);
+            this.lstKWs.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(859, 509);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lstKWs);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtError;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox lstKWs;
     }
 }
 

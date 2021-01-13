71d21d1432f3496545438f08f6430e4aa86c996d
UKUser <UKUser@HEMACHANDER>
2021-01-13 15:19:21 +0000

    Created new Unit Test Project for testing purposes

---
+++ RapidTrackingUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RapidTestingProject;
+
+namespace RapidTrackingUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

89e19d333729cb0f068f0502e9b29d01e28a2952
UKUser <UKUser@HEMACHANDER>
2021-01-18 14:57:58 +0000

    updated

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2211,7 +2211,7 @@ namespace RapidTrackingSingleThread
                      || nd.InnerText.ToLower().Contains("notícias")) //08-01-2021 top stories
                  if ((node.SelectSingleNode(".//div[@id='tscffb']") != null || node.SelectSingleNode(".//div[@class='KJDcUb']") == null) && node.SelectSingleNode(".//div/a[@class='C8nzq BmP5tf']") == null) //04-01-2021 video block
                         return true;
-                if (nd.SelectSingleNode(".//div[@class='ttfMne']") != null) //18-01-2021 top stories
+                if (node.SelectSingleNode(".//div[@class='ttfMne']") != null) //18-01-2021 top stories
                     return true;
                 if (nd.InnerText == "More results" || nd.InnerText == "Top results" || nd.InnerText == "Toppresultater"
                         || nd.InnerText == "Fler resultat" || nd.InnerText == "Flere resultater" || nd.InnerText == "Plus de résultats")    // 13-12-2019

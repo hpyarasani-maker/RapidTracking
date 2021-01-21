ba986181e1f22d387fe400584ec886b30125868e
UKUser <UKUser@HEMACHANDER>
2021-01-21 16:06:36 +0000

    updated

--- RapidTrackingUnitTest/UnitTest1.cs
+++ RapidTrackingUnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RapidTrackingLibrary;
 
@@ -10,7 +11,19 @@ namespace RapidTrackingUnitTest
         [TestMethod]
         public void TestMethod1()
         {
-           
+            ArrayList list1 = new ArrayList();
+            ArrayList list2 = new ArrayList();
+
+            list1.Add("https://www.dixons.co.uk");
+            list1.Add("https://www.currys.co.uk");
+            list1.Sort();
+
+            
+            list2.Add("https://www.dixons.co.uk");
+            list2.Add("https://www.currys.co.uk");
+            list2.Sort();
+            CollectionAssert.AreEqual(list2,list1);
+
         }
     }
 }

bc098a31b04aa5951c7879251ea0f8469c8e2bf0
UKUser <UKUser@HEMACHANDER>
2021-01-22 17:28:03 +0000

    updated two test methods

--- RapidTrackingUnitTest/UnitTest.cs
+++ RapidTrackingUnitTest/UnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using System.Xml.Linq;
@@ -11,39 +12,65 @@ namespace RapidTrackingUnitTest
     [TestClass]
     public class UnitTest
     {
-        [TestMethod]
-        public void TestClassicLinksList()
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
         {
             Generate generate = new Generate();
-            generate.GenerateXml("106", "red dresses");
+            generate.GenerateXml("102", "joe biden");
+        }
 
+
+        List<ArrayList> GetProcessedLists()
+        {
             string xml1 = "C:\\inetpub\\wwwroot\\xml1.xml";
             string xml2 = "C:\\inetpub\\wwwroot\\xml2.xml";
 
-
-
+            List<ArrayList> al = new List<ArrayList>();
             ArrayList list1 = new ArrayList();
             ArrayList list2 = new ArrayList();
 
             XmlDocument xml = new XmlDocument();
-            xml.Load(xml1); 
-            xml.Load(xml2);
+            xml.Load(xml1);
             XmlNodeList xnList1 = xml.SelectNodes("/searchResult/section/item/@url");
             foreach (XmlNode xn1 in xnList1)
             {
                 list1.Add(xn1.InnerText);
             }
-            list1.Sort();
 
+            xml.Load(xml2);
             XmlNodeList xnList2 = xml.SelectNodes("/searchResult/section/item/@url");
             foreach (XmlNode xn2 in xnList2)
             {
                 list2.Add(xn2.InnerText);
             }
+
+            al.Add(list1);
+            al.Add(list2);
+
+            return al;
+        }
+
+
+        [TestMethod]
+        public void TestClassicLinksList()
+        {
+            List<ArrayList> lst = GetProcessedLists();
+            ArrayList list1 = lst[0];
+            ArrayList list2 = lst[1];
+
+            list1.Sort();
             list2.Sort();
 
-            CollectionAssert.AreEqual(list2,list1);
+            CollectionAssert.AreEqual(list2, list1);
+        }
 
+
+        [TestMethod]
+        public void TestClassicLinksCount()
+        {
+            List<ArrayList> lst = GetProcessedLists();
+
+            Assert.AreEqual(lst[0].Count, lst[1].Count);
         }
     }
 }

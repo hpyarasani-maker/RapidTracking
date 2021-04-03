6acb23b3f95731cbbf1b0df5b8987a0af67018c6
UKUser <UKUser@HEMACHANDER>
2021-04-03 15:55:05 +0100

    updated new search engines and video blcok in ios //02-04-2021 and 30-03-2021

--- RapidTrackingReceiving/iOS.cs
+++ RapidTrackingReceiving/iOS.cs
@@ -2001,7 +2001,7 @@ namespace Oxylabs_BulkKeywords
             if (nd != null && node.SelectSingleNode(".//div[contains(@class, ' knowledge-panel ')]") == null)
             {
                 // Changes in Videos block on 25-06-2019
-                if (node.InnerText.ToLower().Contains("videos") || node.InnerText.StartsWith("فيديوهات"))    // 29-11-2019
+                if (node.InnerText.ToLower().Contains("video") || node.InnerText.StartsWith("فيديوهات")) //02-04-2021 removed s from videos condition
                 {
                     return "Videos";
                 }
--- TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/SearchParams.cs
+++ TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/SearchParams.cs
@@ -1845,6 +1845,30 @@ new SearchProperties() //start 25-02-2021
             {
                 seid =529, domain ="co.nz", geo_location="Tauranga,Bay of Plenty,New Zealand", locale= "en-nz", uule = "w+CAIQICIiVGF1cmFuZ2EsQmF5IG9mIFBsZW50eSxOZXcgWmVhbGFuZA==",device="mobile_android"
             }, //end 25-02-2021
+            new SearchProperties()//30-03-2021
+            {
+                seid =530, domain ="com.gh", geo_location="Ghana", locale= "en-gh", uule = "w+CAIQICIFR2hhbmE=",device="desktop"
+            },
+            new SearchProperties()
+            {
+                seid =531, domain ="com.gh", geo_location="Ghana", locale= "en-gh", uule = "w+CAIQICIFR2hhbmE=",device="mobile_android"
+            },
+            new SearchProperties()
+            {
+                seid =532, domain ="co.tz", geo_location="Tanzania", locale= "en-tz", uule = "w+CAIQICIIVGFuemFuaWE=",device="desktop"
+            },
+            new SearchProperties()
+            {
+                seid =533, domain ="co.tz", geo_location="Tanzania", locale= "en-tz", uule = "w+CAIQICIIVGFuemFuaWE=",device="mobile_android"
+            },
+            new SearchProperties()
+            {
+                seid =534, domain ="co.tz", geo_location="Tanzania", locale= "sw-tz", uule = "w+CAIQICIIVGFuemFuaWE=",device="desktop"
+            },
+            new SearchProperties()
+            {
+                seid =535, domain ="co.tz", geo_location="Tanzania", locale= "sw-tz", uule = "w+CAIQICIIVGFuemFuaWE=",device="mobile_android"
+            }, //end 30-03-2021
         };
     }
 

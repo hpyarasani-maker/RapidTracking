a7898d2eb5593c5edc3484640a8fb703ce735035
UKUser <UKUser@HEMACHANDER>
2021-04-23 17:01:47 +0100

    update

--- RapidTrackingLibrary/Common.cs
+++ RapidTrackingLibrary/Common.cs
@@ -4,13 +4,13 @@ using System.Data.SqlClient;
 using System.Xml;
 
 namespace RapidTrackingLibrary
-{ 
-   public class Common
+{
+    public class Common
     {
-      
+
         public static string ReadConnection()
         {
-            try 
+            try
             {
                 XmlDocument xml = new XmlDocument();
                 string fileName = @"C:\Inetpub\wwwroot\Callback_TrackingTrending.xml";
@@ -48,7 +48,7 @@ namespace RapidTrackingLibrary
         //    return name;
         //}
 
-       public static int GetOxylabsTime()
+        public static int GetOxylabsTime()
         {
             int time = 0;
             string strQuery = "exec [dbo].[GetSleepTime]";
@@ -110,103 +110,6 @@ namespace RapidTrackingLibrary
                 }
             }
             return count;
-        }
-        private void SendToDBFailure(string seid, string kw, string jobid, bool isOldPage)
-        {
-            string myDate = DateTime.Today.ToString("yyyy-MM-dd");
-            //string myDate = "2019-10-10";
-
-            string qry = "insert into dashboard_dataerrors (date, name, seid, jobid) values(Convert(varchar(10),'" + myDate + "',103), N'" +
-                  kw.Replace("'", "''") + "', " + seid + ", '" + jobid + "' )";
-
-            string qryOld = "insert into dashboard_oldgooglepage (date, keyword, seid, jobid) values('" + DateTime.Now + "', N'" +
-                 kw.Replace("'", "''") + "', " + seid + ", '" + jobid + "' )";
-
-            using (SqlConnection con = new SqlConnection(Common.ReadConnection()))
-            {
-                try
-                {
-                    con.Open();
-                    using (SqlCommand comm = new SqlCommand(qry, con))
-                    {
-                        comm.CommandTimeout = 0;
-                        comm.ExecuteNonQuery();
-
-                        if (isOldPage)
-                        {
-                            comm.CommandText = qryOld;
-                            comm.ExecuteNonQuery();
-                        }
-                    }
-                }
-                catch (SqlException ex)
-                {
-                    string errorMessage = "Database Error: \r\n";
-                    for (int i = 0; i < ex.Errors.Count; i++)
-                    {
-                        errorMessage += "Index #" + i + "\n" +
-                                         "Message: " + ex.Errors[i].Message + "\n" +
-                                         "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
-                                         "Source: " + ex.Errors[i].Source + "\n" +
-                                         "Procedure: " + ex.Errors[i].Procedure + "\n" +
-                                         "Server: " + ex.Errors[i].Server + "\n";
-                    }
-
-                    //throw new Exception(errorMessage);
-                }
-                catch (Exception)
-                {
-                    //throw ex;
-                }
-            }
-        }
-        private void SendToDB(string seid, string keyword, string xml, string jobid, int urlcount)
-        {
-            try
-            {
-                string myDate = DateTime.Today.ToString("yyyy-MM-dd");
-                //string myDate = "2019-11-20";
-
-                using (SqlConnection con = new SqlConnection(Common.ReadConnection()))  // 12-05-2020
-                {
-                    con.Open();
-
-                    using (SqlCommand comm = con.CreateCommand())
-                    {
-                        comm.CommandTimeout = 0;
-                        comm.CommandType = CommandType.StoredProcedure;
-                        comm.CommandText = "Insert_dashboard_data";
-                        comm.Parameters.Add("Date", SqlDbType.DateTime).Value = myDate;
-                        comm.Parameters.Add("Name", SqlDbType.NVarChar).Value = keyword; //.Replace("'", "''");
-                        comm.Parameters.Add("Seid", SqlDbType.Int).Value = seid;
-                        comm.Parameters.Add("JobId", SqlDbType.NVarChar).Value = jobid;
-                        comm.Parameters.Add("Count", SqlDbType.Int).Value = urlcount;
-                        comm.Parameters.Add("XmlData", SqlDbType.Xml).Value = xml.Replace("'", "''");
-
-                        comm.ExecuteNonQuery();
-                    }
-                }
-
-            }
-            catch (SqlException ex)
-            {
-                string errorMessage = "Database Error: \r\n";
-                for (int i = 0; i < ex.Errors.Count; i++)
-                {
-                    errorMessage += "Index #" + i + "\n" +
-                                     "Message: " + ex.Errors[i].Message + "\n" +
-                                     "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
-                                     "Source: " + ex.Errors[i].Source + "\n" +
-                                     "Procedure: " + ex.Errors[i].Procedure + "\n" +
-                                     "Server: " + ex.Errors[i].Server + "\n";
-                }
-
-                throw new Exception(errorMessage);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message.ToString());
-            }
         }
     }
 }

baf45abe6cee897c88f44d1b8acf9bcacccb1155
UKUser <UKUser@HEMACHANDER>
2021-05-04 16:05:39 +0100

    created new CommonUnitTest File for db methods

---
+++ RapidTrackingUnitTest/CommonUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RapidTrackingUnitTest
+{
+    [TestClass]
+    public class CommonUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

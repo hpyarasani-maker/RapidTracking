ad32550540301f66498c39cb3f21fc7c34222e2d
UKUser <UKUser@HEMACHANDER>
2021-05-05 15:55:28 +0100

    update commonUnitTest.cs

--- RapidTrackingUnitTest/CommonUnitTest.cs
+++ RapidTrackingUnitTest/CommonUnitTest.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using RapidTrackingLibrary;
 namespace RapidTrackingUnitTest
 {
     [TestClass]
     public class CommonUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestReadConnection()
         {
+            string s = Common.ReadConnection();
+            Assert.AreEqual<string>("Data Source=82.136.42.2;User ID=sa;Password = brisbane007;initial catalog = TrackingTrending;", s);
+        }
+
+        [TestMethod]
+        public void TestGetOxylabsTime()
+        {
+            var s = Common.GetOxylabsTime();
+            Assert.AreEqual(450, s);
+        }
+        [TestMethod]
+        public void TestGetOxylabsCount()
+        {
+            var s = Common.GetOxylabsCount();
+            Assert.AreEqual(20, s);
         }
     }
 }
--- RapidTrackingUnitTest/iOSUnitTest.cs
+++ RapidTrackingUnitTest/iOSUnitTest.cs
@@ -139,25 +139,7 @@ namespace RapidTrackingUnitTest
             //ArrayList lst = GetRegExProcessedLists();
             //Assert.IsTrue (lst.Count >= 95);
         }
-        [TestMethod]
-        public void TestReadConnection()
-        {
-            string s = Common.ReadConnection();
-            Assert.AreEqual<string>("Data Source=82.136.42.2;User ID=sa;Password = brisbane007;initial catalog = TrackingTrending;", s);
-        }
-
-        [TestMethod]
-        public void TestGetOxylabsTime()
-        {
-            var s = Common.GetOxylabsTime();
-            Assert.AreEqual(450, s);
-        }
-        [TestMethod]
-        public void TestGetOxylabsCount()
-        {
-            var s = Common.GetOxylabsCount();
-            Assert.AreEqual(20, s);
-        }
+       
 
         
         [TestMethod]

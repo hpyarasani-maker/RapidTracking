727d30ac353fbea413559efc0ace548e02b3b8a8
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2021-05-28 15:14:50 +0100

    Revert "updated sitelinks selectors at line 1341"
    This reverts commit 350d04677158a91a654aca0425b60023f2f16614.

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1338,8 +1338,7 @@ namespace RapidTrackingSingleThread
                         return true;
 
                     if (node.SelectSingleNode(".//div[@class='g']") != null) //04-12-2020 select for class links
-                        if (node.SelectSingleNode(".//table[@class='nrgt']") != null || node.SelectSingleNode(".//table[@class='jmjoTe']") != null)//28-05-2021
-                            return false;
+                        return false;
                 }
                 catch { }
             }

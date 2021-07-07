0ac9ca39a0f6a9508b70a79ea2b8bb77a9193df4
UKUser <UKUser@HEMACHANDER>
2021-07-07 16:55:42 +0100

    updated image urls method

--- RapidTrackingProxiesAndServerIPs/RapidTrackingSingleThread/RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingProxiesAndServerIPs/RapidTrackingSingleThread/RapidTrackingSingleThread/iOS.cs
@@ -1554,8 +1554,8 @@ namespace RapidTrackingSingleThread
         private string GetImageURLs()
         {
             StringBuilder s = new StringBuilder();
-            //string matchPattern1 = @"]n,\[x22(.*?)x22"; //07-07-2021
-            string matchPattern1 = @"\\x22,\W\\x22(.*?)\\\\u0026s\\x22,";//07-07-2021
+            string matchPattern1 = @"]n,\[x22(.*?)x22";
+            string matchPattern6 = @"\\x22,\W\\x22(.*?)\\\\u0026s\\x22,";//07-07-2021
             string matchPattern2 = @"]\n,\[x22(.*?)\?";
             string matchPattern3 = "\"ou\":\"(.*?)\",";
             //string matchPattern4 = "\\W\\W\\Wx22http[s]*://(.*?)\\Wx22";   // 17-02-2020 included pattern
@@ -1636,7 +1636,18 @@ namespace RapidTrackingSingleThread
                 }
             }
             //end 24-06-2020
-
+            //07-07-2021
+            re = new Regex(matchPattern6, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            mc = re.Matches(html);
+            foreach (Match m in mc)
+            {
+                string HtmlText = HttpUtility.HtmlDecode(m.Groups[1].Value);
+                if (HtmlText.StartsWith("http") || HtmlText.StartsWith("https"))
+                {
+                    alDup.Add(HtmlText);
+                }
+            }
+            //end 07-07-2021
             foreach (string s1 in alDup)
             {
                 if (myList.Contains(s1) || string.IsNullOrEmpty(s1)) continue;
--- RapidTrackingReceiving/iOS.cs
+++ RapidTrackingReceiving/iOS.cs
@@ -1548,8 +1548,8 @@ namespace Oxylabs_BulkKeywords
         private string GetImageURLs()
         {
             StringBuilder s = new StringBuilder();
-            //string matchPattern1 = @"]n,\[x22(.*?)x22"; //07-07-2021
-            string matchPattern1 = @"\\x22,\W\\x22(.*?)\\\\u0026s\\x22,";//07-07-2021
+            string matchPattern1 = @"]n,\[x22(.*?)x22"; 
+            string matchPattern6 = @"\\x22,\W\\x22(.*?)\\\\u0026s\\x22,";//07-07-2021
             string matchPattern2 = @"]\n,\[x22(.*?)\?";
             string matchPattern3 = "\"ou\":\"(.*?)\",";
             //string matchPattern4 = "\\W\\W\\Wx22http[s]*://(.*?)\\Wx22";   // 17-02-2020 included pattern
@@ -1630,7 +1630,18 @@ namespace Oxylabs_BulkKeywords
                 }
             }
             //end 24-06-2020
-
+            //07-07-2021
+            re = new Regex(matchPattern6, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            mc = re.Matches(html);
+            foreach (Match m in mc)
+            {
+                string HtmlText = HttpUtility.HtmlDecode(m.Groups[1].Value);
+                if (HtmlText.StartsWith("http") || HtmlText.StartsWith("https"))
+                {
+                    alDup.Add(HtmlText);
+                }
+            }
+            //end 07-07-2021
             foreach (string s1 in alDup)
             {
                 if (myList.Contains(s1) || string.IsNullOrEmpty(s1)) continue;

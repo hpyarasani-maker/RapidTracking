8579fadbe8183ae3037718d08f8487f516f26be0
UKUser <UKUser@HEMACHANDER>
2021-07-08 15:44:33 +0100

    updated selectors for missing peoplealsoask block //08-07-2021

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -870,6 +870,8 @@ namespace RapidTrackingLibrary
             if (nds == null)
                 nds = node.SelectNodes(".//div[@jsname='xXq91c']");
             if (nds == null)
+                nds = node.SelectNodes(".//div[@jsname='jIA8B']"); //08-07-2021
+            if (nds == null)
                 return string.Empty;
             foreach (HtmlNode nd in nds)
             {
@@ -1221,7 +1223,7 @@ namespace RapidTrackingLibrary
             }
 
 
-            nd = node.SelectSingleNode(".//div[@class='kp-blk cUnQKe']|.//div[@class='kp-blk cUnQKe Wnoohf OJXvsb']");//04-12-2020 //11-02-2020
+            nd = node.SelectSingleNode(".//div[@class='kp-blk cUnQKe']|.//div[@class='kp-blk cUnQKe Wnoohf OJXvsb']|.//div[@jsname='N760b']");//08-07-2021//04-12-2020 //11-02-2020
             if (nd != null)
             {
                 return "PeopleAlsoAsk"; //11-02-2020
@@ -1365,7 +1367,8 @@ namespace RapidTrackingLibrary
                 || node.SelectSingleNode(".//h3[@class='GmE3X']") != null //16-10-2020 updated selector for videos
                 || node.SelectSingleNode(".//div[@class='twQ0Be']") != null //03-12-2020 updated selector for videocard
                 || node.SelectSingleNode(".//div[@class='vwfsqc']") != null //07-12-2020
-                || node.SelectSingleNode(".//div[@class='setTDc']") != null); //07-12-2020
+                || node.SelectSingleNode(".//div[@class='setTDc']") != null //07-12-2020
+                || node.SelectSingleNode(".//div[@class='Wt5Tfe']") != null); //08-07-2021
             if (bVal == true)//2019-09-11
             {
                 try

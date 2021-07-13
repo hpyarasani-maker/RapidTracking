daab4d2040fab047a386508b1eb4faf9e872c9a9
UKUser <UKUser@HEMACHANDER>
2021-07-13 17:18:40 +0100

    updated selector back again for TS item urls

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -1692,7 +1692,7 @@ namespace RapidTrackingSingleThread
 
             nds = node.SelectNodes(".//g-inner-card/a");
             if (nds == null)
-                nds = node.SelectNodes(".//g-inner-card/div/a|.//div[contains(@class,'amp_re')]/a"); //13-07-2021 TS item urls   // 30-10-2019
+                nds = node.SelectNodes(".//g-inner-card/div/a|.//div[@class='kno-fb-ctx n49mp']/div/a"); //13-07-2021 TS item urls   // 30-10-2019
             if (nds == null)
                 nds = node.SelectNodes(".//g-inner-card/div/div/a");    // 13-12-2019
             if (nds == null)

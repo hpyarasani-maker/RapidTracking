f14e16d1492fc7325d22284fef807dde34bd280d
UKUser <UKUser@HEMACHANDER>
2021-07-13 17:09:03 +0100

    updated ios files //13-07-2021 for jobs and topstories

--- TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/iOS.cs
+++ TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/iOS.cs
@@ -1687,7 +1687,7 @@ namespace TrendingReceiving
 
             nds = node.SelectNodes(".//g-inner-card/a");
             if (nds == null)
-                nds = node.SelectNodes(".//g-inner-card/div/a");    // 30-10-2019
+                nds = node.SelectNodes(".//g-inner-card/div/a|.//div[contains(@class,'amp_re')]/a"); //13-07-2021 TS item urls   // 30-10-2019
             if (nds == null)
                 nds = node.SelectNodes(".//g-inner-card/div/div/a");    // 13-12-2019
             if (nds == null)
@@ -2262,7 +2262,7 @@ namespace TrendingReceiving
                      || nd.InnerText.ToLower().Contains("notícias")) //08-01-2021 top stories
                     if ((node.SelectSingleNode(".//div[@id='tscffb']") != null || node.SelectSingleNode(".//div[@class='KJDcUb']") == null) && node.SelectSingleNode(".//div/a[contains(@class,'C8nzq BmP5tf')]") == null) //25-05-2021 //04-01-2021 video block
                         return true;
-                if (node.SelectSingleNode(".//div[@class='ttfMne']") != null) //19-01-2021 missing top stories
+                if (node.SelectSingleNode(".//div[@class='ttfMne']|.//div[@class='N60dNb mfMhoc']|.//h2[@class='OEsCyf mfMhoc']") != null)//12-07-2021 job block //12-07-2021 carousel block //19-01-2021 missing top stories
                     return true;
                 if (nd.InnerText == "More results" || nd.InnerText == "Top results" || nd.InnerText == "Toppresultater"
                      || nd.InnerText == "Fler resultat" || nd.InnerText == "Flere resultater" || nd.InnerText == "Plus de résultats")    // 13-12-2019

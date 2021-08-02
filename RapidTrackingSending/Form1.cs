aead176a320636b07ae9a68ffd1c72e880f7113f
UKUser <UKUser@HEMACHANDER>
2021-08-02 16:18:51 +0100

    updated sending keywords timing

--- RapidTrackingSending/Form1.cs
+++ RapidTrackingSending/Form1.cs
@@ -211,12 +211,12 @@ namespace Oxylabs_BulkKeywords
 
             for (int i = 0; i < worklist.Items.Count; i++)
             {
-                mseconds = rd.Next(20, 80) * 1000; //First Sending app 29-06-2020     //SEID=58
-                //mseconds = rd.Next(11, 14) * 1000; //Second Sending app 29-06-2020 //SEID=106
-                //mseconds = rd.Next(15, 19) * 1000; //Third Sending app 29-06-2020  //SEID=OtherDesktop
-                //mseconds = rd.Next(20, 23) * 1000; //Fourth Sending app 29-06-2020 //SEID=OtherMobile
-                //mseconds = rd.Next(24, 27) * 1000; //Fifith Sending app 29-06-2020 //SEID=1
-                //mseconds = rd.Next(28, 30) * 1000; //Sixth Sending app 29-06-2020  //SEID=102
+                mseconds = rd.Next(30, 80) * 1000; //First Sending app 29-06-2020    //SEID=58
+                //mseconds = rd.Next(21, 80) * 1000; //Second Sending app 29-06-2020 //SEID=106
+                //mseconds = rd.Next(25, 80) * 1000; //Third Sending app 29-06-2020 //SEID=OtherDesktop
+                //mseconds = rd.Next(30, 80) * 1000; //Fourth Sending app 29-06-2020 //SEID=OtherMobile
+                //mseconds = rd.Next(34, 80) * 1000; //Fifith Sending app 29-06-2020 //SEID=1
+                //mseconds = rd.Next(38, 80) * 1000; //Sixth Sending app 29-06-2020 //SEID=102
                 resultsString = worklist.Items[i].ToString();
                 sep = ':';
                 resultsArray = resultsString.Split(sep);

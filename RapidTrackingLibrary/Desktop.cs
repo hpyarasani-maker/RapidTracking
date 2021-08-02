2a5420341d8055d5f471dc8d8711182618486796
UKUser <UKUser@HEMACHANDER>
2021-08-02 16:07:52 +0100

    updated selector for missing eventResults block at at line 1223 //02-08-2021

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -1250,7 +1250,8 @@ namespace RapidTrackingLibrary
                 return "AnswerCard";
 
             if (node.SelectSingleNode(".//div[@id='imso-root']") != null || node.SelectSingleNode(".//div[@class='k9uN1c kfn9hb']") != null
-                || node.SelectSingleNode(".//div[@class='HaXvv kfn9hb']") != null || node.SelectSingleNode(".//div[@class='tsp-view']") != null)//24-11-2020 selector for eventresults block//07-02-2020
+                || node.SelectSingleNode(".//div[@class='HaXvv kfn9hb']") != null || node.SelectSingleNode(".//div[@class='tsp-view']") != null //24-11-2020 selector for eventresults block//07-02-2020
+                || node.SelectSingleNode(".//div[@class='tsp-fvcfc']") != null) //02-08-2021 event block selector
                 return "Event";
 
             if (node.SelectSingleNode(".//div[@id='cwmcwd']") != null || node.SelectSingleNode(".//div[@class='ifM9O']") != null

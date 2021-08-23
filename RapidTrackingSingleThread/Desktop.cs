c58bc27692ce9fdebb421641dc6fbb8ebedd29e3
UKUser <UKUser@HEMACHANDER>
2021-08-23 15:42:20 +0100

    updated selectors for map block //23-08-2021 at line 1269 and 1414

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1265,6 +1265,8 @@ namespace RapidTrackingSingleThread
             nd = node.SelectSingleNode(".//*[@id='lu_map']");
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[@class='xERobd']");  //changed on 26-06-2019
+            if (nd == null)
+                nd = node.SelectSingleNode(".//a[contains(@data-url,'/maps/')]");//23-08-2021 map selector
             //if (nd == null)
             //    nd = node.SelectSingleNode(".//div[@class='MHStgc']/span");//05-10-2020 commented //02-10-2020 maps selectors
             if (nd != null)
@@ -1409,7 +1411,7 @@ namespace RapidTrackingSingleThread
                         return true;
                 }
                 // changes on 08-07-2019
-                if (node.SelectSingleNode(".//img[@alt='map image']") != null || node.SelectSingleNode(".//div[@jsname='N760b']|.//div[@class='kno-mrg kno-swp']|.//div[@class='e4xoPb']") != null)//02-08-2021
+                if (node.SelectSingleNode(".//img[@alt='map image']") != null || node.SelectSingleNode(".//div[@jsname='N760b']|.//div[@class='kno-mrg kno-swp']|.//div[@class='e4xoPb']") != null || node.SelectSingleNode(".//img[contains(@data-bsrc,'/maps/')]") != null)//23-08-2021 map selector//02-08-2021
                     return true;
 
                 HtmlNodeCollection nds = node.SelectNodes(".//div");

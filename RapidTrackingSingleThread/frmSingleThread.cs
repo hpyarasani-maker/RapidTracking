40b0f877ebe262225b5422104048b56b19e57a76
UKUser <UKUser@HEMACHANDER>
2021-08-24 15:07:26 +0100

    Revert "updated selectors missing video blocks in desktop //23-08-2021 at line numbers 757, 774,1195 and 1276"
    This reverts commit eae1fe51d56f257c8444003893b89cfeec788a6d.

--- RapidTrackingSingleThread/frmSingleThread.cs
+++ RapidTrackingSingleThread/frmSingleThread.cs
@@ -298,7 +298,7 @@ namespace RapidTrackingSingleThread
                 //lstKWs.Items.Add("160:malmö ff");
                 //lstKWs.Items.Add("102:terry crews");
                 //lstKWs.Items.Add("102:the uninhabitable earth summary");
-                lstKWs.Items.Add("1:bulgogi recipe");
+                lstKWs.Items.Add("61:östersund kalmar");
             });
             return;
 
@@ -590,8 +590,8 @@ namespace RapidTrackingSingleThread
                     string[] reslt = { "", "", "", "" };
                     response = "";
 
-                    Uri uri = new Uri(cbUrl[1]);
-                    //Uri uri = new Uri("http://data.oxylabs.io/v1/queries/6835365044788671489/results");
+                    //Uri uri = new Uri(cbUrl[1]);
+                    Uri uri = new Uri("http://data.oxylabs.io/v1/queries/6835577320539966465/results");
                     if (cbUrl[2] == "done" && cbUrl[3] == "no")
                     {
                         try

2310307d5eeaf13e4b46b31a49970fa1cecf8087
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2021-09-09 12:05:57 +0100

    update line number 825 replaced with contains

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -864,7 +864,7 @@ namespace RapidTrackingLibrary
                              || nd.Attributes["class"].Value == "mnr-c luh4tb xpd O9g5cc uUPGi" || nd.Attributes["class"].Value == "mnr-c PHap3c" //05-01-2021
                              || nd.Attributes["class"].Value == "g card-section svwwZ" || nd.Attributes["class"].Value == "card-section"  //15-12-2020//03-11-2020 //06-10-2020 classic type block type
                              || nd.Attributes["class"].Value == "d5oMvf KJDcUb" || nd.Attributes["class"].Value == "c6gxKe card-section" //20-05-2021) //09-02-2021
-                             || nd.Attributes["class"].Value == "tKdlvb KJDcUb" //09-09-2021 missing classic links
+                             || nd.Attributes["class"].Value.Contains("KJDcUb") //09-09-2021 missing classic links //09-09-2021 missing classic links
                              || nd.Attributes["class"].Value == "wU9Tkd" //10-07-2021
                              || nd.Attributes["class"].Value == "g card-section") //30-08-2021 missing classic link
                             {

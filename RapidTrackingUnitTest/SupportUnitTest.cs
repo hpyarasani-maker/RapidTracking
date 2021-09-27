d83554d237f366982e9e81e6b86dd2e0ab453527
UKUser <UKUser@HEMACHANDER>
2021-09-27 18:28:06 +0100

    updated

--- RapidTrackingUnitTest/SupportUnitTest.cs
+++ RapidTrackingUnitTest/SupportUnitTest.cs
@@ -88,6 +88,34 @@ namespace RapidTrackingUnitTest
             htmlNode = SupportMethods.GetDesktopBlock("knowledgegraph");
             Assert.IsInstanceOfType(htmlNode, typeof(HtmlNode));
         }//end 24-09-2021
+        [TestMethod]//27-09-2021
+        public void TestGetDesktopVideosBlock()
+        {
+            HtmlNode htmlNode;
+            htmlNode = SupportMethods.GetDesktopBlock("videos");
+            Assert.IsInstanceOfType(htmlNode, typeof(HtmlNode));
+        }
+        [TestMethod]
+        public void TestGetDesktopImagesBlock()
+        {
+            HtmlNode htmlNode;
+            htmlNode = SupportMethods.GetDesktopBlock("images");
+            Assert.IsInstanceOfType(htmlNode, typeof(HtmlNode));
+        }
+        [TestMethod]
+        public void TestGetDesktopCarouselBlock()
+        {
+            HtmlNode htmlNode;
+            htmlNode = SupportMethods.GetDesktopBlock("carousel");
+            Assert.IsInstanceOfType(htmlNode, typeof(HtmlNode));
+        }
+        [TestMethod]
+        public void TestGetDesktopAnswerCardBlock()
+        {
+            HtmlNode htmlNode;
+            htmlNode = SupportMethods.GetDesktopBlock("answercard");
+            Assert.IsInstanceOfType(htmlNode, typeof(HtmlNode));
+        }//end 27-09-2021
     }
 }
 

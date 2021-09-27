0811c7fcae954e2ce38f911e52dfc6039fec9df0
UKUser <UKUser@HEMACHANDER>
2021-09-27 18:24:30 +0100

    updated

--- RapidTrackingUnitTest/CommonUnitTest.cs
+++ RapidTrackingUnitTest/CommonUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RapidTrackingLibrary;
 namespace RapidTrackingUnitTest
@@ -12,7 +13,21 @@ namespace RapidTrackingUnitTest
             string s = Common.ReadConnection();
             Assert.AreEqual<string>("Data Source=10.2.0.4;User ID=sa;Password = Pi*Soft1234;initial catalog = TrackingTrending;", s);
         }
-
+        [TestMethod] //27-09-2021
+        [ExpectedException(typeof(FileNotFoundException), "could not found file")]
+        public void TestReadConnectionFile()
+        {
+            string path = @"C:\Inetpub\wwwroot\Callback_TrackingTrending.xml";
+            var s = Common.ReadConnection();
+            if (File.Exists(path))
+            {
+                Assert.IsTrue(true, s);
+            }
+            else
+            {
+                Assert.ThrowsException<FileNotFoundException>(() => s);
+            }
+        }
         [TestMethod]
         public void TestGetOxylabsTime()
         {

2400fc9288d5a1601266cf2f50c31e08fe1c4f3f
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2021-11-12 15:58:10 +0000

    updated missing app block selectors line number 1888 and 673 in ios //12-11-2021

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -670,7 +670,7 @@ namespace RapidTrackingSingleThread
                         "|.//div/g-card[@class='XqIXXe']|.//g-card[@id='tscffb']|.//g-card[@class='g F6CFcc']|.//div[@class='khgTR lWEpfd']" +
                         "|.//div[@class='khgTR R5lVqb']|.//div[@class='mnr-c fp-w qs-ic aig-grd']|.//g-card[@class='URhAHe']" +
                         "|.//div[@class='mnr-c IcwJCe']|.//div[@class='g card-section svwwZ']" + //03-11-2020//26-08-2020 incuded contains functions to the selector//29-07-2020 //20-05-2020 missing classic link //05-06-2020
-                        "|.//div[contains(@class,'card-section')]|.//div[@class='Lgnr0e J88qA vgnU9e BmP5tf']|.//div[@class='wU9Tkd']|.//div[@jsname='wRSfy']|.//div[@class='tKdlvb jqWpsc']");//12-11-2021//11-10-2021 VB //10-07-2021 //20-05-2021 included contains//15-12-2020
+                        "|.//div[contains(@class,'card-section')]|.//div[@class='Lgnr0e J88qA vgnU9e BmP5tf']|.//div[@class='wU9Tkd']|.//div[@jsname='wRSfy']|.//div[@class='tKdlvb jqWpsc']|.//div[@class='g mnr-c']");//12-11-2021//12-11-2021//11-10-2021 VB //10-07-2021 //20-05-2021 included contains//15-12-2020
 
                 if (nds == null)
                     if (node.Attributes["class"]?.Value == "mnr-c xpd O9g5cc uUPGi") //09-09-2021 applied ? condition
@@ -1885,7 +1885,7 @@ namespace RapidTrackingSingleThread
                 {
                     s.Append("<block type=\"apps\" url=\"\">");
                     string innertext = string.Empty;
-                    HtmlNode TextNode = App.SelectSingleNode(".//div[@class='mdKzW']|.//div[@class='mdKzW c7UIMb']");  //22-06-2020
+                    HtmlNode TextNode = App.SelectSingleNode(".//div[contains(@class,'mdKzW')]");//12-11-2021 app block
                     if (TextNode != null)
                         innertext = TextNode.InnerText;
 

5465099c671c31a6e91c604a4936ee5c1be70eae
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2021-11-12 15:48:10 +0000

    Revert "Replaced recently updated selector in desktop at line number 373 and 227 //12-11-2021"
    This reverts commit cc8fdd647bad19edc91f5071809e662fb8b2e7aa.

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -224,7 +224,7 @@ namespace RapidTrackingSingleThread
                 s.Append("<block type=\"adwords\" url=\"\">");
                 foreach (HtmlNode nd in col)
                 {
-                    HtmlNode n = nd.SelectSingleNode(".//h3/a[2]|.//div[@class='ad_cclk']/a[2]|.//div[@class='d5oMvf']/a|.//div[contains(@class,'v5yQqb')]/a);//12-11-2021 //27-06-2020
+                    HtmlNode n = nd.SelectSingleNode(".//h3/a[2]|.//div[@class='ad_cclk']/a[2]|.//div[@class='d5oMvf']/a|.//div[@class='v5yQqb']/a");//12-11-2021 //27-06-2020
                     if (n != null)
                     {
                         HtmlNode tittlenode = n.SelectSingleNode(".//h3|.//div[@role='heading']");//27-06-2020
@@ -370,7 +370,7 @@ namespace RapidTrackingSingleThread
                     foreach (HtmlNode nd in col)
                     {
                         //HtmlNode n = nd.SelectSingleNode(".//h3/a[2]");
-                        HtmlNode n = nd.SelectSingleNode(".//div[@class='ad_cclk']/a[2]|.//div[contains(@class,'d5oMvf')]/a|.//div[contains(@class,'v5yQqb')]/a");//12-11-2021 //29-08-2020 included contains fucntions //23-07-2020 included missing item urls selectors
+                        HtmlNode n = nd.SelectSingleNode(".//div[@class='ad_cclk']/a[2]|.//div[contains(@class,'d5oMvf')]/a|.//div[@class='v5yQqb']/a");//12-11-2021 //29-08-2020 included contains fucntions //23-07-2020 included missing item urls selectors
                         if (n != null)
                         {
                              //25-08-2020

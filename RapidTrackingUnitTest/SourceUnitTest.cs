91b79657c94cc052062116bcb40c7906162848ce
UKUser <UKUser@HEMACHANDER>
2021-11-25 14:29:35 +0000

    updated //25-11-2021

--- RapidTrackingUnitTest/SourceUnitTest.cs
+++ RapidTrackingUnitTest/SourceUnitTest.cs
@@ -9,24 +9,24 @@ namespace RapidTrackingUnitTest
     [TestClass]
     public class SourceUnitTest
     {
-        /*[TestMethod]
+       /* [TestMethod]
         [PexGeneratedBy(typeof(SourceUnitTest))]
        
         public void TestGetHTMLException()
         {
             using (PexDisposableContext disposables = PexDisposableContext.Create())
             {
-                Task<ArrayList> task;
+                ArrayList task;
                 task = source.GetHTML((string)null, 0);
                 disposables.Add((IDisposable)task);
                 //disposables.Dispose();
                 Assert.IsNotNull((object)task);
-                Assert.AreEqual<TaskStatus>(TaskStatus.Faulted, ((Task)task).Status);
-                Assert.AreEqual<bool>(false, ((Task)task).IsCanceled);
-                Assert.IsNull(((Task)task).AsyncState);
+                Assert.AreEqual<TaskStatus>(TaskStatus.Faulted, (task[0);
+                Assert.AreEqual<bool>(false, task[0]);
+                Assert.IsNull(task[0], null);
                 Assert.AreEqual<bool>(true, ((Task)task).IsFaulted);
             }
-        }
+        }*/
         [TestMethod] //04-05-2021
         public void TestGetOxylabsWebDataSourcesException()
         {
@@ -42,7 +42,7 @@ namespace RapidTrackingUnitTest
                 Assert.IsNull(((Task)task).AsyncState);
                 Assert.AreEqual<bool>(true, ((Task)task).IsFaulted);
             }
-        }*/
+        }
         [TestMethod]
         public void TestGetOxylabsWebDataSources()
         {

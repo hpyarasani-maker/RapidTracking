e7ab2075ee3826e17b6f4d96573acf52a4f31384
UKUser <UKUser@HEMACHANDER>
2021-12-14 16:43:50 +0000

    updated selector in library ios file for missing people also ask block titles at line number 1536 //14-12-2021

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -1534,6 +1534,8 @@ namespace RapidTrackingLibrary
             if (nds == null)
                 nds = node.SelectNodes(".//div[@jsname='ARU61']"); // 14-12-2020
             if (nds == null)
+                nds = node.SelectNodes(".//div[@jsname='lN6iy']"); //14-12-2021 tiles for Peope also ask block
+            if (nds == null)
                 return string.Empty;
             foreach (HtmlNode nd in nds)
             {

48db9a20e135b11dd10eb17104e8b4b94c35073e
UKUser <UKUser@HEMACHANDER>
2021-12-29 17:02:48 +0000

    updated selector for missing top stories at line number 1414 //29-12-2021

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -1438,7 +1438,8 @@ namespace RapidTrackingLibrary
              || node.SelectSingleNode(".//div[@jscontroller='hFvNdd']") != null //08-10-2021 images
              || node.SelectSingleNode(".//div[@class='g jNVrwc Y4pkMc']") != null //07-12-2021
              || node.SelectSingleNode(".//div[@class='e2BEnf q8U8x']") != null //07-12-2021
-            || node.SelectSingleNode(".//div[@jsname='wRSfy']") != null); //07-12-2021
+            || node.SelectSingleNode(".//div[@jsname='wRSfy']") != null) //07-12-2021
+            || node.SelectSingleNode(".//div[@class='e2BEnf axf3qc q8U8x']") != null;//29-12-2021 top stories
             if (bVal == true)//2019-09-11
             {
                 try

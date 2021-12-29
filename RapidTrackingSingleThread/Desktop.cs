a5c2d0b80a2945d9fe2379af35530b8515e6645e
UKUser <UKUser@HEMACHANDER>
2021-12-29 15:21:11 +0000

    updated selectors for duplicates images blocks in desktop at line number 1304 //29-12-2021

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1301,7 +1301,8 @@ namespace RapidTrackingSingleThread
                 nd = node.SelectSingleNode(".//div[@jscontroller='hFvNdd']");//08-10-2021 images
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[@class='kno-fiu kno-liu']"); //07-12-2021 for images block
-            if (nd != null)
+            if (nd != null && node.Attributes["id"]?.Value != "Odp5De") //29-12-2021
+            //if (nd != null)
             {
                 return "Images";
             }

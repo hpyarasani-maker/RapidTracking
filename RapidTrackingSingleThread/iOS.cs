8b5419cfa9000239fcb48f5220f27e0ead3f6e9d
UKUser <UKUser@HEMACHANDER>
2022-01-05 14:52:26 +0000

    updated selector for missing carousel block in ios at line number 2057 //05-01-2022

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2055,6 +2055,8 @@ namespace RapidTrackingSingleThread
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[@jsname='GDPwke']"); // 18-10-2019
             if (nd == null)
+                nd = node.SelectSingleNode(".//div[@class='Y2NmGf']"); //05-01-2022
+            if (nd == null)
                 nd = node.SelectSingleNode(".//div[@class='MIyI4c']"); //19-08-2021 updated for carousel block 
             if (nd != null)
                 if (node.SelectSingleNode(".//img[contains(@alt,'Map of ')]") == null) //15-05-2020

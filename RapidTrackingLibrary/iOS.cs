ac4a79fa3691ba90597251066b3e0f8642969df1
UKUser <UKUser@HEMACHANDER>
2022-01-14 16:27:34 +0000

    updated

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -1398,10 +1398,9 @@ namespace RapidTrackingLibrary
             StringBuilder s = new StringBuilder();
             HtmlNode n = node.SelectSingleNode(".//h3[@class='r']/a");
             if (n == null)
-                // n = node.SelectSingleNode(".//a[@class='C8nzq JTuIPc']");
-                n = node.SelectSingleNode(".//a[contains(@class,'BmP5tf')]");//14-01-2022
+                n = node.SelectSingleNode(".//a[@class='C8nzq JTuIPc']");
             if (n == null)
-                n = node.SelectSingleNode(".//a[contains(@class,'C8nzq BmP5tf')]"); //14-09-2020 contains
+                n = node.SelectSingleNode(".//a[contains(@class,'BmP5tf')]"); //14-09-2020 contains
             if (n == null)
                 n = node.SelectSingleNode(".//a[contains(@class,'sXtWJb')]");//16-12-2020 //15-12-2020
             if (n != null)

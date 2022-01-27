4ab3f39493d74c8bc585010589988300764f31b7
UKUser <UKUser@HEMACHANDER>
2022-01-27 14:59:51 +0000

    updated selector for missing answer card in ios at line number 2397 //27-01-2022

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2394,7 +2394,7 @@ namespace RapidTrackingSingleThread
 
             // if (node.SelectSingleNode(".//div[@class='g card-section svwwZ']|.//div[@class='c6gxKe card-section']|.//div[@class='g card-section']") != null) //30-08-2021 //20-05-2021//03-11-2020
             if (node.SelectSingleNode(".//div[@class='g card-section svwwZ']|.//div[@class='c6gxKe card-section']|.//div[@class='g card-section']") != null
-                 && node.SelectSingleNode(".//div[@data-tts='answers']") == null) //07-09-2021
+                 && node.SelectSingleNode(".//div[@data-tts='answers']|.//div[@class='ifM9O']") == null)//27-01-2022 answered card //07-09-2021
                 return false;
 
             //22-11-2019

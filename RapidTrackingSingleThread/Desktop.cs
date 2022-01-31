f941c2b8a634bcbb631ef8a8640e6facfe305485
UKUser <UKUser@HEMACHANDER>
2022-01-31 16:19:36 +0000

    updated people also ask method to pick item urls in desktop through regex //31-01-2022

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -841,7 +841,7 @@ namespace RapidTrackingSingleThread
             return s.ToString();
         }
 
-        /*private string PeopleAlsoAsk(HtmlNode node) //commented 19-01-2022
+        private string PeopleAlsoAsk(HtmlNode node) //included item urls code 31-01-2022
         {
             StringBuilder s = new StringBuilder();
             HtmlNodeCollection nds = node.SelectNodes(".//div[@class='_eHi']/div"); // (".//h3[@class='r']/a");
@@ -857,13 +857,42 @@ namespace RapidTrackingSingleThread
                 nds = node.SelectNodes(".//div[@jsname='Cpkphb']"); //30-11-2021 people also ask titles
             if (nds == null)
                 return string.Empty;
+           string[] titles = new string[nds.Count];
+            int x = 0;
             foreach (HtmlNode nd in nds)
             {
-                s.Append("<item url=\"\" title=\"" + SetTitle(nd.InnerText) + "\" />");
+                //s.Append("<item url=\"\" title=\"" + SetTitle(nd.InnerText) + "\" />");
+                titles[x++] = nd.InnerText;
+            }
+            var res = GetPeopleAlsoAskUrls(titles);
+            if(string.IsNullOrEmpty(res))
+                foreach(var t in titles)
+                    s.Append("<item url=\"\" title=\"" + SetTitle(t) + "\" />");
+            s.Append(res);
+            return s.ToString();
+        }//end of item urls code 31-01-2022
+        private string GetPeopleAlsoAskUrls(string[] titles) //People also method 31-01-2022
+        {
+            StringBuilder s = new StringBuilder();
+            string pattern = @"WEB_ANSWERS_STANDARD_RESULT_(.*?)div class\\x3d\\x22yuRUbf\\x22\\x3e\\x3ca href\\x3d\\x22(.*?)\\x22 data-jsarwt";
+            Regex re = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            MatchCollection mc = re.Matches(html);
+            ArrayList myList = new ArrayList();
+            int x = 0;
+            foreach (Match m in mc)
+            {
+                string url = HttpUtility.HtmlDecode(m.Groups[2].Value);
+                if (url.StartsWith("http") || url.StartsWith("https"))
+                {
+                    int n = url.IndexOf("?");
+                    if (n > 0)
+                        url = url.Remove(n);
+                    s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(titles[x++]) + "\" />");
+                }
             }
             return s.ToString();
-        }*/ //end comments 19-01-2022
-        private string PeopleAlsoAsk(HtmlNode node) //new method to pick item urls in people also block 19-01-2022
+        }
+        /*private string PeopleAlsoAsk(HtmlNode node) //new method to pick item urls in people also block 19-01-2022
         {
             StringBuilder s = new StringBuilder();
             HtmlNodeCollection nds = node.SelectNodes(".//div[@jsname='F79BRe']");
@@ -894,8 +923,8 @@ namespace RapidTrackingSingleThread
                 s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" />");
             }
             return s.ToString();
-        }
-        
+        }*/
+
 
         private string GetAnswerCard(HtmlNode node)
         {

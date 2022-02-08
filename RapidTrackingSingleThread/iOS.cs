b5fda5c5680deaf48b081a3d72e2816b667ae039
UKUser <UKUser@HEMACHANDER>
2022-02-08 16:44:04 +0000

    update again in topstuff method for textads urls //08-02-2022

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -567,6 +567,10 @@ namespace RapidTrackingSingleThread
                                 {
                                     url = GetRedirectedUrl_TextAds(n.Attributes["data-pcu"].Value);
                                 }
+                                else if (!string.IsNullOrEmpty(GetRedirectedUrl_TextAds(n.SelectSingleNode(".//span[@class='yKd8Hd qzEoUe']")?.InnerText)))//08-02-2022
+                                {
+                                    url = GetRedirectedUrl_TextAds(n.SelectSingleNode(".//span[@class='yKd8Hd qzEoUe']")?.InnerText);
+                                }//08-02-2022
                                 else
                                 {
                                     HtmlNode n1 = nd.SelectSingleNode(".//div[@class='ads-visurl']/cite|.//div[@class='QNz0M ellip GsCRYb']/cite");

01c9e177904a96ec2005f84be6b9a96d5eed0930
UKUser <UKUser@HEMACHANDER>
2022-02-15 14:49:15 +0000

    updated selector for missing KP block and return as answer card block in ios at line number 2078 //16-02-2022

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -2084,7 +2084,7 @@ namespace RapidTrackingLibrary
             //if (nd == null)
             //    nd = node.SelectSingleNode(".//div[@class='kp-blk EyBRub knowledge-panel OJXvsb']");//05-10-2020 commented  //13-07-2020 images block type and KP block type
             if (nd == null)
-                nd = node.SelectSingleNode(".//div[contains(@class,'kp-blk EyBRub')]"); //05-10-2020 included selector for missing KP block
+                nd = node.SelectSingleNode(".//div[contains(@class,'kp-blk EyBRub')]|.//div[@class='kp-hc']"); //16-02-2022 //05-10-2020 included selector for missing KP block
             if (nd != null)
             {
                 // 24-04-2020

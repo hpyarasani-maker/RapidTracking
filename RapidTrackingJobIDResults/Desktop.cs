6e61088dd99e3b043f2b1bd824fdbed318860315
UKUser <UKUser@HEMACHANDER>
2022-02-16 14:36:35 +0000

    updated jobid single thread project //16-02-20222

--- RapidTrackingJobIDResults/Desktop.cs
+++ RapidTrackingJobIDResults/Desktop.cs
@@ -833,7 +833,29 @@ namespace RapidTrackingJobIDResults
             }
             return s.ToString();
         }
-        private string PeopleAlsoAsk(HtmlNode node) //commented 19-01-2022
+        /* private string PeopleAlsoAsk(HtmlNode node) //commented 19-01-2022
+         {
+             StringBuilder s = new StringBuilder();
+             HtmlNodeCollection nds = node.SelectNodes(".//div[@class='_eHi']/div"); // (".//h3[@class='r']/a");
+             if (nds == null)
+                 nds = node.SelectNodes(".//div[@class='psDd8d']/div");
+             if (nds == null)
+                 nds = node.SelectNodes(".//div[@class='DUeSlb']/div");
+             if (nds == null)
+                 nds = node.SelectNodes(".//div[@jsname='xXq91c']");
+             if (nds == null)
+                 nds = node.SelectNodes(".//div[@jsname='jIA8B']"); //08-07-2021
+             if (nds == null)
+                 nds = node.SelectNodes(".//div[@jsname='Cpkphb']"); //30-11-2021 people also ask titles
+             if (nds == null)
+                 return string.Empty;
+             foreach (HtmlNode nd in nds)
+             {
+                 s.Append("<item url=\"\" title=\"" + SetTitle(nd.InnerText) + "\" />");
+             }
+             return s.ToString();
+         }*/
+        private string PeopleAlsoAsk(HtmlNode node) //included item urls code 31-01-2022
         {
             StringBuilder s = new StringBuilder();
             HtmlNodeCollection nds = node.SelectNodes(".//div[@class='_eHi']/div"); // (".//h3[@class='r']/a");
@@ -849,29 +871,7 @@ namespace RapidTrackingJobIDResults
                 nds = node.SelectNodes(".//div[@jsname='Cpkphb']"); //30-11-2021 people also ask titles
             if (nds == null)
                 return string.Empty;
-            foreach (HtmlNode nd in nds)
-            {
-                s.Append("<item url=\"\" title=\"" + SetTitle(nd.InnerText) + "\" />");
-            }
-            return s.ToString();
-        }
-        /*private string PeopleAlsoAsk(HtmlNode node) //included item urls code 31-01-2022
-        {
-            StringBuilder s = new StringBuilder();
-            HtmlNodeCollection nds = node.SelectNodes(".//div[@class='_eHi']/div"); // (".//h3[@class='r']/a");
-            if (nds == null)
-                nds = node.SelectNodes(".//div[@class='psDd8d']/div");
-            if (nds == null)
-                nds = node.SelectNodes(".//div[@class='DUeSlb']/div");
-            if (nds == null)
-                nds = node.SelectNodes(".//div[@jsname='xXq91c']");
-            if (nds == null)
-                nds = node.SelectNodes(".//div[@jsname='jIA8B']"); //08-07-2021
-            if (nds == null)
-                nds = node.SelectNodes(".//div[@jsname='Cpkphb']"); //30-11-2021 people also ask titles
-            if (nds == null)
-                return string.Empty;
-           string[] titles = new string[nds.Count];//31-01-2022
+            string[] titles = new string[nds.Count];//31-01-2022
             int x = 0;
             foreach (HtmlNode nd in nds)
             {
@@ -879,8 +879,8 @@ namespace RapidTrackingJobIDResults
                 titles[x++] = nd.InnerText;
             }
             var res = GetPeopleAlsoAskUrls(titles);
-            if(string.IsNullOrEmpty(res))
-                foreach(var t in titles)
+            if (string.IsNullOrEmpty(res))
+                foreach (var t in titles)
                     s.Append("<item url=\"\" title=\"" + SetTitle(t) + "\" />");
             s.Append(res);//31-01-2022
             return s.ToString();
@@ -1466,7 +1466,7 @@ namespace RapidTrackingJobIDResults
                 try
                 {
                     //if (node.SelectSingleNode(".//div[@class='g']/div[@class='g jNVrwc Y4pkMc']") != null) return false; //07-12-2021
-                    if (node.SelectSingleNode(".//div[@class='g jNVrwc Y4pkMc']|.//div[@class='g eejeod up9jud']") != null) return false;//02-02-2022//31-12-2021 missing CLinks
+                    if (node.SelectSingleNode(".//div[@class='g jNVrwc Y4pkMc']|.//div[@class='g tF2Cxc']|.//div[@class='g eejeod up9jud']") != null) return false;//15-02-2022//02-02-2022//31-12-2021 missing CLinks
                     if (node.SelectSingleNode(".//div[@class='twQ0Be']|.//div[@jsname='N760b']|.//div[@jsname='wRSfy']|.//div[@class='e2BEnf U7izfe hWIMdd q8U8x']|.//div[@jsname='A6RGif']") != null) return true;//28-12-2021//10-12-2021//09-12-2021 //08-12-2021 PAlsoB   //30-08-2021 video card
                     if (node.SelectSingleNode(".//div[@class='osrp-blk']|.//div[@class='tpa-cc']") != null && node.SelectSingleNode(".//div[@class='l44Vof']") == null) //31-12-2021
                         return false; //20-08-2021
@@ -1488,7 +1488,7 @@ namespace RapidTrackingJobIDResults
                         && node.SelectSingleNode(".//div[@class='Brgz0 tw-res']|.//div[@class='kp-blk cUnQKe Wnoohf OJXvsb']") == null)
                         bVal = false;
 
-                    if (node.SelectSingleNode(".//div[contains(@class,'kp-blk')]") != null || node.Attributes["class"]?.Value == "kp-blk c2xzTb")//23-12-2021//07-12-2020 for answercard block 
+                    if (node.SelectSingleNode(".//div[contains(@class,'kp-blk')]|.//div[contains(@class,'c2xzTb')]") != null || node.Attributes["class"]?.Value == "kp-blk c2xzTb")//16-02-2022//23-12-2021//07-12-2020 for answercard block 
                         return true;//22-12-2021 commented
                     //if (node.SelectSingleNode(".//div[@class='kp-blk Wnoohf OJXvsb']") != null)//22-12-2021
                     //    return false;

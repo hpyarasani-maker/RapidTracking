5f7bc11e42d71a436e42440113baaced718b1dfc
UKUser <UKUser@HEMACHANDER>
2022-03-04 15:55:10 +0000

    update text in peoplealsoak method in ios //04-03-2022

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -1572,7 +1572,31 @@ namespace RapidTrackingSingleThread
              return s.ToString();
          }//31-01-2022*/
 
-        private string GetPeopleAlsoAskUrls(string[] titles) //People also method 31-01-2022
+        /* private string GetPeopleAlsoAskUrls(string[] titles) //People also method 31-01-2022
+         {
+             StringBuilder s = new StringBuilder();
+             string pattern = @"WEB_ANSWERS_STANDARD_RESULT_(.*?)div class\\x3d\\x22Xv4xee\\x22\\x3e\\x3ch3 class\\x3d\\x22yuRUbf JtG40d MBeuO q8U8x\\x22\\x3e\\x3ca class\\x3d\\x22sXtWJb\\x22 href\\x3d\\x22(.*?)\\x22";
+             Regex re = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+             MatchCollection mc = re.Matches(html);
+             ArrayList myList = new ArrayList();
+             int x = 0;
+             foreach (Match m in mc)
+             {
+                 string url = HttpUtility.HtmlDecode(m.Groups[2].Value);
+                 if (url.StartsWith("http") || url.StartsWith("https"))
+                 {
+                     int n = url.IndexOf("?");
+                     if (n > 0)
+                         url = url.Remove(n);
+                     if (x < titles.Length)//22-02-2022
+                         s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(titles[x++]) + "\" />");//22-02-2022
+                 }
+             }
+             for (; x < titles.Length; x++)//18-02-2022
+                 s.Append("<item url=\"\" title=\"" + SetTitle(titles[x]) + "\" />");//18-02-2022
+             return s.ToString();
+         }*/
+        private string GetPeopleAlsoAskUrls(string[] titles) //People also method 04-03-2022
         {
             StringBuilder s = new StringBuilder();
             string pattern = @"WEB_ANSWERS_STANDARD_RESULT_(.*?)div class\\x3d\\x22Xv4xee\\x22\\x3e\\x3ch3 class\\x3d\\x22yuRUbf JtG40d MBeuO q8U8x\\x22\\x3e\\x3ca class\\x3d\\x22sXtWJb\\x22 href\\x3d\\x22(.*?)\\x22";
@@ -1588,12 +1612,24 @@ namespace RapidTrackingSingleThread
                     int n = url.IndexOf("?");
                     if (n > 0)
                         url = url.Remove(n);
-                    if (x < titles.Length)//22-02-2022
-                        s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(titles[x++]) + "\" />");//22-02-2022
+                    //04-03-2022
+                    //    s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(titles[x++]) + "\" />");
+                    //}
                 }
+                string textPattern = @"\\x3cspan class\\x3d\\x22hgKElc\\x22\\x3e(.*?)\\x3c/span\\x3e";
+                Regex _rx = new Regex(textPattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                Match _m = _rx.Match(m.Groups[0].Value);
+                string text = string.Empty;
+                if (_m.Success)
+                {
+                    text = HttpUtility.HtmlDecode(_m.Groups[1].Value).Replace(@"\x3cb\x3e", "").Replace(@"\x3c/b\x3e", "");
+                }
+                if (x < titles.Length)
+                    s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(titles[x++]) + "\" text=\"" + text + "\" />");
+                //end 04-03-2022
             }
-            for (; x < titles.Length; x++)//18-02-2022
-                s.Append("<item url=\"\" title=\"" + SetTitle(titles[x]) + "\" />");//18-02-2022
+            for (; x < titles.Length; x++)
+                s.Append("<item url=\"\" title=\"" + SetTitle(titles[x]) + "\" text=\"\" />");
             return s.ToString();
         }
         private string GetAnswerCard(HtmlNode node)

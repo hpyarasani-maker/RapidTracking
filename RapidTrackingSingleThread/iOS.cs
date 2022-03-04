3d5d3af33de58bc35353413cd847ce2096bee7d8
UKUser <UKUser@HEMACHANDER>
2022-03-04 15:59:50 +0000

    updated selector for missing map block in ios at line number 2261,2262 //04-03-2022

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2258,6 +2258,8 @@ namespace RapidTrackingSingleThread
                 nd = node.SelectSingleNode(".//div[@class='utyL0c']");//01-05-2020
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[@class='IbDT9d q8U8x aTI8gc RES9jf']"); //17-02-2022
+            if (nd == null)
+                nd = node.SelectSingleNode(".//div[@class='IEBeid']"); //04-03-2022
             if (nd != null)
             {
                 return "Maps";

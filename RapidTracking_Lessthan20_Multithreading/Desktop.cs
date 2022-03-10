80e9a1c3c705cb13f71ab8f8ac55cf8fd93e7994
UKUser <UKUser@HEMACHANDER>
2022-03-10 17:14:59 +0000

    update again //10-03-2022 in both files

--- RapidTracking_Lessthan20_Multithreading/Desktop.cs
+++ RapidTracking_Lessthan20_Multithreading/Desktop.cs
@@ -876,8 +876,8 @@ namespace TrackingTrending
                     //int n = url.IndexOf("%3F");
                     //if (n > 0)
                     //    url = url.Remove(n);
-                    if (url.Contains(@"\x3d"))
-                        url = url.Replace(@"\x3d", "="); //10-02-2022
+                    if (url.Contains(@"\x3d") || url.Contains(@"\x26amp;vl=en"))
+                        url = url.Replace(@"\x3d", "=").Replace(@"\x26amp;vl=en", ""); //10-03-2022
                     if (x < titles.Length)//22-02-2022
                         s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(titles[x++]) + "\" />");//22-02-2022
                 }

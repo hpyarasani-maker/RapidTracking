c40e6dda211d419ead9d0d5ce28ca8418977ab57
UKUser <UKUser@HEMACHANDER>
2022-03-28 14:19:51 +0100

    update peoplealsoask

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -1568,7 +1568,7 @@ namespace RapidTrackingSingleThread
              return s.ToString();
          }//31-01-2022*/
 
-        /*private string GetPeopleAlsoAskUrls(string[] titles) //People also method 31-01-2022
+        private string GetPeopleAlsoAskUrls(string[] titles) //People also method 31-01-2022
         {
             StringBuilder s = new StringBuilder();
             string pattern = @"WEB_ANSWERS_STANDARD_RESULT_(.*?)div class\\x3d\\x22Xv4xee\\x22\\x3e\\x3ch3 class\\x3d\\x22yuRUbf JtG40d MBeuO q8U8x\\x22\\x3e\\x3ca class\\x3d\\x22sXtWJb\\x22 href\\x3d\\x22(.*?)\\x22";
@@ -1590,9 +1590,9 @@ namespace RapidTrackingSingleThread
             for (; x < titles.Length; x++)//18-02-2022
                 s.Append("<item url=\"\" title=\"" + SetTitle(titles[x]) + "\" />");//18-02-2022
             return s.ToString();
-        }*/
+        }
 
-        private string GetPeopleAlsoAskUrls(string[] titles) //People also method 04-03-2022
+        /*private string GetPeopleAlsoAskUrls(string[] titles) //People also method 04-03-2022
         {
             StringBuilder s = new StringBuilder();
             //22-03-2022
@@ -1712,7 +1712,7 @@ namespace RapidTrackingSingleThread
             for (; x < titles.Length; x++)
                 s.Append("<item url=\"\" title=\"" + SetTitle(titles[x]) + "\" text=\"\" image=\"\" />"); //17-03-2022
             return s.ToString();
-        }
+        }*/
         private string GetAnswerCard(HtmlNode node)
         {
             StringBuilder s = new StringBuilder();

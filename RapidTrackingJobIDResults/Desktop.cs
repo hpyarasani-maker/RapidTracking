e82c40686e1fca5fcf614e9d0863d99f04b8161a
UKUser <UKUser@HEMACHANDER>
2022-04-05 18:12:24 +0100

    updated desktop class file and selector at line number 485 //04-04-2022

--- RapidTrackingJobIDResults/Desktop.cs
+++ RapidTrackingJobIDResults/Desktop.cs
@@ -475,7 +475,7 @@ namespace RapidTrackingJobIDResults
                 || node.SelectNodes(".//div[contains(@class,'g card-section')]|.//div[@class='N3nEGc']") != null //03-03-2022 //06-10-2020 classic link
                 || node.SelectNodes(".//div[@class='g tF2Cxc']|.//div[@class='g dFd2Tb']") != null) //24-08-2021 video block //01-06-2021
             {
-                HtmlNodeCollection nds = node.SelectNodes(".//div[contains(@class,'tF2Cxc')]"); //25-01-2022
+                HtmlNodeCollection nds = node.SelectNodes(".//div[contains(@class,'g tF2Cxc')]"); //04-04-2022
                 if (nds == null) //25-01-2022
                     nds = node.SelectNodes(".//div[@class='g']|.//div[@class='HD8Pae luh4tb cUezCb xpd O9g5cc uUPGi']|.//div[contains(@class,'g card-section')]");//25-01-2022
                 if (nds == null)

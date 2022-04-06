5e772d52ec4d898be9939ea38ecb6d85b1e107bf
UKUser <UKUser@HEMACHANDER>
2022-04-06 15:28:45 +0100

    udpated date

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1511,7 +1511,7 @@ namespace RapidTrackingSingleThread
                 || node.SelectSingleNode(".//div[@class='setTDc']") != null //07-12-2020 answered card selector
                 || node.SelectSingleNode(".//div[@class='kp-blk ouUsKb G45kvd']") != null //24-08-2021 answer card selector
                 || node.SelectSingleNode(".//div[@class='M0XuFe mnr-c vk_c']") != null //23-12-2021
-                || node.SelectSingleNode(".//div[@id='wob_wc']") != null) //06-05-2022//31-03-2022 
+                || node.SelectSingleNode(".//div[@id='wob_wc']") != null) //06-04-2022//31-03-2022 
             {
                 if (node.SelectSingleNode(".//div[@class='BET1rd']|.//div[@class='EfDVh wDYxhc NFQFxe']") == null && node.SelectSingleNode(".//div[@class='tpa-cc']") == null //23-12-2021 //21-08-2021 //25-09-2020
                     && node.SelectSingleNode(".//div[contains(@class,'kno-fb-ctx')]") == null) //13-10-2021

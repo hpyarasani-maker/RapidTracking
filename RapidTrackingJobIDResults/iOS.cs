b8b2e3eaf9b3b281429fb65b4abf045ef4164423
UKUser <UKUser@HEMACHANDER>
2022-04-11 15:16:31 +0100

    updated ios class file singlethread at line number 2035 and 2036 //11-04-2022

--- RapidTrackingJobIDResults/iOS.cs
+++ RapidTrackingJobIDResults/iOS.cs
@@ -2027,9 +2027,9 @@ namespace RapidTrackingJobIDResults
                         else
                             url = nd.Attributes["data-url"].Value;
                         //end 26-06-2020
-                        if (url.Contains("/search?")) url = "";
-                        //if(!string.IsNullOrEmpty(url.Trim()) || !string.IsNullOrEmpty(title.Trim()))
-                        s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" />");
+                        if (url.Contains("/search?") || url.StartsWith("#")) url = "";//11-04-2022
+                        if (!string.IsNullOrEmpty(url.Trim()) || !string.IsNullOrEmpty(title.Trim())) //11-04-2022
+                            s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" />");
                     }
                     catch { }
                 }
--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2032,8 +2032,8 @@ namespace RapidTrackingSingleThread
                         else
                             url = nd.Attributes["data-url"].Value;
                         //end 26-06-2020
-                        if (url.Contains("/search?")) url = "";
-                        //if(!string.IsNullOrEmpty(url.Trim()) || !string.IsNullOrEmpty(title.Trim()))
+                        if (url.Contains("/search?") || url.StartsWith("#")) url = "";//11-04-2022
+                        if(!string.IsNullOrEmpty(url.Trim()) || !string.IsNullOrEmpty(title.Trim())) //11-04-2022
                         s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" />");
                     }
                     catch { }

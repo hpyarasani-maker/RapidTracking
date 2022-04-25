d7cd2e256f349fec8abd9ed4cfd9d253f2a4bcc8
UKUser <UKUser@HEMACHANDER>
2022-04-25 18:47:46 +0100

    updated

---
+++ RapidTrackingFindButton/Message.cs
@@ -0,0 +1,23 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidTrackingFindButton
+{
+    public class Message
+    {
+
+        public string GetSeeMoreText(string seid, string keyword, HtmlDocument doc)
+        {
+            var nodes = doc.DocumentNode.SelectNodes(".//div[@class='tF2Cxc']/div/a");
+            if (nodes == null)
+                nodes = doc.DocumentNode.SelectNodes(".//div[@class='g tF2Cxc']/div/a");
+
+            return nodes.ToString();
+        }
+    }
+}

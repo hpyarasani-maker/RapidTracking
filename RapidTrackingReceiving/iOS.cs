8d5fe5804c0c8c4ef6319d481ba7055b57bf1128
UKUser <UKUser@HEMACHANDER>
2022-05-14 15:37:13 +0100

    updated all ios class files in all projects //14-05-2022

--- RapidTrackingReceiving/iOS.cs
+++ RapidTrackingReceiving/iOS.cs
@@ -2396,7 +2396,7 @@ namespace Oxylabs_BulkKeywords
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[@class='IbDT9d q8U8x aTI8gc RES9jf']"); //17-02-2022
             if (nd == null)
-                nd = node.SelectSingleNode(".//div[@class='IEBeid']"); //04-03-2022
+                nd = node.SelectSingleNode(".//div[@class='IEBeid']|.//g-img[@class='o8ebK']");//10-05-2022 //04-03-2022
             if (nd != null)
             {
                 return "Maps";
@@ -2745,7 +2745,7 @@ namespace Oxylabs_BulkKeywords
                 if (nd != null && node.SelectSingleNode(".//div[@class='EDblX m8vZ3d']") == null || node.SelectSingleNode(".//div[@jscontroller='KP4k7d']") != null) //12-11-2021 CL  // 16-10-2019
                 {
                     //if (node.SelectSingleNode(".//div[contains(@class,'BNeawe')]") != null && node.SelectSingleNode(".//div[contains(@class,'au0C1b')]") == null)//02-03-2021 included contains for classic links
-                    if (node.SelectSingleNode(".//div[contains(@class,'BNeawe')]") != null && node.SelectSingleNode(".//div[contains(@class,'au0C1b')]") == null && node.SelectSingleNode(".//div[contains(@class,'PUrSVb')]") == null && node.SelectSingleNode(".//div[@class='Q9mvUc']") == null)//05-01-2022 carousel //25-10-2021
+                    if (node.SelectSingleNode(".//div[contains(@class,'BNeawe')]") != null && node.SelectSingleNode(".//div[contains(@class,'au0C1b')]") == null && node.SelectSingleNode(".//div[contains(@class,'PUrSVb')]") == null && node.SelectSingleNode(".//div[@class='Q9mvUc']") == null || node.SelectSingleNode(".//div[@class='pXvdUe']") != null) //11-05-2022//05-01-2022 carousel //25-10-2021
                         return true;
                     return false;
                 }

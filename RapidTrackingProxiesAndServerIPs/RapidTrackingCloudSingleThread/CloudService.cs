b087fb7844e383822e44d5bba71ff45992d4e117
UKUser <UKUser@HEMACHANDER>
2022-06-03 16:33:26 +0100

    updated

--- RapidTrackingProxiesAndServerIPs/RapidTrackingCloudSingleThread/CloudService.cs
+++ RapidTrackingProxiesAndServerIPs/RapidTrackingCloudSingleThread/CloudService.cs
@@ -33,7 +33,7 @@ namespace RapidTrackingCloudSingleThread
             cs = new SourceService.Service1Client();
             string HTML = cs.GetGoogleSource(seid.ToString(), keyword);
             File.WriteAllText(@"c:\inetpub\wwwroot\html\cartyres.html", HTML);
-            string[] dr = DesktoppatternTrending(System.Net.WebUtility.HtmlDecode(HTML), keyword, seid.ToString());
+            string[] dr = DesktoppatternTrending(HTML, keyword, seid.ToString());
             return dr;
         }
         //----------------------------------------------- For Non Hotel Keywords -------------------------------------//

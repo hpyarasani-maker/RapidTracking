3068bdd399a248660ab32fda5402321c67ad1676
UKUser <UKUser@HEMACHANDER>
2022-06-06 14:29:25 +0100

    updated selector  for missing answered card in desktop at line number 1521 //06-06-2022

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1518,7 +1518,8 @@ namespace RapidTrackingSingleThread
                 || node.SelectSingleNode(".//div[@class='setTDc']") != null //07-12-2020 answered card selector
                 || node.SelectSingleNode(".//div[@class='kp-blk ouUsKb G45kvd']") != null //24-08-2021 answer card selector
                 || node.SelectSingleNode(".//div[@class='M0XuFe mnr-c vk_c']") != null //23-12-2021
-                || node.SelectSingleNode(".//div[@id='wob_wc']") != null) //06-04-2022//31-03-2022 
+                || node.SelectSingleNode(".//div[@id='wob_wc']") != null //06-04-2022//31-03-2022 
+                || node.SelectSingleNode(".//div[@class='YXEKBb']") != null) //06-06-2022 Answered Card
             {
                 if (node.SelectSingleNode(".//div[@class='BET1rd']|.//div[@class='EfDVh wDYxhc NFQFxe']") == null && node.SelectSingleNode(".//div[@class='tpa-cc']") == null //23-12-2021 //21-08-2021 //25-09-2020
                     && node.SelectSingleNode(".//div[contains(@class,'kno-fb-ctx')]") == null) //13-10-2021

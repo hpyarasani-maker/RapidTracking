db1a12e62d60c561b96a56970c3971a3aa4711e8
UKUser <UKUser@HEMACHANDER>
2022-06-17 15:51:35 +0100

    updated selector for Answer Card at line number 1524 //17-06-2022

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1521,7 +1521,7 @@ namespace RapidTrackingSingleThread
                 || node.SelectSingleNode(".//div[@class='M0XuFe mnr-c vk_c']") != null //23-12-2021
                 || node.SelectSingleNode(".//div[@id='wob_wc']") != null //06-04-2022//31-03-2022 
                 || node.SelectSingleNode(".//div[@class='YXEKBb']") != null //06-06-2022 Answered Card
-                || node.SelectSingleNode(".//div[@class='ellip vk_h lxZILe']") != null) //16-06-2022
+                || node.SelectSingleNode(".//div[@class='ellip vk_h lxZILe']|.//div[contains(@class, 'vk_c')]") != null)//17-06-2022 //16-06-2022
             {
                 if (node.SelectSingleNode(".//div[@class='BET1rd']|.//div[@class='EfDVh wDYxhc NFQFxe']") == null && node.SelectSingleNode(".//div[@class='tpa-cc']") == null //23-12-2021 //21-08-2021 //25-09-2020
                     && node.SelectSingleNode(".//div[contains(@class,'kno-fb-ctx')]") == null) //13-10-2021

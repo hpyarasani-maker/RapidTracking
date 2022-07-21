a19c70fc9a90b3ec66b2d43da7b634f7c15496f8
UKUser <UKUser@HEMACHANDER>
2022-07-21 15:55:41 +0100

    Updated selector for wrong carousel block for mobile searches at line number 2389 //21-07-2022

--- RapidTrackingJobIDResults/iOS.cs
+++ RapidTrackingJobIDResults/iOS.cs
@@ -2382,7 +2382,7 @@ namespace RapidTrackingJobIDResults
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[@class='MIyI4c']"); //19-08-2021 updated for carousel block 
             if (nd != null)
-                if (node.SelectSingleNode(".//img[contains(@alt,'Map of ')]|.//div[@jsname='sSUqrd']") == null) //19-07-2022
+                if (node.SelectSingleNode(".//img[contains(@alt,'Map of ')]|.//div[@jsname='sSUqrd']|.//div[@class='EDblX DAVP1']") == null)//21-07=2022
                     return "Carousel";
 
             nd = node.SelectSingleNode(".//div[@class='TyzpY']");

d4c06fece749701c4a7c9584ba103a31aa9e0a1f
UKUser <UKUser@HEMACHANDER>
2022-07-26 15:42:16 +0100

    updated selector for missing carousel block for mobile searches at line number 2314 //26-07-2022

--- RapidTrackingLoopSingleThread/iOS.cs
+++ RapidTrackingLoopSingleThread/iOS.cs
@@ -2312,6 +2312,8 @@ namespace RapidTrackingLoopSingleThread
             //    nd = node.SelectSingleNode(".//div[@class='kp-blk EyBRub knowledge-panel OJXvsb']");//05-10-2020 commented  //13-07-2020 images block type and KP block type
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[contains(@class,'kp-blk EyBRub')]|.//div[contains(@class,'kp-hc')]"); //16-02-2022//05-10-2020 included selector for missing KP block
+            if (nd == null)
+                nd = node.SelectSingleNode(".//div[@class='Y2NmGf']");//26-07-2022
             if (nd != null)
             {
                 // 24-04-2020

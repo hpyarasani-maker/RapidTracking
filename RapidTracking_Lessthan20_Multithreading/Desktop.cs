0150f4d21773791657b267f674adca9793d15297
UKUser <UKUser@HEMACHANDER>
2022-08-01 16:39:02 +0100

    updated desktop process document at line number 47 to 50

--- RapidTracking_Lessthan20_Multithreading/Desktop.cs
+++ RapidTracking_Lessthan20_Multithreading/Desktop.cs
@@ -43,11 +43,11 @@ namespace TrackingTrending
                 nodeCol = doc.DocumentNode.SelectNodes("//div[@id='rso']/div|//div[@id='rso']/g-section-with-header|//div[@class='Hpbsqe']|//div[@id='Odp5De']");//23-12-2021//08-10-2021 images //03-12-2020  //01-05-2020 
             if (nodeCol == null || (nodeCol.Count > 1 && nodeCol.Count <= 3))//25-07-2022
                 nodeCol = doc.DocumentNode.SelectNodes(".//div[contains(@class,'WvKfwe')]/div|.//div[@class='UDZeY OTFaAf']/div|.//div[@class='ULSxyf']") ?? nodeCol;//19-07-2022//15-07-2022
-            if (nodeCol == null || nodeCol.Count <= 1)
+            if (nodeCol == null || nodeCol.Count <= 1) //01-08-2022 swapped lines
             {
-                nodeCol = doc.DocumentNode.SelectNodes("//div[@id='kp-wp-tab-overview']/div|//div[@class='hlcw0c']/div") ?? nodeCol;
+                nodeCol = doc.DocumentNode.SelectNodes("//div[contains(@class,'WvKfwe')]/div|.//div[@class='UDZeY OTFaAf']/div") ?? nodeCol;
                 if (nodeCol == null || nodeCol.Count <= 3)
-                    nodeCol = doc.DocumentNode.SelectNodes("//div[contains(@class,'WvKfwe')]/div|.//div[@class='UDZeY OTFaAf']/div") ?? nodeCol;
+                    nodeCol = doc.DocumentNode.SelectNodes("//div[@id='kp-wp-tab-overview']/div|//div[@class='hlcw0c']/div") ?? nodeCol;
             }
             foreach (HtmlNode node in nodeCol)
             {

f4b53aeb2d8439e490d200a2f56d1d1cdacbd542
UKUser <UKUser@HEMACHANDER>
2022-08-23 17:02:53 +0100

    updated

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -834,6 +834,7 @@ namespace RapidTrackingSingleThread
                         }
                         catch { title = ""; }
                         string url = nd.Attributes["href"].Value.Trim();
+                        if (!string.IsNullOrEmpty(SetUrl(url)))//08-08-2022
                             s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" />");
                     }
                     catch { }
@@ -1790,7 +1791,7 @@ namespace RapidTrackingSingleThread
             {
                 //21-11-2019
                 url = url.Replace("HTTPS://", "https://").Replace("HTTP://", "http://");
-                if (string.IsNullOrEmpty(url.Trim())) return string.Empty;
+                if (string.IsNullOrEmpty(url.Trim()) || url.StartsWith("#")) return string.Empty; //08-08-2022
                 //29-09-2020            
                 if (url.IndexOf("https://") == 0 || url.IndexOf("https://") >= 0) //01-10-2020
                     url = url.Remove(0, url.IndexOf("https://"));

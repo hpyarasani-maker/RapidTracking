3899e121bb599b6eb61c6e056f63f9cb3cff8932
UKUser <UKUser@HEMACHANDER>
2022-09-08 18:17:26 +0100

    updated se;ector for missing classic links for mobile searches at line number 266 //09-09-2022

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -263,7 +263,7 @@ namespace RapidTrackingSingleThread
 
                             HtmlNodeCollection cl = pla.SelectNodes(".//a[@class='pla-unit eUPzHb']|.//div[@class='mnr-c pla-unit']/a[2]|.//a[@class='plantl pla-unit-single-clickable-target clickable-card']");
                             if (cl == null)
-                                cl = pla.SelectNodes(".//a[@class='pla-unit']");
+                                cl = pla.SelectNodes(".//a[@class='pla-unit']|.//div[@class='v5yQqb jqWpsc']");//09-09-2022
                             if (cl != null)
                             {
                                 foreach (HtmlNode nd in cl)

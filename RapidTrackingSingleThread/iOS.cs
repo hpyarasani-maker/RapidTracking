fc7c0f1e37d1cb90bdccf1cdc9fc252bf17da7fb
UKUser <UKUser@HEMACHANDER>
2022-09-15 19:18:31 +0100

    update selector for maps and people also ask for mobile searches at line 2379 //15-09-2022

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -137,8 +137,7 @@ namespace RapidTrackingSingleThread
                             {
                                 //Current 15-12-2020 swapped from bottom HtmlNodeCollection
                                 HtmlNodeCollection nc = node.SelectNodes(".//div[@class='WvKfwe']/div|.//div[@class='WvKfwe a3spGf']/div" +
-                                    //"|.//div[@class='ChlgHf']|.//div[contains(@class,'UDZeY')]|.//div[@class='a3spGf WvKfwe']/div" +
-                                    "|.//div[@class='ChlgHf']|.//div[@class='a3spGf WvKfwe']/div" + //15-09-2022 missing top stories
+                                    "|.//div[@class='ChlgHf']|.//div[contains(@class,'UDZeY')]|.//div[@class='a3spGf WvKfwe']/div" +
                                     "|.//div[@class='WvKfwe a3spGf']/g-card|.//div[@class='WvKfwe a3spGf']/block-component");//20-05-2022  
                                 //if (nc == null || node.SelectNodes(".//div[@id='kp-wp-tab-overview']/div") != null)//07-10-2021 answer card and PAA blocks
                                 if (nc == null)//02-09-2022
@@ -2376,7 +2375,8 @@ namespace RapidTrackingSingleThread
                 nd = node.SelectSingleNode(".//div[@class='ifM9O']|.//div[contains(@class,'wob_ans')]");//31-03-2022 //28-02-2022
             if (nd != null)
             {
-                if (node.SelectSingleNode(".//div[@class='FEoF4d']") == null)//08-10-2021 Answer Card
+                //if (node.SelectSingleNode(".//div[@class='FEoF4d']") == null)//08-10-2021 Answer Card
+                if (node.SelectSingleNode(".//div[@class='FEoF4d']") == null && node.SelectSingleNode(".//div[@class='PZPZlf hb8SAc']") == null) //15-09-2022
                     return "AnswerCard";
             }
 

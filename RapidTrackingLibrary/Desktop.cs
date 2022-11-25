0943cca0799615b62f477a0be79f23079ef25e91
UKUser <UKUser@HEMACHANDER>
2022-11-25 17:26:30 +0000

    updated selector for missing classic links for desktop searches at line number 82 //25-11-2022

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -79,7 +79,7 @@ namespace RapidTrackingLibrary
                     if (nodeCol == null)
                         nodeCol = doc.DocumentNode.SelectNodes("//div[@class='vC5Ym DhKAUb']/div");  // 03-04-2020
                     if (nodeCol == null)
-                        nodeCol = doc.DocumentNode.SelectNodes(".//div[contains(@class,'WvKfwe')]/div|.//div[contains(@class,'WvKfwe')]/g-section-with-header|.//div[@class='UDZeY OTFaAf']");//09-12-2020
+                        nodeCol = doc.DocumentNode.SelectNodes(".//div[contains(@class,'WvKfwe')]/div|.//div[contains(@class,'WvKfwe')]/g-section-with-header|.//div[@class='UDZeY OTFaAf']|.//div[@class='ULSxyf V9qXjb']");//25-11-2022//09-12-2020
                     if (nodeCol != null) //11-08-2022
                     foreach (HtmlNode node in nodeCol)
                     {

7da9193f5c2bb4eebe16f873189496a8531d368b
UKUser <UKUser@HEMACHANDER>
2022-11-30 15:52:30 +0000

    Revert "updated selectors for missing KP and answer card for mobile searches at line number 139,140,141 //30-11-2022"
    This reverts commit 9d6c4cf0e3e60cbb4660ab16efda4f7d7d8907a7.

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -134,9 +134,7 @@ namespace RapidTrackingLibrary
                             if (node.HasClass("kp-wholepage") || node.SelectNodes(".//div[contains(@class, 'kp-wholepage')]") != null)
                             {
                                 //Current 15-12-2020 swapped from bottom HtmlNodeCollection
-                                HtmlNodeCollection nc = node.SelectNodes(".//div[contains(@class,'TzHB6b mnr-c UBoxCb')]");//30-11-2022
-                                if (nc == null)//30-11-2022
-                                    nc = node.SelectNodes(".//div[@class='TjcfIc eE3xqf B03h3d V14nKc ptcLIOszQJu__wholepage-card wp-ms']|.//div[@class='WvKfwe a3spGf']/div");
+                                HtmlNodeCollection nc = node.SelectNodes(".//div[@class='TjcfIc eE3xqf B03h3d V14nKc ptcLIOszQJu__wholepage-card wp-ms']|.//div[@class='WvKfwe a3spGf']/div");
                                 if (nc == null)
                                     nc = node.SelectNodes(".//div[@class='WvKfwe']/div|.//div[@class='WvKfwe a3spGf']/div" +
                                "|.//div[@class='ChlgHf']|.//div[@class='a3spGf WvKfwe']/div|.//div[contains(@class,'TzHB6b mnr-c UBoxCb')]" + //17-11-2022

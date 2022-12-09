fb6515fff52a86a41fbbcb85fb4f458bbdf14401
UKUser <UKUser@HEMACHANDER>
2022-12-09 15:15:49 +0000

    updated

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -1126,7 +1126,7 @@ namespace RapidTrackingSingleThread
                         name = link.SelectSingleNode(".//div[@class='DAB5ue']")?.InnerText;
                     }
                     if (!string.IsNullOrEmpty(SetUrl(url)) || !string.IsNullOrEmpty(title))
-                        s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" price=\"" + SetTitle(price) + "\" site=\"" + SetUrl(name) + "\" />");
+                        s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" price=\"" + SetTitle(price) + "\" site=\"" + SetTitle(name) + "\" />");
                 }
             }
             return s.ToString();

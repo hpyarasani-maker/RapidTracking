24e0c9c9ef0f567bdd712a2e3f1550a2abeca83e
UKUser <UKUser@HEMACHANDER>
2022-12-14 16:21:53 +0000

    updated selector for missing classic links for mobile searches at line number 139 and 140 //14-12-2022

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -134,8 +134,9 @@ namespace RapidTrackingLibrary
                             if (node.HasClass("kp-wholepage") || node.SelectNodes(".//div[contains(@class, 'kp-wholepage')]") != null)
                             {
                                 //Current 15-12-2020 swapped from bottom HtmlNodeCollection
-                                HtmlNodeCollection nc = node.SelectNodes(".//div[contains(@class,'TzHB6b mnr-c UBoxCb')]");//13-12-2022
-                                if (nc == null)//13-12-2022
+                                HtmlNodeCollection nc = node.SelectNodes(".//div[contains(@class,'TzHB6b')]");//14-12-2022
+                                if (nc == null || nc.Count <= 2) //14-12-2022
+                                    if (nc == null)//13-12-2022
                                     nc = node.SelectNodes(".//div[@class='TjcfIc eE3xqf B03h3d V14nKc ptcLIOszQJu__wholepage-card wp-ms']|.//div[@class='WvKfwe a3spGf']/div");//13-12-2022 //28-11-2022
                                 if (nc == null)
                                     nc = node.SelectNodes(".//div[@class='WvKfwe']/div|.//div[@class='WvKfwe a3spGf']/div" +

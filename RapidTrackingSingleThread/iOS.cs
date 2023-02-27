8e1e17ff3214099d24c393e3e842a2599bf32c76
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2023-02-27 16:55:36 +0000

    udpated wrong block for top stories for mobile searches at line nubmer 2497 //27-02-2023

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2473,6 +2473,7 @@ namespace RapidTrackingSingleThread
                 HtmlNode n = node.SelectSingleNode(".//div[@class='FRH7Ye gsrt']");
                 if (n == null)
                     n = node.SelectSingleNode(".//div[@class='TSyGMd']/a");  // changes on 05-07-2019
+
                 if (n != null)
                 {
                     if (n.InnerText.ToLower().Contains("news"))
@@ -2491,6 +2492,8 @@ namespace RapidTrackingSingleThread
                         if (n.InnerText.ToLower().Trim().Contains("top sights") || n.InnerText.ToLower().Trim().Contains("popular trip")) //19-09-2020 does not picks this blocks
                             ts = false;
                         if (node.SelectSingleNode(".//div[@class='YoZiHf']") != null) //08-01-2021 for not top sights
+                            ts = false;
+                        if (node.SelectSingleNode(".//div[@class='CEzIMc h7uAbe']") != null) //27-02-2023
                             ts = false;
                     }
                 }
@@ -2547,7 +2550,7 @@ namespace RapidTrackingSingleThread
 
                 nd = node.SelectSingleNode(".//div[@class='kp-blk nGydZ Wnoohf OJXvsb']|.//div[@class='NFQFxe XbtRGb qxsd xsZWvb EfDVh WDjuKe mod']|.//div[@class='NFQFxe viOShc LKPcQc mod']|.//div[@class='B3nbW mfMhoc']|.//div[@class='Ph8vHd']");//08-04-2022 KP
                 if (nd == null || node.SelectSingleNode(".//div[@class='kp-header']") != null || node.SelectSingleNode(".//div[@class='K2Sb0e kp-header']") != null)   //23-06-2020 //19-06-2020
-                    if (node.SelectSingleNode(".//div[@class='RzdJxc']|.//div[@class='EDblX DAVP1 qIfKhf yUxSId']") == null) //06-12-2021 wrong block //30-08-2021 video block missing
+                    if (node.SelectSingleNode(".//div[@class='RzdJxc']|.//div[@class='EDblX DAVP1 qIfKhf yUxSId']|.//div[@class='CEzIMc h7uAbe']") == null) //27-02-2023//06-12-2021 wrong block //30-08-2021 video block missing
                         return "KnowledgePanel";
             }
             //swapped 19-03-2020

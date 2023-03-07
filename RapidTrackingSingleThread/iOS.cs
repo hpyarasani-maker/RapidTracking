e388aa90e53b5075a5a0bffdf9c1e4c9819cb448
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2023-03-07 17:22:27 +0000

    updated selector for wrong video block for mobile searches at line number 2664 //07-03-2023

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2670,7 +2670,8 @@ namespace RapidTrackingSingleThread
                     if (nd != null)
                 {
                     if (nd.InnerHtml.ToLower().StartsWith("video") || nd.InnerHtml.ToLower().StartsWith("vídeo")) //07-02-2020 included title for video block for different language
-                        return "Videos";
+                        if (node.SelectSingleNode(".//div[@class='vbbrJ']") == null)//07-03-2023
+                            return "Videos";
                     else if (nd.InnerHtml.ToLower().StartsWith("top stories") || nd.InnerHtml.Contains("Interesting finds")) // 18-12-2019)
                         return "Topstories";
                     HtmlNode nd1 = node.SelectSingleNode(".//div[@class='UDZeY fAgajc']|.//div[@class='rKFBM gsrt CAd2fd wp-ms']");  //31-03-2020

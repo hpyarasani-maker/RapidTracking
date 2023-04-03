90a2f9f9d948cd4eb417ff0ae93325d76e03edfe
UKUser <UKUser@HEMACHANDER>
2023-04-03 14:50:27 +0100

    updated missing unclosed TS block and people also ask and images duplicates at line number 777,  833 and 847 to 853 //02-04-2023

--- RapidTrackingSingleThread/frmSingleThread.cs
+++ RapidTrackingSingleThread/frmSingleThread.cs
@@ -302,7 +302,7 @@ namespace RapidTrackingSingleThread
                 //lstKWs.Items.Add("160:malmö ff");
                 //lstKWs.Items.Add("102:terry crews");
                 //lstKWs.Items.Add("102:the uninhabitable earth summary");
-                lstKWs.Items.Add("106:renault clio");
+                lstKWs.Items.Add("102:netflix shows to watch");
             });
             return;
 

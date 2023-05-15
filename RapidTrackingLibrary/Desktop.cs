25d4599e36943c54bb6823ddf315e2909316290b
UKUser <UKUser@HEMACHANDER>
2023-05-15 16:46:33 +0100

    updated selectors for duplicate item list in HotelPack (GetHotels method) for Desktop Searches at line 1608,1609,1610 //15-06-2023

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -1509,7 +1509,10 @@ namespace RapidTrackingLibrary
         public string GetHotels(HtmlNode node)//24-03-2022 new element Maps
         {
             StringBuilder s = new StringBuilder();
-            HtmlNodeCollection nds = node.SelectNodes(".//div[contains(@class,'hmHBZd')]|.//div/a[contains(@class,'hmHBZd')]|.//div[@class='fQtNvd']|.//div[@class='KmZaZb']|.//div[@class='Fcmcxd']");//11-05-2023//26-04-2023//19-04-2023
+            //HtmlNodeCollection nds = node.SelectNodes(".//div[contains(@class,'hmHBZd')]|.//div/a[contains(@class,'hmHBZd')]|.//div[@class='fQtNvd']|.//div[@class='KmZaZb']|.//div[@class='Fcmcxd']");//11-05-2023//26-04-2023//19-04-2023
+            HtmlNodeCollection nds = node.SelectNodes(".//div[contains(@class,'hmHBZd')]|.//div[@class='fQtNvd']|.//div[@class='KmZaZb']");//15-05-2023
+            if (nds == null)//15-05-2023
+                nds = node.SelectNodes(".//div/a[contains(@class,'hmHBZd')]|.//div[@class='Fcmcxd']");//15-05-2023
             if (nds != null)
             {
                 foreach (HtmlNode nd in nds)

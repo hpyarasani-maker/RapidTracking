b197d6491b13076bdcbacec96196a4b5de7e7790
UKUser <UKUser@HEMACHANDER>
2023-06-07 17:01:07 +0100

    updated desktop processDocument

--- TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/Desktop.cs
+++ TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/Desktop.cs
@@ -48,7 +48,7 @@ namespace TrendingReceiving
             if (nodeCol == null)
                 nodeCol = doc.DocumentNode.SelectNodes("//div[@id='rso']/div|//div[@id='rso']/g-section-with-header|//div[@class='Hpbsqe']|//div[@id='Odp5De']");//12-04-2023
             if (nodeCol == null || (nodeCol.Count <= 1))//12-04-2023
-                nodeCol = doc.DocumentNode.SelectNodes(".//div[contains(@class,'TzHB6b cLjAic')]|//div[contains(@id, 'arc-srp')]/div/div[@class='MjjYud']|//div[contains(@id, 'arc-srp')]/div/div/div[@class='MjjYud']") ?? nodeCol;//12-04-2023
+                nodeCol = doc.DocumentNode.SelectNodes("//div[@id='rso']/div|//div[@id='rso']/g-section-with-header|//div[@class='Hpbsqe']|//div[@id='Odp5De']|//div[contains(@id, 'arc-srp')]/div/div[@class='MjjYud']|//div[contains(@id, 'arc-srp')]/div/div/div[@class='MjjYud']");//06-06-2023
             if (nodeCol == null || nodeCol.Count <= 4)//02-05-2023//21-04-2023
                 nodeCol = doc.DocumentNode.SelectNodes(".//div[@class='WvKfwe a3spGf']/div") ?? nodeCol;//26-04-2023//21-04-2023
             if (nodeCol == null || (nodeCol.Count > 1 && nodeCol.Count <= 3))//25-07-2022

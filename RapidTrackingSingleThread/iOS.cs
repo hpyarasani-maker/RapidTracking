9fc2313a98b3cfd289761cb742681015f425e9b1
UKUser <UKUser@HEMACHANDER>
2023-06-12 16:08:11 +0100

    updated 891 in IOS

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -888,7 +888,7 @@ namespace RapidTrackingSingleThread
                             {
                                 try
                                 {
-                                    if (Regex.IsMatch(img.OuterHtml, "id=\"vidthumb\\d*\"") || Regex.IsMatch(img.OuterHtml, "id=\"dimg_\\d*\"") || img.Attributes["class"].Value.Contains("__video-result") || nd.SelectSingleNode(".//div[contains(@class,'c8rnLc flgn0c')]")!=null || nd.SelectSingleNode(".//video-voyager") != null  || nd.SelectSingleNode(".//div[@class='c8rnLc flgn0c']|.//div[@class='J1mWY']")  != null)//10-06-2023//29-03-2023//27-03-2023 //08-04-2022 videos
+                                    if (Regex.IsMatch(img.OuterHtml, "id=\"vidthumb\\d*\"") || Regex.IsMatch(img.OuterHtml, "id=\"dimg_\\d*\"") || img.Attributes["class"].Value.Contains("__video-result") || nd.SelectSingleNode(".//div[contains(@class,'c8rnLc flgn0c')]")!=null || nd.SelectSingleNode(".//video-voyager") != null  || nd.SelectSingleNode(".//div[@class='J1mWY']")  != null)//10-06-2023//29-03-2023//27-03-2023 //08-04-2022 videos
                                     {
                                         HtmlNode vdo = nd.SelectSingleNode(".//a[contains(@class,'BmP5tf')]"); //11-11-2021
                                         // video block.

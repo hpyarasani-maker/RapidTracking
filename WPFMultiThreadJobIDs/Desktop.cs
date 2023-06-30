0dbf8858f82cb3cd3a8e18b899ba4e5cae27c3e5
UKUser <UKUser@HEMACHANDER>
2023-06-30 17:54:46 +0100

    updated

--- WPFMultiThreadJobIDs/Desktop.cs
+++ WPFMultiThreadJobIDs/Desktop.cs
@@ -1877,7 +1877,7 @@ namespace WPFMultiThreadJobIDs
                 return "TopSights";*///23-03-2022//19-01-2023
 
             nd = node.SelectSingleNode(".//div[contains(@class,'WlTAzf')]|.//div[contains(@class, 'vdQmEd')]");//29-06-2023//23-03-2022
-            if (nd != null)
+            if (nd != null && nd.SelectNodes(".//div[@class='MxQnIc']") == null)//30-06-2023
                 return "Flights";//23-03-2022
 
             //nd = node.SelectSingleNode(".//div[@class='kp-blk cUnQKe']|.//div[@class='kp-blk cUnQKe Wnoohf OJXvsb']|.//div[@jsname='N760b']");//08-07-2021//04-12-2020 //11-02-2020

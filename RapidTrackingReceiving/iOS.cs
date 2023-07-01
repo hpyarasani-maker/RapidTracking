f184c7605074c82d7d6e09cacf2565395561c3a0
UKUser <UKUser@HEMACHANDER>
2023-07-01 18:03:28 +0100

    updated

--- RapidTrackingReceiving/iOS.cs
+++ RapidTrackingReceiving/iOS.cs
@@ -2538,6 +2538,8 @@ namespace Oxylabs_BulkKeywords
                 price = mc.Value;
             if (price.Equals("unknown"))
                 price = "0";
+            if (price.Equals("check price"))//01-06-2023
+                price = "0";//01-06-2023
             return price;
         }
         private string ConvertHours(string hours) //29-06-2023

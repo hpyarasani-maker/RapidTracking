a93d63ef6239825df49dd124861076dc3942b30f
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2023-08-24 18:12:13 +0100

    updated selector for missing video block for mobile searches at line number 1307 and 1308 //24-08-2023

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -1318,6 +1318,8 @@ namespace RapidTrackingLibrary
                 videos = nd.SelectSingleNode(".//jsname[@class='ibnC6b']/a|.//div[@jsname='ibnC6b']/a");
             if (videos == null)
                 videos = nd.SelectSingleNode(".//div[@class='BycXVc']/a");//12-10-2021 //11-10-2021
+            if (videos == null)//24-08-2023
+                videos = nd.SelectSingleNode(".//div[@class='QvOPBf']");//24-08-2023
             if (videos != null)
             {
                 s.Append("<block type=\"videos\" url=\"\">");

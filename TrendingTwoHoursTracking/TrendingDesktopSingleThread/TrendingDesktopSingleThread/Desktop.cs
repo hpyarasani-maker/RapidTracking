13f2b6425c32b96f313d2cdf6fcf004635e588b8
UKUser <UKUser@HEMACHANDER>
2023-09-21 17:42:26 +0100

    updated desktop files //21-09-2023 in process document line number 52

--- TrendingTwoHoursTracking/TrendingDesktopSingleThread/TrendingDesktopSingleThread/Desktop.cs
+++ TrendingTwoHoursTracking/TrendingDesktopSingleThread/TrendingDesktopSingleThread/Desktop.cs
@@ -54,7 +54,7 @@ namespace TrendingDesktopSingleThread
             if (nodeCol == null || (nodeCol.Count <= 1))//12-04-2023
                 nodeCol = doc.DocumentNode.SelectNodes(".//div[contains(@class,'TzHB6b cLjAic')]|//div[contains(@id, 'arc-srp')]/div/div[@class='MjjYud']|//div[contains(@id, 'arc-srp')]/div/div/div[@class='MjjYud']") ?? nodeCol;//12-04-2023
             if (nodeCol == null || nodeCol.Count <= 4)//02-05-2023//21-04-2023
-                nodeCol = doc.DocumentNode.SelectNodes(".//div[@class='WvKfwe a3spGf']/div") ?? nodeCol;//26-04-2023//21-04-2023
+                nodeCol = doc.DocumentNode.SelectNodes(".//div[@class='WvKfwe a3spGf']/div|.//div[@class='TzHB6b cLjAic LMRCfc']") ?? nodeCol;//21-09-2023 //19-09-2023 
             if (nodeCol == null || (nodeCol.Count > 1 && nodeCol.Count <= 3))//25-07-2022
                 nodeCol = doc.DocumentNode.SelectNodes(".//div[contains(@class,'WvKfwe')]/div|.//div[@class='UDZeY OTFaAf']/div|.//div[contains(@class,'ULSxyf')]|//div[@class='hlcw0c']/div") ?? nodeCol;//16-12-2022//17-08-2022
             if (nodeCol == null || nodeCol.Count <= 1) //01-08-2022 swapped lines

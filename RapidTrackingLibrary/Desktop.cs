2c034199f62c85d28c9c48e892d480f55c50bf70
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2023-09-23 17:17:33 +0100

    updated selector for wrong image block instead of Answer Card for desktop searches at line number 1916 //23-09-2023

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -1924,7 +1924,8 @@ namespace RapidTrackingLibrary
                     return "AnswerCard";
                 else if (node.SelectSingleNode(".//div[@class='WcS13d']") != null && node.SelectSingleNode(".//div[@class='EfDVh wDYxhc NFQFxe']") != null //27-12-2021
                     || node.SelectSingleNode(".//div[@class='nmVgI3FLyE0__answer']") != null
-                    || node.SelectSingleNode(".//div[@class='N6Sb2c i29hTd']") != null)//16-06-2022 //23-03-2022
+                    || node.SelectSingleNode(".//div[@class='N6Sb2c i29hTd']") != null//16-06-2022 //23-03-2022
+                    || node.SelectSingleNode(".//div[@class='ifM9O']") != null) //23-09-2023
                     return "AnswerCard";//27-12-2021
             }
             //05-10-2020 KP Block selectors updated

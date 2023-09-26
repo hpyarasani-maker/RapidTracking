9602ea13a818e780e95f14c842bc89de0ec235de
UKUser <UKUser@HEMACHANDER>
2023-09-26 19:46:44 +0100

    UPDATED ProcessDocument line number 43 //25-09-2023

--- TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/iOS.cs
+++ TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/iOS.cs
@@ -41,7 +41,7 @@ namespace TrendingReceiving
                 nodeCol = nodeCol[nodeCol.Count - 1].SelectNodes("a/div");  //28-04-2020
             if (nodeCol == null)//20-09-2023
                 nodeCol = doc.DocumentNode.SelectNodes("//div[@id='rso']/div[@class='MjjYud']/div|//div[@id='rso']/div[@class='MjjYud']/block-component|.//div[contains(@class,'TzHB6b')]");//22-09-2023//21-09-2023//20-09-2023
-            if (nodeCol == null)
+            if (nodeCol == null || doc.DocumentNode.SelectNodes("//div[@class='WtZO4e']/div|//div[@classname='WtZO4e']/div")?.Count > 1)//25-09-2023
                 nodeCol = doc.DocumentNode.SelectNodes("//div[@id='rso']/div|//div[@id='rso']/g-card|//div[@id='taw']/div[@class='med']/div[2]/div|//div[@id='rso']/nav|//div[@id='rso']/block-component/div|//div[contains(@id, 'arc-srp')]/div/div[@class='MjjYud']|//div[contains(@id, 'arc-srp')]/div/div/div[@class='MjjYud']|.//div[contains(@class,'TzHB6b')]");//29-03-2023//07-01-2022 event results//28-04-2020
             if (nodeCol != null && nodeCol.Count == 1)
                 nodeCol = doc.DocumentNode.SelectNodes("//div[@id='rso']/div|//div[@class='vC5Ym DhKAUb']/div");  //23-03-2023  //17-09-2019

5b279e036262455fe5c82991b17575371862b291
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2023-11-03 15:52:54 +0000

    updated selector for missing answer card and images for desktop searches at line number 589, 590 and 1938 //03-11-2023

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -605,8 +605,8 @@ namespace RapidTrackingLibrary
                 }
             }
             ///30-09-2022 start new code for answer carc
-            HtmlNode ac = doc.DocumentNode.SelectSingleNode(".//div[@class='ULSxyf a2qDab EyBRub']");
-            if (ac != null && ac.SelectSingleNode(".//div[@class='NhRr3b']") != null)
+            HtmlNode ac = doc.DocumentNode.SelectSingleNode(".//div[@class='ULSxyf a2qDab EyBRub']|.//div[@id='Odp5De']");//03-11-2023
+            if (ac != null && ac.SelectSingleNode(".//div[contains(@class,'NhRr3b')]") != null)//03-11-2023
             {
                 s.Append("<block type=\"answerCard\" url=\"\">");
                 s.Append(GetAnswerCard(ac));
@@ -1960,7 +1960,8 @@ namespace RapidTrackingLibrary
                 else if ((node.SelectSingleNode(".//div[@class='WcS13d']") != null && node.SelectSingleNode(".//div[@class='EfDVh wDYxhc NFQFxe']") != null //10-10-2023
                      || node.SelectSingleNode(".//div[@class='nmVgI3FLyE0__answer']") != null //10-10-2023
                      || node.SelectSingleNode(".//div[@class='N6Sb2c i29hTd']") != null //10-10-2023
-                     || node.SelectSingleNode(".//div[@class='ifM9O']") != null) && node.SelectSingleNode(".//img[contains(@alt, 'Map of')]") == null && node.SelectSingleNode(".//h3[@role='heading']")?.InnerText != "Recipes" && node.SelectSingleNode(".//ul/product-viewer-group|.//div[@class='aJegcc']") == null)//02-11-2023//30-10-2023//10-10-2023
+                     || node.SelectSingleNode(".//div[@class='ifM9O']") != null) && node.SelectSingleNode(".//img[contains(@alt, 'Map of')]") == null && node.SelectSingleNode(".//h3[@role='heading']")?.InnerText != "Recipes" && node.SelectSingleNode(".//ul/product-viewer-group|.//div[@class='aJegcc']|.//div[@id='iur']") == null)//03-11-2023//02-11-2023//30-10-2023//10-10-2023
+
                     return "AnswerCard";//27-12-2021
             }
             //05-10-2020 KP Block selectors updated

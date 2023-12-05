107c613829fc82ac943e0a37a92b79cb28c0bcf4
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2023-12-05 18:16:31 +0000

    updated selector for missing map block for desktop searches at line numbers 2194,2195,2196 //05-12-2023

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -2191,8 +2191,10 @@ namespace RapidTrackingSingleThread
                 nd = node.SelectSingleNode(".//g-img/img");
                 if (nd != null)
                 {
-                    if (nd.Attributes["alt"].Value.StartsWith("Map of ") || node.SelectSingleNode(".//div[@class='H93uF']|.//div[@class='uaxL4e ef0Hld']") != null || node.SelectSingleNode(".//img[contains(@alt,'Map of ')]") != null)//24-11-2023//06-12-2022//21-04-2022
-                        return true;
+                    if (nd.Attributes["alt"].Value.StartsWith("Map of ") || node.SelectSingleNode(".//div[@class='H93uF']" + //24-11-2023//06-12-2022//21-04-2022
+                        "|.//div[@class='uaxL4e ef0Hld']") != null || node.SelectSingleNode(".//img[contains(@alt,'Map of ')]" +
+                        "|.//img[contains(@alt,'Karte von ')]") != null)//05-12-2023
+                            return true;
                     if (node.SelectSingleNode(".//div[@class='U1TUId LYh3vc']") != null) //16-12-2021
                         return false; //16-12-2021
                 }

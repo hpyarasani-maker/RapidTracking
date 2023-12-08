65d5e412b10a0945734fbd91baf53f87c50c1ff9
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2023-12-08 17:08:59 +0000

    updated again

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -1733,8 +1733,7 @@ namespace RapidTrackingSingleThread
             if (nds == null)
                 nds = node.SelectNodes(".//div[contains(@class,'MUxGbd lyLwlc')]/a"); //01-11-2022
             if (nds == null)//08-12-2023
-                nds = node.SelectNodes(".//div[@class='DkX4ue Va3FIb EE3Upf lVm3ye']/a" +//08-12-2023
-                    "|.//div[@class='DkX4ue Va3FIb EE3Upf']/a|.//div[@class='DkX4ue IBylpb Va3FIb EE3Upf lVm3ye ']/a");//08-12-2023
+                nds = node.SelectNodes(".//div[@class='E8hWLe SVMeif BmP5tf']/div/a");//08-12-2023
             if (nds != null)
             {
                 s.Append("<block type=\"siteLinks\" url=\"\">");

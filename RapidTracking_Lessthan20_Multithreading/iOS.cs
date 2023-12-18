4c6d6d04f224634727d1e2e4ea94ad5ae8142b05
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2023-12-18 12:53:44 +0000

    updated 2289

--- RapidTracking_Lessthan20_Multithreading/iOS.cs
+++ RapidTracking_Lessthan20_Multithreading/iOS.cs
@@ -2290,8 +2290,10 @@ namespace TrackingTrending
                             url = nd.Attributes["data-url"]?.Value; //23-02-2021
                         //string title = nd.SelectSingleNode(".//div[@class='fJiQld']|.//div[@class='fJiQld oz3cqf vH5Lmd']").InnerText;//12-06-2020
                         string title = nd.SelectSingleNode(".//div[contains(@class,'fJiQld')]").InnerText; //end 23-03-2021
+                        string creator = nd.SelectSingleNode(".//div[@class='YDQ8Te Fy8FNd OSrXXb']/span[3]")?.InnerText ?? "";//04-12-2023
                         if (url.Contains("/search?")) url = "";
-                        s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" />");
+                        //s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" />");
+                        s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" creatorName=\"" + SetTitle(creator) + "\" />");//04-12-2023
                     }
                 }
                 // 16-12-2019

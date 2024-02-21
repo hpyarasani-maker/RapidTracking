ecd63e5b687746e08159adb3202a90d90831880f
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-02-21 16:18:34 +0000

    updated new search engines in all searchParams Files from SEID=778 to 786 //21-02-2024

--- ReceivingProject/SearchParams.cs
+++ ReceivingProject/SearchParams.cs
@@ -2836,6 +2836,42 @@ namespace ReceivingProject
             {
             seid =777, domain ="co.ma", geo_location="Morocco", locale= "fr-ma", uule = "w+CAIQICIHTW9yb2Njbw==",device="mobile_android"
             },//27-01-2024
+            new SearchProperties()//21-02-2024
+            {
+               seid =778, domain ="hn", geo_location="Honduras", locale= "es-hn", uule = "w+CAIQICIISG9uZHVyYXM=", device="mobile_android"
+            },
+           new SearchProperties()
+            {
+               seid =779, domain ="com.sv", geo_location="El Salvador", locale= "es-sv", uule = "w+CAIQICILRWwgU2FsdmFkb3I=", device="mobile_android"
+            },
+            new SearchProperties()
+            {
+               seid =780, domain ="com.cy", geo_location="Famagusta,Cyprus", locale= "tr-cy", uule = "w+CAIQICIQRmFtYWd1c3RhLEN5cHJ1cw==", device="mobile_android"
+            },
+           new SearchProperties()
+            {
+                seid =781, domain ="com.ua", geo_location="Ukraine", locale= "uk-ua", uule = "w+CAIQICIHVWtyYWluZQ==", device="mobile_android"
+            },
+            new SearchProperties()
+            {
+                seid =782, domain ="com.ua", geo_location="Ukraine", locale= "ru-ua", uule = "w+CAIQICIHVWtyYWluZQ==", device="mobile_android"
+            },
+            new SearchProperties()
+            {
+               seid = 783, domain ="rs", geo_location="Serbia", locale= "sr-rs", uule = "w+CAIQICIGU2VyYmlh", device="mobile_android"
+            },
+           new SearchProperties()
+            {
+              seid = 784, domain ="sk", geo_location="Slovakia", locale= "sk-sk", uule = "w+CAIQICIIU2xvdmFraWE=", device="mobile_android"
+            },
+           new SearchProperties()
+            {
+              seid =785, domain ="si", geo_location="Slovenia", locale= "sl-si", uule = "w+CAIQICIIU2xvdmVuaWE=", device="mobile_android"
+            },
+           new SearchProperties()
+            {
+               seid =786, domain ="co.il", geo_location="Israel", locale= "iw-il", uule = "w+CAIQICIGSXNyYWVs", device="mobile_android"
+            },//21-02-2024
         };
     }
     class SearchProperties

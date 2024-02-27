2498140e560fc33966825c8c596c1ad4ee5e34e3
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-02-27 19:35:14 +0000

    updated selector for missing hotel pack for mobile searches at line number 2963 //27-02-2024

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2960,7 +2960,7 @@ namespace RapidTrackingSingleThread
                 || (node.SelectSingleNode(".//div[@class='VqeGe']") != null && node.SelectSingleNode(".//div[@class='NYidgb']|.//div[@class='xKf9F']") == null))//27-02-2024//26-02-2024//20-02-2024
             {
                 if ((node.SelectSingleNode(".//div[@class='Gqsa8d']") != null && node.SelectSingleNode(".//div[@class='EDblX HG5ZQb']") != null)//27-02-2024
-                    || node.SelectSingleNode(".//div[@class='HOslld dutT5c']") != null)//27-02-2024
+                    || node.SelectSingleNode(".//div[@class='HOslld dutT5c']|.//div[@class='zJUuqf adDDi']") != null)//27-02-2024
                     return "Hotel";
             }
             nd = node.SelectSingleNode(".//*[@id='rXuTZe']");

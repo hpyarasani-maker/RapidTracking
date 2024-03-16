9123f83dcf23bce0d6f93b056432bfb1eb2b2c78
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-03-16 16:50:21 +0000

    updated selector in al IOS at line number 3041 //16-03-2024

--- RapidMissingJobsReceiving/iOS.cs
+++ RapidMissingJobsReceiving/iOS.cs
@@ -3030,7 +3030,7 @@ namespace RapidMissingJobsReceiving
                 || (node.SelectSingleNode(".//div[@class='VqeGe']") != null && node.SelectSingleNode(".//div[@class='NYidgb']|.//div[@class='xKf9F']") == null))//27-02-2024//26-02-2024//20-02-2024
             {
                 if ((node.SelectSingleNode(".//div[@class='Gqsa8d']") != null && node.SelectSingleNode(".//div[@class='EDblX HG5ZQb']") != null)//27-02-2024
-                    || node.SelectSingleNode(".//div[@class='HOslld dutT5c']|.//div[@class='zJUuqf adDDi']") != null)//27-02-2024
+                     || node.SelectSingleNode(".//div[@class='HOslld dutT5c']|.//div[@class='zJUuqf adDDi']") != null && node.SelectSingleNode(".//div[@class='RyIFgf']") == null)//16-03-2024//27-02-2024
                     return "Hotel";
             }
             nd = node.SelectSingleNode(".//*[@id='rXuTZe']");

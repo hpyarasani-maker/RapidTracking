2098528d0e86ffb2541e0568ba4f0bdfabda1449
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-03-20 18:17:56 +0000

    updated slash for url attribute

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2030,7 +2030,7 @@ namespace RapidTrackingSingleThread
             }//20-03-2024
             string cardType = lst ? "list" : tbl ? "table" : video ? "video" : chrt ? "chart" : "text";
             s.Append("<block type=\"answerCard\" url=\"" + SetUrl(url) + "\" >");
-            s.Append("<item featureTitle=\"" + SetTitle(f_title) + "\" url="" + "\" title=\"" +
+            s.Append("<item featureTitle=\"" + SetTitle(f_title) + "\" url=\"" + "\" title=\"" +
                 SetTitle(title) + "\" description=\"" + SetTitle(desc) + "\" cardType=\"" + cardType + "\" />");
             s.Append("</block>");
             return s.ToString();

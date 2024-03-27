51563318866f36ae97cddfdc4d4fd23a10078346
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-03-27 17:08:51 +0000

    updated throw statement for old answer card

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -718,7 +718,8 @@ namespace RapidTrackingSingleThread
                             App2 = answernode.SelectNodes(".//div[@class='WLSb4b']");//23-06-2020
                         if (App2 != null)
                         {
-                            s.Append("<block type=\"answerCard\" url=\"\">");
+                            throw new Exception("Old Answer card block returned");//27-03-2024
+                            /*s.Append("<block type=\"answerCard\" url=\"\">");
                             foreach (HtmlNode nd in App2)
                             {
                                 string title = "";
@@ -732,7 +733,7 @@ namespace RapidTrackingSingleThread
                                 s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" />");
                             }
 
-                            s.Append("</block>");
+                            s.Append("</block>");*/
                         }
                     }
                 }

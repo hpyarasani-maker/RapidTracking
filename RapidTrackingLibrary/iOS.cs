e115a50aa7ee6ee7da04b8b4bdf4371475282c26
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-04-02 18:05:40 +0100

    updated again for twitter card and crousel block for mobile searches at line number 2843

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -2868,7 +2868,7 @@ namespace RapidTrackingLibrary
                             ts = false;
                     }
                 }
-                if (ts && node.SelectSingleNode(".//div[@class='imso-ml-c PZPZlf']|.//div[@class='Bv2VAe']") == null)//02-04-2024
+                if (ts && node.SelectSingleNode(".//div[@class='imso-ml-c PZPZlf']|.//div[@class='Bv2VAe']|.//div[@jsname='GDPwke']") == null)//02-04-2024
                     return "Topstories";
             }
              if (node.SelectSingleNode(".//div[contains(@class, 'RPdfze')]") != null || (node.SelectSingleNode(".//div[contains(@class, 'Qkn3ie')]" +

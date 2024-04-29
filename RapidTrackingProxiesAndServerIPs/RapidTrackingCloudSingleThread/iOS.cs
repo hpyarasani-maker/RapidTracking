3a4d4c1b677561975c4b566019f8898614de5e05
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-04-29 15:12:13 +0100

    updated IOS //29-04-2024

--- RapidTrackingProxiesAndServerIPs/RapidTrackingCloudSingleThread/iOS.cs
+++ RapidTrackingProxiesAndServerIPs/RapidTrackingCloudSingleThread/iOS.cs
@@ -3066,7 +3066,7 @@ namespace RapidTrackingCloudSingleThread
                 || (node.SelectSingleNode(".//div[@class='VqeGe']") != null && node.SelectSingleNode(".//div[@class='NYidgb']|.//div[@class='xKf9F']") == null))//27-02-2024//26-02-2024//20-02-2024
             {
                 if ((node.SelectSingleNode(".//div[@class='Gqsa8d']") != null && node.SelectSingleNode(".//div[@class='EDblX HG5ZQb']") != null)//27-02-2024
-                    || node.SelectSingleNode(".//div[@class='HOslld dutT5c']|.//div[@class='zJUuqf adDDi']") != null && node.SelectSingleNode(".//div[@class='RyIFgf']") == null)//16-03-2024//27-02-2024
+                    || node.SelectSingleNode(".//div[@class='HOslld dutT5c']|.//div[@class='zJUuqf adDDi']") != null && (node.SelectSingleNode(".//div[@class='RyIFgf']") == null || node.SelectNodes(".//div[contains(@class,'EXH1Ce')]") != null))//29-04-2024//16-03-2024//27-02-2024
                     return "Hotel";
             }
             nd = node.SelectSingleNode(".//*[@id='rXuTZe']");

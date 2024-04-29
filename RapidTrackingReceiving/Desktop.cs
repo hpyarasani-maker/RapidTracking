607613489450fefffcf5ee4caa6216f45061b758
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-04-29 12:30:51 +0100

    updated missing adwords block for desktop searhes at line nuber 2513 to 2516 //29-04-2024

--- RapidTrackingReceiving/Desktop.cs
+++ RapidTrackingReceiving/Desktop.cs
@@ -2504,7 +2504,10 @@ namespace Oxylabs_BulkKeywords
                     {
                         indx = url.LastIndexOf("https://");
                     }
-                    url = url.Remove(0, indx);
+                    if (indx > 0) //29-04-2024
+                    {
+                        url = url.Remove(0, indx);
+                    }//29-04-2024
                 }//28-03-2024
 
                 if (url.Contains("&amp;grqid="))
--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -2510,7 +2510,10 @@ namespace RapidTrackingSingleThread
                     {
                         indx = url.LastIndexOf("https://");
                     }
-                    url = url.Remove(0, indx);
+                    if (indx > 0) //29-04-2024
+                    {
+                        url = url.Remove(0, indx);
+                    }//29-04-2024
                 }//28-03-2024
 
                 if (url.Contains("&amp;grqid="))

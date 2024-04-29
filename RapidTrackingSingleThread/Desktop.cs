0c3e805c3d5fcffcf5b08e719f1ce57149c59d04
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-04-29 15:03:06 +0100

    updated htmlNode in GetCarousel method parameter name for desktop at line number 493 //29-04-2024

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -490,7 +490,7 @@ namespace RapidTrackingSingleThread
                 if (carousel != null)//07-12-2023 end
                 {
                     s.Append("<block type=\"carousel\" url=\"\">");
-                    s.Append(GetCarousel(crNode));//29-04-2024 uncommented  // 23-10-2019
+                    s.Append(GetCarousel(carousel));//29-04-2024 uncommented  // 23-10-2019
                     s.Append("</block>");
                 }
             }

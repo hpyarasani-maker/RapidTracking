3431b725412dc40df589ba3ca202eca8578636b0
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-07-08 18:19:31 +0100

    updated selector for missing Twitter and PeopleAlsoAsk blocks for desktop searches at line number 53 //08-07-2024

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -50,7 +50,7 @@ namespace RapidTrackingSingleThread
                 if (nodeCol == null || (nodeCol.Count >= 1 && nodeCol.Count <= 5))//09-02-2024
                 {//08-02-2024
                     if (rso.SelectNodes("//div[contains(@id,'kp-wp-tab-')]") != null)
-                        nodeCol = rso.SelectNodes(".//div[contains(@class,'TzHB6b cLjAic')]|.//div[contains(@class,'g Ww4FFb')]|.//div[@class='g PmEWq']");//05-07-2024
+                    nodeCol = rso.SelectNodes(".//div[contains(@class,'TzHB6b cLjAic')]|.//div[contains(@class,'g Ww4FFb')]|.//div[@class='g PmEWq']|.//div[@class='g eejeod']|.//div[@class='uVMCKf']|.//div[@class='cUnQKe']");//08-07-2024
                     else
                         nodeCol = rso.SelectNodes(".//div[contains(@class,'WvKfwe')]/div|.//div[@class='UDZeY OTFaAf']/div|.//div[@class='MjjYud']|.//div[contains(@class,'g Ww4FFb')]");
                 }//08-02-2024

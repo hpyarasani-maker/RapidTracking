56f2e7da8b1639954345c9f6d9b71e9823d22fa2
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-09-11 17:49:00 +0100

    updated IOS //11-09-2024

--- TrendingTwoHoursTracking/TrendingMobileSingleThread/TrendingMobileSingleThread/iOS.cs
+++ TrendingTwoHoursTracking/TrendingMobileSingleThread/TrendingMobileSingleThread/iOS.cs
@@ -1338,7 +1338,7 @@ namespace TrendingMobileSingleThread
                     string name = string.Empty;
                     string price_value = "0";//28-11-2023
                     //HtmlNode link = nd.SelectSingleNode(".//a");
-                    HtmlNode link = nd.SelectSingleNode(".//div[@class='NcgzWb Nplhsf']|.//div[@class='yDecDe w4sdvb']" +
+                    HtmlNode link = nd.SelectSingleNode(".//div[contains(@class,'NcgzWb Nplhsf')]|.//div[@class='yDecDe w4sdvb']" +//09-09-2024
                         "|.//g-inner-card[@class='ivG6i']/div|.//div[@class='NcgzWb']/div[@class='Nplhsf']|.//div[@class='UC8ZCe QS8Cxb']" +
                         "|.//div[contains(@class,'wTrwWd')]|.//div[@class='wSg9Ab StGn8b']");//01-08-2024//07-06-2023//25-04-2023//16-03-2023
                     if (link != null)
@@ -2205,7 +2205,7 @@ namespace TrendingMobileSingleThread
                 if (nds == null)
                     nds = node.SelectNodes(".//g-card-section[contains(@class,'jDsVJf')]/a"); //27-10-2021
                 if (nds == null)
-                    nds = node.SelectNodes(".//div[@class='agqCtf tw-res']/g-card-section/a");//24-04-2024
+                    nds = node.SelectNodes(".//div[@class='agqCtf tw-res']/g-card-section/a|.//a[@class='WlydOe eR6uYd']");//11-09-2024//24-04-2024
                 if (nds == null)
                     nds = node.SelectNodes(".//div[@class='agqCtf tw-res']/g-image-section/a");//03-04-2024
                 if (nds != null)
@@ -3006,7 +3006,7 @@ namespace TrendingMobileSingleThread
                             ts = false;
                     }
                 }
-                if (ts && node.SelectSingleNode(".//div[@class='imso-ml-c PZPZlf']|.//div[@class='Bv2VAe']|.//div[@jsname='GDPwke']|.//div[@id='imso-root']|.//table[@class='vk_tbl Uekwlc']|.//div[@jscontroller='WoVPie']|.//div[@class='j2cNYe']") == null)//20-07-2024//07-05-2024//22-04-2024//04-04-2024//02-04-2024
+                if (ts && node.SelectSingleNode(".//div[@class='imso-ml-c PZPZlf']|.//div[@class='Bv2VAe']|.//div[@jsname='GDPwke']|.//div[@id='imso-root']|.//table[@class='vk_tbl Uekwlc']|.//div[@jscontroller='WoVPie']|.//div[@class='j2cNYe']|.//div[@class='agqCtf tw-res']") == null)//11-09-2024//20-07-2024//07-05-2024//22-04-2024//04-04-2024//02-04-2024
                     return "Topstories";
             }
             if (node.SelectSingleNode(".//div[contains(@class, 'RPdfze')]") != null || (node.SelectSingleNode(".//div[contains(@class, 'Qkn3ie')]" +//26-04-2024//23-04-2024
@@ -3244,7 +3244,7 @@ namespace TrendingMobileSingleThread
                     return "Maps";
             }
 
-            nd = node.SelectSingleNode(".//div[@class='JVrfPc']");
+            nd = node.SelectSingleNode(".//div[@class='JVrfPc']|.//div[@class='lU8tTd EyBRub']");//11-09-2024
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[@class='bUNBRd mnr-c']|.//div[@class='HnYYW i8lZMc']|.//div[@class='HnYYW mfMhoc']|.//div[@class='HnYYW']/div|.//div[@class='HnYYW DFkChc']");//04-07-2023//20-11-2020 twiter classic links//26-06-2020 //13-03-2020 //include on 2019-06-24
             if (nd == null)

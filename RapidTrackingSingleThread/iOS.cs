3d50e2d368ce291eb6945a8861778f2b0f33e239
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-09-20 17:53:50 +0100

    updated selectors for missing classic link as video for mobile searches at line number 953 //20-09-2024

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -997,7 +997,7 @@ namespace RapidTrackingSingleThread
                             if (n == null)
                                 n = nd.SelectSingleNode(".//a[@class='C8nzq Tj0U2 BmP5tf amp_r']"); // 29-11-2019
                             if (n == null)
-                                n = nd.SelectSingleNode(".//a[contains(@class,'cz3goc BmP5tf')]");//03-02-2023 //31-05-2022
+                                n = nd.SelectSingleNode(".//a[contains(@class,'cz3goc')]");//20-09-2024//03-02-2023 //31-05-2022
                             if (n == null)
                                 n = nd.SelectSingleNode(".//a[contains(@class,'sXtWJb')]"); //16-12-2020
                             if (n == null)//13-08-2024

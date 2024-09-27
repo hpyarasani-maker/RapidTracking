e96f4a4d5426fd793afd43c024680138e3a30f30
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-09-27 03:10:39 +0100

    UPdated Desktop 27-09-2024

--- RapidTrackingReceiving/Desktop.cs
+++ RapidTrackingReceiving/Desktop.cs
@@ -2035,7 +2035,7 @@ namespace Oxylabs_BulkKeywords
             if (nd == null)
                 nd = node.SelectSingleNode(".//div[@class='CEMjEf NUnG9d']/g-img"); //06-04-2022 TS
             if (nd == null)//27-04-2023
-                nd = node.SelectSingleNode(".//div[@class='aUSklf']");//27-04-2023
+                nd = node.SelectSingleNode(".//div[@class='aUSklf']|.//div[@jsname='K9a4Re']");//25-09-2024
             if (nd != null)
                 return "topstories";
             if (nd == null)

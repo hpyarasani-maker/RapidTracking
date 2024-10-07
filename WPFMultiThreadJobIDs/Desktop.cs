ab3d802eed54ee05045b0069125aa80aaee66a32
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-10-07 15:13:20 +0100

    updated Desktop ProcessDocument line number 53 //07-10-2024

--- WPFMultiThreadJobIDs/Desktop.cs
+++ WPFMultiThreadJobIDs/Desktop.cs
@@ -47,7 +47,7 @@ namespace WPFMultiThreadJobIDs
             if (nodeCol == null || (nodeCol.Count >= 1 && nodeCol.Count <= 5))//09-02-2024
             {//08-02-2024
                 if (rso.SelectNodes("//div[contains(@id,'kp-wp-tab-')]") != null)
-                    nodeCol = rso.SelectNodes(".//div[contains(@class,'TzHB6b cLjAic')]|.//div[contains(@class,'g Ww4FFb')]|.//div[@class='g PmEWq']|.//div[@class='Wt5Tfe']|.//div[@class='g']/div[@class='BYM4Nd']|.//div[@class='Lv2Cle']|.//g-section-with-header[@class='yG4QQe TBC9ub']");//03-10-2024//22-07-2024//20-07-2024//09-07-2024
+                    nodeCol = rso.SelectNodes(".//div[contains(@class,'TzHB6b cLjAic')]|.//div[contains(@class,'TzHB6b j8lBAb')]|.//div[contains(@class,'g Ww4FFb')]|.//div[@class='g PmEWq']|.//div[@class='Wt5Tfe']|.//div[@class='g']/div[@class='BYM4Nd']|.//div[@class='Lv2Cle']|.//g-section-with-header[@class='yG4QQe TBC9ub']");//07-10-2024//03-10-2024//22-07-2024//20-07-2024//09-07-2024
                 else
                     nodeCol = rso.SelectNodes(".//div[contains(@class,'WvKfwe')]/div|.//div[@class='UDZeY OTFaAf']/div|.//div[@class='MjjYud']|.//div[contains(@class,'g Ww4FFb')]");
             }//08-02-2024

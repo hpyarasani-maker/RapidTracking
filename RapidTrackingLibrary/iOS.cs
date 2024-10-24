9f5e8e7cc0958319c09f40b0a1adfadfe4d94549
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-10-24 19:09:51 +0100

    updated

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -178,6 +178,8 @@ namespace RapidTrackingLibrary
                             {
                                 if (s.StartsWith("<block type=\"popularProducts\"") && sb.ToString().Contains(s))
                                     s = string.Empty;
+                                if (s.StartsWith("<block type=\"peopleAlsoSearch\"") && sb.ToString().Contains(s))//24-10-2024
+                                    s = string.Empty;//24-10-2024
                             }//29-07-2024
                             ndText += s;
                             if (s.Length > 0)

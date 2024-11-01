68d2fa8456f41e230175600ada5468e4b4276ed4
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-11-01 16:09:37 +0000

    updated selectors for missing PeopleAlsoSearch block for desktop searches at line number 308 and 1160 //01-11-2024

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -334,7 +334,7 @@ namespace RapidTrackingLibrary
                     if (node.SelectSingleNode(".//div[contains(@class,'T6zPgb')]/div[@role='heading']") != null)
                     {
                         s.Append("<block type=\"peopleAlsoSearch\" >");
-                        HtmlNodeCollection nc = node.SelectNodes(".//div[@class='oatEtb']/span");
+                        HtmlNodeCollection nc = node.SelectNodes(".//div[@class='oatEtb']/span|.//div[@class='oatEtb']/div/span");//01-11-2024
                         if (nc != null)
                             foreach (HtmlNode n in nc)
                             {
@@ -1233,7 +1233,7 @@ namespace RapidTrackingLibrary
                 if (nd.SelectSingleNode(".//div[contains(@class,'T6zPgb')]/div[@role='heading']") != null)
                 {
                     s.Append("<block type=\"peopleAlsoSearch\" >");
-                    HtmlNodeCollection nc = nd.SelectNodes(".//div[@class='oatEtb']/span");
+                    HtmlNodeCollection nc = node.SelectNodes(".//div[@class='oatEtb']/span|.//div[@class='oatEtb']/div/span");//01-11-2024
                     if (nc != null)
                         foreach (HtmlNode n in nc)
                         {

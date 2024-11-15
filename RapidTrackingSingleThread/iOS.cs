acb1d5daec70df784a47af8b3c69a992a73c7611
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-11-15 15:36:44 +0000

    updated selector for wrong image block for mobile searches at line number 3569 //15-11-2024

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -3566,7 +3566,7 @@ namespace RapidTrackingSingleThread
                 nd = node.SelectSingleNode(".//div[@class='Vvrpbd']");//05-12-2023
             if (nd != null)
             {
-                if (node.SelectSingleNode(".//div[@class='VPyzge']|.//div[@class='aJegcc']|.//div[@jsname='Sicp7d']") == null && (node.SelectSingleNode(".//div[@class='kno-fiu kno-liu']" +//10-06-2024 //04-04-2024
+                if (node.SelectSingleNode(".//div[@class='VPyzge']|.//div[@class='aJegcc']|.//div[@jsname='Sicp7d']|.//div[@class='YB4h9 ky4hfd']") == null && (node.SelectSingleNode(".//div[@class='kno-fiu kno-liu']" +//15-11-2024//10-06-2024 //04-04-2024
                     "|.//div[@class='N60dNb mfMhoc']|.//div[@jsmodel='vqHyhf']|.//div[@jsmodel='Wn3aEc']|.//div[@class='HdBr8']|.//div[@class='VKHL9c']" +
                     "|.//div[@class='IZE3Td']/div[@id='iur']|.//div[@class='IZE3Td']/div[@jscontroller='gOTY1']") != null))//29-07-2024//23-07-2024//02-02-2024//01-02-2024//15-12-2023
                     return "Images";

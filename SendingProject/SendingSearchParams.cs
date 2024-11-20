3ee8cc7b59c4796e0db549bdc41ca2f86c0b35b7
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-11-20 17:20:09 +0000

    Updated SearchParams //20-11-2024

--- SendingProject/SendingSearchParams.cs
+++ SendingProject/SendingSearchParams.cs
@@ -4178,6 +4178,22 @@ new SearchProperties() //01-05-2024 from seid 807 to 896
             {
                 seid =1023, domain ="co.mz", geo_location = "Mozambique", locale= "en-mz", uule = "w+CAIQICIKTW96YW1iaXF1ZQ==",device="mobile_android"
             },//06-11-2024
+            new SearchProperties()//20-11-2024
+            {
+                seid =1024, domain ="com.co", geo_location="Colombia", locale= "es-co", uule = "w+CAIQICIIQ29sb21iaWE",device="desktop"
+            },
+            new SearchProperties()
+            {
+                seid =1025, domain ="com.co", geo_location="Colombia", locale= "es-co", uule = "w+CAIQICIIQ29sb21iaWE",device="mobile_android"
+            },//20-11-2024
+            new SearchProperties()//duplicate of 1024 and 1025 for AIOverview Testing//20-11-2024
+            {
+                seid =1026, domain ="com.co", geo_location="Colombia", locale= "es-co", uule = "w+CAIQICIIQ29sb21iaWE",device="desktop"
+            },
+            new SearchProperties()
+            {
+                seid =1027, domain ="com.co", geo_location="Colombia", locale= "es-co", uule = "w+CAIQICIIQ29sb21iaWE",device="mobile_android"
+            },//duplicate of 1024 and 1025 for AIOverview Testing//20-11-2024
         };
     }
 

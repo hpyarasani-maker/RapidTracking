c1e184b48dc71a8d81af809984727dc5c0f899c3
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-11-21 19:50:04 +0000

    Updated

--- RapidMissingJobsReceiving/SearchParams.cs
+++ RapidMissingJobsReceiving/SearchParams.cs
@@ -3827,22 +3827,6 @@ namespace RapidMissingJobsReceiving
             {
                 seid =1025, domain ="com.co", geo_location="Colombia", locale= "es-co", uule = "w+CAIQICIIQ29sb21iaWE",device="mobile_android"
             },//20-11-2024
-            new SearchProperties()//Seid's for AI overview testing  1026 to 1029
-            {
-                seid =1026, domain ="co.uk",geo_location="United Kingdom",locale= "en-gb",uule = "w+CAIQICIOVW5pdGVkIEtpbmdkb20=",device="desktop"
-            },
-            new SearchProperties()
-            {
-                seid =1027, domain ="co.uk", geo_location="United Kingdom", locale = "en-gb", uule = "w+CAIQICIOVW5pdGVkIEtpbmdkb20=",device="mobile_android"
-            },
-            new SearchProperties()
-            {
-                seid =1028, domain ="com", geo_location="United States", locale="en-us", uule="w+CAIQICINVW5pdGVkIFN0YXRlcw==", device="desktop"
-            },
-            new SearchProperties()
-            {
-                seid =1029, domain ="com", geo_location="United States", locale = "en-us", uule = "w+CAIQICINVW5pdGVkIFN0YXRlcw==",device="mobile_android"
-            },//Seid's for AI overview testing 1026 to 1029
         };
     }
     class SearchProperties

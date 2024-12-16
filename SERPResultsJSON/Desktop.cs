4360d51109aa8f2ca763819da5c6278cb0d7b4f1
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-12-16 17:40:57 +0000

    Updated Desktop //16-12-2024

--- SERPResultsJSON/Desktop.cs
+++ SERPResultsJSON/Desktop.cs
@@ -526,7 +526,7 @@ namespace SERPResultsJSON
                 }
             }
             //AIOverview block //18-11-2024
-            HtmlNode colt = doc.DocumentNode.SelectSingleNode(".//div[@class='M8OgIe']|.//div[@class='GcKpu']");//13-11-2024//08-11-2024//03-09-2024//21-08-2024 AIOverview
+            HtmlNode colt = doc.DocumentNode.SelectSingleNode(".//div[contains(@class,'M8OgIe')]|.//div[@class='GcKpu']");//12-12-2024//13-11-2024//08-11-2024//03-09-2024//21-08-2024 AIOverview
             if (colt != null && doc.DocumentNode.SelectSingleNode(".//div[contains(@class,'Fzsovc')]") != null)//13-11-2024
             {
                 s.Append(GetAioverview(colt));
@@ -1409,7 +1409,7 @@ namespace SERPResultsJSON
                         s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(titles[x++]) + "\" />");//22-02-2022
                 }
             }
-            string pattern1 = @"\\x22\\x3e\\x3ca jsname\\x3d\\x22UWckNb\\x22[ class\\x3d\\x22zReHs\\x22]* href\\x3d\\x22(.*?)\\x22*[ data-jsarwt\\x3d\\x221\\x22]";//26-11-2024//08-09-2023
+            string pattern1 = @"\\x22\\x3e\\x3ca jsname\\x3d\\x22UWckNb\\x22[ class\\x3d\\x22zReHs\\x22]*[ class\\x3d\\x22VfSr4c\\x22]* href\\x3d\\x22(.*?)\\x22*[ data-jsarwt\\x3d\\x221\\x22]";//16-12-2024//26-11-2024//08-09-2023
             re = new Regex(pattern1, RegexOptions.IgnoreCase | RegexOptions.Singleline);
             mc = re.Matches(html);
             foreach (Match m in mc)
@@ -1557,7 +1557,7 @@ namespace SERPResultsJSON
                 if (nds == null)
                     nds = node.SelectNodes(".//g-inner-card/div/div/div[1]/a[1]");  //02-06-2020
                 if (nds == null)
-                    nds = node.SelectNodes(".//a[@class='h4kbcd']"); //27-05-2020 twitterCard item URLs included selector
+                    nds = node.SelectNodes(".//a[contains(@class,'h4kbcd')]");//16-12-2024 //27-05-2020 twitterCard item URLs included selector
                 if (nds != null)
                     foreach (HtmlNode nd in nds)
                     {
@@ -2229,7 +2229,8 @@ namespace SERPResultsJSON
                 nd = node.SelectSingleNode(".//div[contains(@class,'tw-res')]");//21-11-2024
             if (nd != null)
             {
-                return "Twitters";
+                if (node.SelectSingleNode(".//g-scrolling-carousel[@class='m5t0v QwDjz']") == null)//16-12-2024
+                    return "Twitters";
             }
             if (node.SelectSingleNode(".//div[contains(@class, 'RPdfze')]|.//div[contains(@class, 'nJMOzb')]|.//div[contains(@class, 'Qkn3ie')]|.//div[@class='lMMUFc']") != null)//21-07-2023//07-07-2023
                 return "FindResultsOn";//07-07-2023
@@ -2406,7 +2407,7 @@ namespace SERPResultsJSON
             nd = node.SelectSingleNode(".//div[contains(@class, 'bba2i')]");//11-06-2024 DataSet Block
             if (nd != null)
                 return "Dataset";//11-06-2024
-            nd = node.SelectSingleNode(".//div[@class='GcKpu']|.//div[@class='M8OgIe']");//13-11-2024//03-09-2024//21-08-2024 AIOverview
+            nd = node.SelectSingleNode(".//div[@class='GcKpu']|.//div[contains(@class,'M8OgIe')]");//13-11-2024//03-09-2024//21-08-2024 AIOverview
             if (nd != null && node.SelectSingleNode(".//div[contains(@class,'Fzsovc')]") != null)//13-11-2024
                 return "aiOverview";//21-08-2024 AIOverview
             nd = node.SelectSingleNode(".//div[@class='oIk2Cb']");//14-10-2024 //11-10-2024  peoplealsosearch
@@ -2476,7 +2477,7 @@ namespace SERPResultsJSON
             {
                 try
                 {
-                    if (node.SelectSingleNode(".//div[@class='Brgz0 tw-res']|.//div[@class='k9uN1c kfn9hb']|.//div[@class='baPFxb g kSMK2']") != null
+                    if (node.SelectSingleNode(".//div[contains(@class,'tw-res')]|.//div[@class='k9uN1c kfn9hb']|.//div[@class='baPFxb g kSMK2']") != null//16-12-2024
                         && node.SelectNodes(".//div[contains(@class,'g Ww4FFb')]") == null) return true;//24-11-2023//26-10-2023//04-01-2023//16-12-2022
                     if (node.SelectSingleNode(".//div[@class='g jNVrwc Y4pkMc']|.//div[@class='g tF2Cxc']|.//div[@class='g eejeod up9jud']" +
                     //"|.//div[@class='g Ww4FFb tF2Cxc']") != null) return false; //21-07-2022//15-02-2022//02-02-2022//31-12-2021 missing CLinks
@@ -2541,7 +2542,7 @@ namespace SERPResultsJSON
                 if (node.SelectSingleNode(".//div[contains(@class, 'RPdfze')]|.//div[contains(@class, 'nJMOzb')]" +
                     "|.//div[contains(@class, 'Qkn3ie')]|.//div[contains(@class, 'vdQmEd')]|.//div[@class='lMMUFc']" +
                     "|.//div[@class='wH6SXe']|.//div[contains(@class,'WlTAzf')]|.//div[@class='cj1ht QkBAO oYQBg']" +
-                    "|.//div[@class='oIk2Cb']|.//div[@jsmodel='Wn3aEc']") != null && node.SelectSingleNode(".//div[contains(@class,'g')]") == null)//10-12-2024//09-12-2024//04-12-2024//14-10-2024
+                    "|.//div[@class='oIk2Cb']|.//div[@jsmodel='Wn3aEc']") != null && node.SelectSingleNode(".//div[contains(@class,'g ')]") == null)//16-12-2024//10-12-2024//09-12-2024//04-12-2024//14-10-2024
                     return true;//29-06-2023
                 // changes in map block on 19-06-2019.
                 nd = node.SelectSingleNode(".//g-img/img");

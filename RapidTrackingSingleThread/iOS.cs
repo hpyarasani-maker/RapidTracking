0b1a7240c124725426f93829270b9cebe92c08bf
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2024-12-23 17:07:21 +0000

    updated selector for missing PeopleAlsoAsk Item URLs for the mobile searches at line number 2206 //23-12-2024

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2203,7 +2203,7 @@ namespace RapidTrackingSingleThread
                 }
             }
             catch { }
-            string pattern1 = @"\\x22\\x3e\\x3ca jsname\\x3d\\x22UWckNb\\x22 class\\x3d\\x22sXtWJb\\x22 href\\x3d\\x22(.*?)\\x22";//21-09-2023 //11-09-2023
+            string pattern1 = @"\\x22\\x3e\\x3ca [jsname\\x3d\\x22UWckNb\\x22 class\\x3d\\x22sXtWJb\\x22]*[ class\\x3d\\x22ddkIM rz5jw c30Ztd\\x22]* href\\x3d\\x22(.*?)\\x22";//23-12-2024//21-09-2023 //11-09-2023
             re = new Regex(pattern1, RegexOptions.IgnoreCase | RegexOptions.Singleline);
             mc = re.Matches(html);
             try

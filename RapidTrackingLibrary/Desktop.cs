ca5ba09a8cca574c461443122d597e73630e3047
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-01-29 15:42:27 +0000

    updated selector for missing flight block for desktop searches at line number 2608 //29-01-2025

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -2641,7 +2641,7 @@ namespace RapidTrackingLibrary
                         return false;//10-12-2021
                     else //10-12-2021
                         return true;
-                if (node.SelectSingleNode(".//div[contains(@class,'WlTAzf')]|.//div[@class='wYpZje']") != null)//08-07-2024
+                if (node.SelectSingleNode(".//div[contains(@class,'WlTAzf')]|.//div[@class='wYpZje']|.//div[@class='cj1ht QkBAO oYQBg']") != null)//29-01-2025//08-07-2024
                     return true;//24-11-2023
                 HtmlNodeCollection nds = node.SelectNodes(".//div");
                 if (nds != null)
--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -2605,7 +2605,7 @@ namespace RapidTrackingSingleThread
                         return false;//10-12-2021
                     else //10-12-2021
                         return true;
-                if (node.SelectSingleNode(".//div[contains(@class,'WlTAzf')]|.//div[@class='wYpZje']") != null)//08-07-2024
+                if (node.SelectSingleNode(".//div[contains(@class,'WlTAzf')]|.//div[@class='wYpZje']|.//div[@class='cj1ht QkBAO oYQBg']") != null)//29-01-2025//08-07-2024
                     return true;
                 HtmlNodeCollection nds = node.SelectNodes(".//div");
                 if (nds != null)

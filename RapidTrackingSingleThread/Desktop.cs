9a48dfb291f87f2db548af1c77ae597e6687ef69
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-02-18 16:42:28 +0000

    updated selector for missing ClassicLinkSiteLink block for desktop searches at line number 1161 //18-02-2025

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -1158,7 +1158,7 @@ namespace RapidTrackingSingleThread
             if (n != null)
             {
                 HtmlNode t = n.SelectSingleNode(".//h3");
-                HtmlNodeCollection nds = node.SelectNodes(".//div[contains(@class,'HiHjCd')]/a|.//div[@class='Mwdfte']/a|.//div[@jsname='BPrWId']/a|.//div[contains(@class,'JpOecb')]/a|.//div[@class='kb0PBd']/a");//14-02-2025//20-01-2025//21-10-2024
+                HtmlNodeCollection nds = node.SelectNodes(".//div[contains(@class,'HiHjCd')]/a|.//div[@class='Mwdfte']/a|.//div[@jsname='BPrWId']/a|.//div[contains(@class,'JpOecb')]/a|.//div[contains(@class,'kb0PBd')]/a");//18-02-2025//14-02-2025//20-01-2025//21-10-2024
                 if (nds != null)
                 {
                     s.Append("<block type=\"classicLinkSiteLinks\" url=\"" + SetUrl(n.Attributes["href"].Value) + "\" title=\"" + SetTitle(t.InnerText) + "\" >");

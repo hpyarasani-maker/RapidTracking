4974d6c524745d32089f7675aae5ad94437ebf04
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-02-21 17:43:37 +0000

    Revert "updated selector for missing flightpack for desktop searches at line number 2279 //21-02-2025"
    This reverts commit eac33592882d4e263d7397e380c866ea16b8c739.

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -2313,7 +2313,7 @@ namespace RapidTrackingLibrary
             if (nd != null)
                 return "TopSights";*///23-03-2022//19-01-2023
 
-            nd = node.SelectSingleNode(".//div[contains(@class,'WlTAzf')]|.//div[contains(@class, 'vdQmEd')]|.//div[@class='p21Z4']");//21-02-2025//18-02-2025//29-07-2024//29-06-2023//23-03-2022
+            nd = node.SelectSingleNode(".//div[contains(@class,'WlTAzf')]|.//div[contains(@class, 'vdQmEd')]|.//div[@class='cj1ht QkBAO oYQBg']|.//div[@class='p21Z4']");//18-02-2025//29-07-2024//29-06-2023//23-03-2022
             if (nd != null && nd.SelectNodes(".//div[@class='MxQnIc']") == null && node.SelectSingleNode(".//div[@class='XNfAUb']|.//div/h1[contains(@class, 'bNg8Rb')]") == null)//27-12-2024//19-08-2024//30-06-2023
                 return "Flights";//23-03-2022
 

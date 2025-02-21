0db9133e14be8935beb4b8546373ad1c8c5ccc1c
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-02-21 17:41:34 +0000

    updated duration hours regex line number 2183, 2190 and 2203 for desktop searches //21-02-2025

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -2180,14 +2180,14 @@ namespace RapidTrackingSingleThread
                 int min = Convert.ToInt32(match.Groups[4].Value);
                 return (days > 0 || hrs > 0 || min > 0) ? ((days * 24) + hrs + (min / 60.0)).ToString("##.##") : "0.0";
             }
-            match = Regex.Match(hours, @"(\d+)[\s]?[d|T][\W]* (\d+)[\s]?(h|Std\.)");
+            match = Regex.Match(hours, @"(\d+)[\s]?[d|T|\p{L}][\W]* (\d+)[\s]?(h|\p{L}|Std\.)");//21-02-2025
             if (match.Success)
             {
                 int days = Convert.ToInt32(match.Groups[1].Value);
                 int hrs = Convert.ToInt32(match.Groups[2].Value);
                 return (days > 0 || hrs > 0) ? ((days * 24) + hrs) + "." + "0" : "0.0";
             }
-            match = Regex.Match(hours, @"(\d+)[\s]?(h|Std)[\W]* (\d+)[\s]?(m|[M|m]in)");
+            match = Regex.Match(hours, @"(\d+)[\s]?(h|Std|\p{L})[\W]* (\d+)[\s]?(m|\p{L}|[M|m]in)");//21-02-2025
             if (match.Success)
             {
                 int hrs = Convert.ToInt32(match.Groups[1].Value);
@@ -2200,7 +2200,7 @@ namespace RapidTrackingSingleThread
                 int days = Convert.ToInt32(match.Groups[1].Value);
                 return (days > 0) ? (days * 24).ToString("##.##") : "0.0";
             }
-            match = Regex.Match(hours, @"(\d+)[\s]?(h|Std)");
+            match = Regex.Match(hours, @"(\d+)[\s]?(h|Std|\p{L})");//21-02-2025
             if (match.Success)
             {
                 int hrs = Convert.ToInt32(match.Groups[1].Value);

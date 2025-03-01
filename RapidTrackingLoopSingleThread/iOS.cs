c4956661401812a294bb4356a87b490fd502f77a
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-03-01 17:44:26 +0000

    updated IOS //01-03-2025

--- RapidTrackingLoopSingleThread/iOS.cs
+++ RapidTrackingLoopSingleThread/iOS.cs
@@ -2624,7 +2624,7 @@ namespace RapidTrackingLoopSingleThread
 
             nds = node.SelectNodes(".//g-inner-card/a");
             if (nds == null) //14-09-2022 shifted from 1991 2 lines
-                nds = node.SelectNodes(".//div[contains(@class,'amp_re')]/a|.//div[@class='dbsr']/a|.//div[@class='Fq8eSd']/a|.//div[@class='y1Boce']/div/a|.//div[@data-ved]/a|.//div[contains(@class,'JJJtgd')]/a");//28-07-2020   // 18-12-2019//27-07-2020
+                nds = node.SelectNodes(".//div[contains(@class,'amp_re')]/a|.//div[@class='dbsr']/a|.//div[@class='Fq8eSd']/a|.//div[@class='y1Boce']/div/a|.//div[@data-ved]/a|.//div[contains(@class,'JJJtgd')]/a|.//div[@class='x2PRle DvHpd']/a");//27-02-2025//28-07-2020   // 18-12-2019//27-07-2020
             if (nds == null)
                 nds = node.SelectNodes(".//g-inner-card/div/a|.//div[@class='kno-fb-ctx n49mp']/div/a|.//div[@class='zZ9K7e']/a|.//div[contains(@class,'kno-fb-ctx')]/div/a|.//div[contains(@class,'kno-fb-ctx')]/div/div/a|.//g-inner-card/div/div/a");//10-08-2022 TS Item Urls
             if (nds == null)

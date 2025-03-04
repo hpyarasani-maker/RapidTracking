e64bb0aafb15ad895fe81430839a9cbbfd64b16c
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-03-04 17:01:53 +0000

    updated

--- RapidTrackingReceiving/HTMLParserNewTask.cs
+++ RapidTrackingReceiving/HTMLParserNewTask.cs
@@ -180,7 +180,7 @@ namespace Oxylabs_BulkKeywords
                         //SearchProperties sp = SearchParams.searches.Where(s => s.locale == hl && s.device == device && s.geo_location == gl).SingleOrDefault();
                         //seid = sp.seid.ToString();
 
-                        if (device == "desktop")
+                        if (device == "desktop_chrome")
                             result = desktop.ProcessDocument(seid, kw, jobid, response, out orgUrls);//30-10-2024
                         else
                             result = ios.ProcessDocument(seid, kw, jobid, response, out orgUrls);//30-10-2024

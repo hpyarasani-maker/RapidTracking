f955cbed5950fe954c77addb23573c15a3e1ae71
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-03-04 13:48:26 +0000

    updated user and pwd in singleThread forms at line number 548 to 559 //04-02-2025

--- RapidTrackingSingleThread/frmSingleThread.cs
+++ RapidTrackingSingleThread/frmSingleThread.cs
@@ -544,9 +544,19 @@ namespace RapidTrackingSingleThread
             Uri queryUri = new Uri("http://data.oxylabs.io/v1/queries/batch");
             //string username = "gpidatametrics";
             //string password = "sdV5X3fcX6";
-            
-            string username = "piapp";
-            string password = "b5FCvgkjxx";
+
+            string username = string.Empty;//04-02-2025
+            string password = string.Empty;
+            if (sp.device == "mobile_android")
+            {
+                username = "piapp";
+                password = "b5FCvgkjxx";
+            }
+            else if (sp.device == "desktop_chrome")
+            {
+                username = "piapp-aio";
+                password = "4gvfnA+aBYpBNs37";
+            }//04-03-2025
             string authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(username + ":" + password));
             string[] keyword = { sp.query };
 

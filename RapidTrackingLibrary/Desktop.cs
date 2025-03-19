4893171c89b6211dd3a9462050de43cd0a1d6f34
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-03-19 17:43:40 +0000

    updated selector for missing classic link for the desktop searches at line number 952 //19-03-2025

--- RapidTrackingLibrary/Desktop.cs
+++ RapidTrackingLibrary/Desktop.cs
@@ -971,7 +971,7 @@ namespace RapidTrackingLibrary
                 HtmlNodeCollection nc = null; //10-10-2022
                 if (node.SelectSingleNode(".//div[@id='rhs']") != null || node.Attributes["id"]?.Value == "rhs")
                 {
-                    nc = node.SelectNodes(".//div[@class='g']|.//div[contains(@class,'g Ww4FFb')]|.//div[@class='g eejeod up9jud']|.//div[contains(@class,'g dFd2Tb')]" +
+                    nc = node.SelectNodes(".//div[@class='g']|.//div[contains(@class,'g Ww4FFb')]|.//div[@class='g eejeod up9jud']|.//div[contains(@class,'g dFd2Tb')]|.//div[@class='wHYlTd Ww4FFb vt6azd tF2Cxc asEBEc']" + //19-03-2025
                          "|.//div[contains(@class,'g ZYT4Gf')]|.//div[@class='g PmEWq']");//21-11-2023//07-08-2023//26-04-2023//12-10-2022
                     if (nc == null) return string.Empty;
                 }

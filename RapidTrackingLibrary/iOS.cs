2b9a2012dc33cc3e24664e9f03fe15c3c78b208c
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-03-19 16:45:59 +0000

    updated selector for missing topstories for mobile searches at line number 3249 //19-03-2025

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -3293,7 +3293,7 @@ namespace RapidTrackingLibrary
                     }
                 }
                 if (ts && node.SelectSingleNode(".//div[@class='imso-ml-c PZPZlf']|.//div[@class='Bv2VAe']|.//div[@jsname='GDPwke']|.//div[@id='imso-root']" +
-                    "|.//table[@class='vk_tbl Uekwlc']|.//div[@jscontroller='WoVPie']|.//div[@class='j2cNYe']|.//div[@class='agqCtf tw-res']" +
+                    "|.//table[@class='vk_tbl Uekwlc']|.//div[@jscontroller='WoVPie']|.//div[@class='b7hBnb Gp3GQd']|.//div[@class='MBOAae O0UFB']|.//div[@class='j2cNYe']|.//div[@class='agqCtf tw-res']" +//19-03-2025
                     "|.//div[contains(@class,'rsmgO')]") == null || node.SelectSingleNode(".//div[@class='Xeztj WIlpZe xuc']") != null)//24-12-2024//18-12-2024//11-09-2024//20-07-2024//07-05-2024//22-04-2024//04-04-2024//02-04-2024
                     return "Topstories";
             }

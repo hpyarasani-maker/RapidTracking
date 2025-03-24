c2dd28c7209bfa645f1423dd296f82af31e2e9d5
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-03-24 15:08:34 +0000

    updated

--- RapidTrackingSingleThread/SearchParams.cs
+++ RapidTrackingSingleThread/SearchParams.cs
@@ -3456,91 +3456,91 @@ namespace RapidTrackingSingleThread
             },//03-09-2024
             new SearchProperties()//07-10-2024
             {
-                seid=1002, domain="co.uk", geo_location = "Hertford,Hertfordshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICItSGVydGZvcmQsSGVydGZvcmRzaGlyZSxFbmdsYW5kLFVuaXRlZCBLaW5nZG9t", device="desktop_chrome"
+                seid=1002, domain="co.uk", geo_location="Hertford,Hertfordshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICItSGVydGZvcmQsSGVydGZvcmRzaGlyZSxFbmdsYW5kLFVuaXRlZCBLaW5nZG9t", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1003, domain="co.uk", geo_location = "Hertford,Hertfordshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICItSGVydGZvcmQsSGVydGZvcmRzaGlyZSxFbmdsYW5kLFVuaXRlZCBLaW5nZG9t", device="mobile_android"
+                seid=1003, domain="co.uk", geo_location="Hertford,Hertfordshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICItSGVydGZvcmQsSGVydGZvcmRzaGlyZSxFbmdsYW5kLFVuaXRlZCBLaW5nZG9t", device="mobile_android"
             },
             new SearchProperties()
             {
-                seid=1004, domain="co.uk", geo_location = "Redbourne,Lincolnshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICItUmVkYm91cm5lLExpbmNvbG5zaGlyZSxFbmdsYW5kLFVuaXRlZCBLaW5nZG9t", device="desktop_chrome"
+                seid=1004, domain="co.uk", geo_location="Redbourne,Lincolnshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICItUmVkYm91cm5lLExpbmNvbG5zaGlyZSxFbmdsYW5kLFVuaXRlZCBLaW5nZG9t", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1005, domain="co.uk", geo_location = "Redbourne,Lincolnshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICItUmVkYm91cm5lLExpbmNvbG5zaGlyZSxFbmdsYW5kLFVuaXRlZCBLaW5nZG9t", device="mobile_android"
+                seid=1005, domain="co.uk", geo_location="Redbourne,Lincolnshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICItUmVkYm91cm5lLExpbmNvbG5zaGlyZSxFbmdsYW5kLFVuaXRlZCBLaW5nZG9t", device="mobile_android"
             },
             new SearchProperties()
             {
-                seid=1006, domain="co.uk", geo_location = "Welwyn Garden City,Hertfordshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICI3V2Vsd3luIEdhcmRlbiBDaXR5LEhlcnRmb3Jkc2hpcmUsRW5nbGFuZCxVbml0ZWQgS2luZ2RvbQ==", device="desktop_chrome"
+                seid=1006, domain="co.uk", geo_location="Welwyn Garden City,Hertfordshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICI3V2Vsd3luIEdhcmRlbiBDaXR5LEhlcnRmb3Jkc2hpcmUsRW5nbGFuZCxVbml0ZWQgS2luZ2RvbQ==", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1007, domain="co.uk", geo_location = "Welwyn Garden City,Hertfordshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICI3V2Vsd3luIEdhcmRlbiBDaXR5LEhlcnRmb3Jkc2hpcmUsRW5nbGFuZCxVbml0ZWQgS2luZ2RvbQ==", device="mobile_android"
+                seid=1007, domain="co.uk", geo_location="Welwyn Garden City,Hertfordshire,England,United Kingdom", locale="en-gb", uule="w+CAIQICI3V2Vsd3luIEdhcmRlbiBDaXR5LEhlcnRmb3Jkc2hpcmUsRW5nbGFuZCxVbml0ZWQgS2luZ2RvbQ==", device="mobile_android"
             },//07-10-2024
             new SearchProperties()//06-11-2024
             {
-                seid=1008, domain="cd", geo_location = "Democratic Republic of the Congo", locale="fr-cd", uule="w+CAIQICIgRGVtb2NyYXRpYyBSZXB1YmxpYyBvZiB0aGUgQ29uZ28=", device="desktop_chrome"
+                seid=1008, domain="cd", geo_location="Democratic Republic of the Congo", locale="fr-cd", uule="w+CAIQICIgRGVtb2NyYXRpYyBSZXB1YmxpYyBvZiB0aGUgQ29uZ28=", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1009, domain="cd", geo_location = "Democratic Republic of the Congo", locale="fr-cd", uule="w+CAIQICIgRGVtb2NyYXRpYyBSZXB1YmxpYyBvZiB0aGUgQ29uZ28=", device="mobile_android"
+                seid=1009, domain="cd", geo_location="Democratic Republic of the Congo", locale="fr-cd", uule="w+CAIQICIgRGVtb2NyYXRpYyBSZXB1YmxpYyBvZiB0aGUgQ29uZ28=", device="mobile_android"
             },
             new SearchProperties()
             {
-                seid=1010, domain="cd", geo_location = "Democratic Republic of the Congo", locale="en-cd", uule="w+CAIQICIgRGVtb2NyYXRpYyBSZXB1YmxpYyBvZiB0aGUgQ29uZ28=", device="desktop_chrome"
+                seid=1010, domain="cd", geo_location="Democratic Republic of the Congo", locale="en-cd", uule="w+CAIQICIgRGVtb2NyYXRpYyBSZXB1YmxpYyBvZiB0aGUgQ29uZ28=", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1011, domain="cd", geo_location = "Democratic Republic of the Congo", locale="en-cd", uule="w+CAIQICIgRGVtb2NyYXRpYyBSZXB1YmxpYyBvZiB0aGUgQ29uZ28=", device="mobile_android"
+                seid=1011, domain="cd", geo_location="Democratic Republic of the Congo", locale="en-cd", uule="w+CAIQICIgRGVtb2NyYXRpYyBSZXB1YmxpYyBvZiB0aGUgQ29uZ28=", device="mobile_android"
             },
             new SearchProperties()
             {
-                seid=1012, domain="com.et", geo_location = "Ethiopia", locale="am-et", uule="w+CAIQICIIRXRoaW9waWE=", device="desktop_chrome"
+                seid=1012, domain="com.et", geo_location="Ethiopia", locale="am-et", uule="w+CAIQICIIRXRoaW9waWE=", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1013, domain="com.et", geo_location = "Ethiopia", locale="am-et", uule="w+CAIQICIIRXRoaW9waWE=", device="mobile_android"
+                seid=1013, domain="com.et", geo_location="Ethiopia", locale="am-et", uule="w+CAIQICIIRXRoaW9waWE=", device="mobile_android"
             },
             new SearchProperties()
             {
-                seid=1014, domain="com.et", geo_location = "Ethiopia", locale="en-et", uule="w+CAIQICIIRXRoaW9waWE=", device="desktop_chrome"
+                seid=1014, domain="com.et", geo_location="Ethiopia", locale="en-et", uule="w+CAIQICIIRXRoaW9waWE=", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1015, domain="com.et", geo_location = "Ethiopia", locale="en-et", uule="w+CAIQICIIRXRoaW9waWE=", device="mobile_android"
+                seid=1015, domain="com.et", geo_location="Ethiopia", locale="en-et", uule="w+CAIQICIIRXRoaW9waWE=", device="mobile_android"
             },
             new SearchProperties()
             {
-                seid=1016, domain="co.ls", geo_location = "Lesotho", locale="st-ls", uule="w+CAIQICIHTGVzb3Robw==", device="desktop_chrome"
+                seid=1016, domain="co.ls", geo_location="Lesotho", locale="st-ls", uule="w+CAIQICIHTGVzb3Robw==", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1017, domain="co.ls", geo_location = "Lesotho", locale="st-ls", uule="w+CAIQICIHTGVzb3Robw==", device="mobile_android"
+                seid=1017, domain="co.ls", geo_location="Lesotho", locale="st-ls", uule="w+CAIQICIHTGVzb3Robw==", device="mobile_android"
             },
             new SearchProperties()
             {
-                seid=1018, domain="co.ls", geo_location = "Lesotho", locale="en-ls", uule="w+CAIQICIHTGVzb3Robw==", device="desktop_chrome"
+                seid=1018, domain="co.ls", geo_location="Lesotho", locale="en-ls", uule="w+CAIQICIHTGVzb3Robw==", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1019, domain="co.ls", geo_location = "Lesotho", locale="en-ls", uule="w+CAIQICIHTGVzb3Robw==", device="mobile_android"
+                seid=1019, domain="co.ls", geo_location="Lesotho", locale="en-ls", uule="w+CAIQICIHTGVzb3Robw==", device="mobile_android"
             },
             new SearchProperties()
             {
-                seid=1020, domain="co.mz", geo_location = "Mozambique", locale="pt-mz", uule="w+CAIQICIKTW96YW1iaXF1ZQ==", device="desktop_chrome"
+                seid=1020, domain="co.mz", geo_location="Mozambique", locale="pt-mz", uule="w+CAIQICIKTW96YW1iaXF1ZQ==", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1021, domain="co.mz", geo_location = "Mozambique", locale="pt-mz", uule="w+CAIQICIKTW96YW1iaXF1ZQ==", device="mobile_android"
+                seid=1021, domain="co.mz", geo_location="Mozambique", locale="pt-mz", uule="w+CAIQICIKTW96YW1iaXF1ZQ==", device="mobile_android"
             },
             new SearchProperties()
             {
-                seid=1022, domain="co.mz", geo_location = "Mozambique", locale="en-mz", uule="w+CAIQICIKTW96YW1iaXF1ZQ==", device="desktop_chrome"
+                seid=1022, domain="co.mz", geo_location="Mozambique", locale="en-mz", uule="w+CAIQICIKTW96YW1iaXF1ZQ==", device="desktop_chrome"
             },
             new SearchProperties()
             {
-                seid=1023, domain="co.mz", geo_location = "Mozambique", locale="en-mz", uule="w+CAIQICIKTW96YW1iaXF1ZQ==", device="mobile_android"
+                seid=1023, domain="co.mz", geo_location="Mozambique", locale="en-mz", uule="w+CAIQICIKTW96YW1iaXF1ZQ==", device="mobile_android"
             },//06-11-2024
             new SearchProperties()//20-11-2024
             {

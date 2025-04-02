c7f27e42a3408a76182d152e6eee9f1990888b31
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-04-02 20:00:04 +0100

    updated selectors for missing SiteLinks, ClassLinksSiteLinks and ClassicLinkCarousel blocks for the mobiles searches at line number 56 and 892 //02-04-2025

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -51,7 +51,7 @@ namespace RapidTrackingLibrary
                     "|//div[contains(@class,'wHYlTd Ww4FFb vt6azd')]|//div[contains(@class,'Lv2Cle Ww4FFb vt6azd')]|//div[contains(@class,'Ww4FFb vt6azd xpd')]" +
                     "|//div[contains(@class,'Ww4FFb vt6azd oGMpge')]|//div[contains(@class,'Ww4FFb vt6azd tRkSqb')]|//div[contains(@class,'Ww4FFb vt6azd F6CFcc')]" +
                    "|//div[@class='lU8tTd']|//g-card[@class='g F6CFcc']|.//div[@class='Ww4FFb vt6azd g']|.//div[@class='vtSz8d Ww4FFb vt6azd']" +
-                   "|.//div[@class='AGopnf']|.//div[@class='Ww4FFb vt6azd']|.//div[contains(@class,'WlTAzf Ww4FFb vt6azd')]");//27-03-2025//01-01-2025//28-11-2024//26-09-2024//10-09-2024//09-05-2024//12-03-2024
+                   "|.//div[@class='AGopnf']|.//div[@class='Ww4FFb vt6azd']|.//div[contains(@class,'WlTAzf Ww4FFb vt6azd')]|.//div[@class='vt6azd Ww4FFb']|.//div[@class='grvXqb fZ46zc']|.//g-card[@class='XqIXXe']");//02-04-2025//27-03-2025//01-01-2025//28-11-2024//26-09-2024//10-09-2024//09-05-2024//12-03-2024
                 if (nodeCol == null)
                     nodeCol = doc.DocumentNode.SelectNodes("//*[@id='tscffb']");
 
@@ -931,7 +931,7 @@ namespace RapidTrackingLibrary
                     nds = node.SelectNodes(".");//14-02-2024//21-09-2023 //02-02-2022 moved from 687 line   // 25-10-2019
                 if (nds == null)//30-09-2024
                     nds = node.SelectNodes(".//div[@class='AGopnf']|.//div[@class='ycw3p']");//12-03-2025//30-09-2024
-                if (nds != null && nds.Count == 1 && (node.Attributes["class"]?.Value == "Ww4FFb vt6azd" || node.Attributes["class"]?.Value == "g Ww4FFb vt6azd"))//07-01-2025//21-10-2024
+                if (nds != null && nds.Count == 1 && (node.Attributes["class"]?.Value == "Ww4FFb vt6azd" || node.Attributes["class"]?.Value == "g Ww4FFb vt6azd" || node.Attributes["class"]?.Value == "Ww4FFb vt6azd xpd EtOod pkphOe" || node.Attributes["class"]?.Value == "XqIXXe"))//02-04-2025//07-01-2025//21-10-2024
                     nds = node.SelectNodes(".");//21-10-2024
             }
             if (nds != null)

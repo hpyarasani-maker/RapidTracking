79fc6950b16197daa2484d90f5130908a75a2d81
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-04-04 17:15:54 +0100

    updated selectors for missing videos block creator names for mobile searches at line number 2768 //04-03-2025

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -2765,7 +2765,7 @@ namespace RapidTrackingSingleThread
                         //end 26-06-2020
                         if (url.Contains("/search?") || url.StartsWith("#")) url = "";//11-04-2022
                         string creator = nd.SelectSingleNode(".//div[contains(@class,'YDQ8Te Fy8FNd OSrXXb')]/span[3]|.//div[@class='pDdfmc YqCW4e OSrXXb']/span[3]" +//19-11-2024
-                            "|.//span[@class='GbZYR OSrXXb']|.//div[contains(@class,'R8BTeb q8U8x')]|.//div[@class='SPa6uf Hqu6dd OSrXXb CUQOj']/span[3]")?.InnerText.Trim() ?? "";//12-02-2025//01-10-2024//12-08-2024//22-07-2024//04-12-2023
+                            "|.//span[@class='GbZYR OSrXXb']|.//div[contains(@class,'R8BTeb q8U8x')]|.//div[contains(@class,'SPa6uf Hqu6dd OSrXXb')]/span[3]")?.InnerText.Trim() ?? "";//04-03-2025//12-02-2025//01-10-2024//12-08-2024//22-07-2024//04-12-2023
                         if (!string.IsNullOrEmpty(url.Trim()) || !string.IsNullOrEmpty(title.Trim())) //11-04-2022
                                                                                                       //s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" />");
                             s.Append("<item url=\"" + SetUrl(url) + "\" title=\"" + SetTitle(title) + "\" creatorName=\"" + SetTitle(creator) + "\" />");//04-12-2023

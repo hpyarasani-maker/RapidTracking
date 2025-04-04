65bafe342935b290665e7c46d15fbc3b1a895ab7
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-04-04 18:22:53 +0100

    updated

--- RapidTrackingLoopSingleThread/Desktop.cs
+++ RapidTrackingLoopSingleThread/Desktop.cs
@@ -49,8 +49,10 @@ namespace RapidTrackingLoopSingleThread
                 if (nodeCol == null || (nodeCol.Count >= 1 && nodeCol.Count <= 5))//09-02-2024
                 {//08-02-2024
                     if (rso.SelectNodes("//div[contains(@id,'kp-wp-tab-')]") != null)
-                        nodeCol = rso.SelectNodes(".//div[contains(@class,'TzHB6b cLjAic')]|.//div[contains(@class,'TzHB6b j8lBAb p7kDMc')]|.//div[contains(@class,'g Ww4FFb')]|.//div[@class='g PmEWq']|.//div[@class='Wt5Tfe']" +
-                        "|.//div[@class='g']/div[@class='BYM4Nd']|.//div[@class='Lv2Cle']|.//g-section-with-header[contains(@class,'yG4QQe TBC9ub')]|.//div[@class='uVMCKf']|.//div[@class='vtSz8d']|.//div[@class='wHYlTd Ww4FFb vt6azd tF2Cxc asEBEc']|.//div[contains(@class,'PmEWq wHYlTd')]");//21-03-2025//18-02-2025//14-02-2025//31-12-2024//09-10-2024
+                        nodeCol = rso.SelectNodes(".//div[contains(@class,'TzHB6b cLjAic')]|.//div[contains(@class,'TzHB6b j8lBAb p7kDMc')]|.//div[contains(@class,'g Ww4FFb')]|.//div[@class='g PmEWq']" +
+                            "|.//div[@class='Wt5Tfe']|.//div[@class='vt6azd Ww4FFb']|.//div[@class='xfX4Ac JI5uCe qB9BY yWNJXb qzPQNd']|.//div[@class='oIk2Cb']|.//div[@class='yi8zHf']/div[@id='tadsb']|.//div[@jscontroller='uMC5pc']" +//03-04-2025
+                            "|.//div[@class='g']/div[@class='BYM4Nd']|.//div[@class='Lv2Cle']|.//g-section-with-header[contains(@class,'yG4QQe TBC9ub')]|.//div[@class='uVMCKf']|.//div[@class='vtSz8d']" +
+                            "|.//div[@class='wHYlTd Ww4FFb vt6azd tF2Cxc asEBEc']|.//div[contains(@class,'PmEWq wHYlTd')]|.//div[@id='knowledge-finance-wholepage__entity-summary']");//03-04-2025//21-03-2025//18-02-2025//14-02-2025//31-12-2024//09-10-2024
                     else
                         nodeCol = rso.SelectNodes(".//div[contains(@class,'WvKfwe')]/div|.//div[@class='UDZeY OTFaAf']/div|.//div[@class='MjjYud']|.//div[contains(@class,'g Ww4FFb')]");
                 }//08-02-2024

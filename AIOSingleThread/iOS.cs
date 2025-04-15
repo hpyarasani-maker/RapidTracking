288b36ef53622031fc0f34b3dad96bba5f409995
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-04-15 18:35:27 +0100

    updated selector for missing AI Block content for mobile searches at line number 3949 //15-04-2025

--- AIOSingleThread/iOS.cs
+++ AIOSingleThread/iOS.cs
@@ -3946,7 +3946,7 @@ namespace AIOSingleThread
             {
                 foreach (HtmlNode nd in nodes)
                 {
-                    HtmlNodeCollection ls = nd.SelectNodes(".//div[@class='RJPOee EIJn2']/ul/li|.//div[@class='RJPOee EIJn2']/div");//05-04-2025
+                    HtmlNodeCollection ls = nd.SelectNodes(".//div[@class='RJPOee EIJn2']/ul/li|.//div[@class='RJPOee EIJn2']/ul/li/ul/li|.//div[@class='RJPOee EIJn2']/div");//15-04-2025//05-04-2025
                     if (ls == null)
                         ls = nd.SelectNodes(".//ol[@jscontroller='M2ABbc']/li|.//ul[@jscontroller='M2ABbc']/li|.//ul[@jscontroller='M2ABbc']/div");//04-04-2025
                     if (ls == null)

fefada6347072f2fbcf5993850e74056425d1fec
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-04-16 20:28:22 +0100

    updated

--- TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/HTMLParserNewTaskAll.cs
+++ TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/HTMLParserNewTaskAll.cs
@@ -209,7 +209,7 @@ namespace TrendingReceiving
                         bool isOldPage = false;
                         if (ex.Message == "Old page found.")
                             isOldPage = true;
-                        await ProcessError(kw, seid, jobid, isOldPage); //06-08-2024
+                        await ProcessError(kw, seid, jobid, ex.Message.ToString(), isOldPage); //06-08-2024
                     }
                     finally { }
                 }
@@ -217,13 +217,13 @@ namespace TrendingReceiving
             }
         }
 
-        private async Task ProcessError(string kw, string seid, string jobid, bool isOldPage) //06-08-2024
+        private async Task ProcessError(string kw, string seid, string jobid,string message, bool isOldPage) //06-08-2024
         {
-            string qry = "insert into dashboard_dataerrors (date, name, seid, jobid) values(Convert(varchar(10),'" + myDate + "',103), N'" +
-                  kw.Replace("'", "''") + "', " + seid + ", '" + jobid + "' )";
+            string qry = "insert into dashboard_dataerrors (date, name, seid, jobid,message) values(Convert(varchar(10),'" + myDate + "',103), N'" +
+                  kw.Replace("'", "''") + "', " + seid + ", '" + jobid + "',N'" + message.Replace("'", "''") + "' )";//09-11-2024
 
-            string qryOld = "insert into dashboard_oldgooglepage (date, keyword, seid, jobid) values('" + DateTime.Now + "', N'" +
-                 kw.Replace("'", "''") + "', " + seid + ", '" + jobid + "' )";
+            string qryOld = "insert into dashboard_oldgooglepage (date, keyword, seid, jobid,message) values('" + DateTime.Now + "', N'" +
+                 kw.Replace("'", "''") + "', " + seid + ", '" + jobid + "',N'" + message.Replace("'", "''") + "'  )";//09-11-2024
 
             using (SqlConnection con = new SqlConnection(StrConn()))
             {

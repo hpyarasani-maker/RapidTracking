c63c67b198ac5f3ef5d2c0f518973e44b93d852e
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-05-05 16:28:31 +0100

    updated selector for wrong flight pack block for desktop searches at line number 2299 and 3000 //05-05-2025

--- RapidTrackingSingleThread/Desktop.cs
+++ RapidTrackingSingleThread/Desktop.cs
@@ -2296,7 +2296,8 @@ namespace RapidTrackingSingleThread
                 return "TopSights";*///23-03-2022//19-01-2023
 
             nd = node.SelectSingleNode(".//div[contains(@class,'WlTAzf')]|.//div[contains(@class, 'vdQmEd')]|.//div[@class='cj1ht QkBAO oYQBg']|.//div[@class='p21Z4']");//18-02-2025//29-07-2024//29-06-2023//23-03-2022
-            if (nd != null && nd.SelectNodes(".//div[@class='MxQnIc']") == null && node.SelectSingleNode(".//div[@class='XNfAUb']|.//h1[contains(@class, 'bNg8Rb')]|.//div[@class='ad5fcd']|.//div[contains(@class,'vmod')]") == null)//22-04-2025//21-02-2025//27-12-2024//19-08-2024//30-06-2023
+            if (nd != null && nd.SelectNodes(".//div[@class='MxQnIc']") == null && node.SelectSingleNode(".//div[@class='XNfAUb']|.//h1[contains(@class, 'bNg8Rb')]" +
+                "|.//div[@class='ad5fcd']|.//div[contains(@class,'vmod')]|.//div[@class='HJSKzf']") == null)//05-05-2025//22-04-2025//21-02-2025//27-12-2024//19-08-2024//30-06-2023
                 return "Flights";//23-03-2022
 
             //nd = node.SelectSingleNode(".//div[@class='kp-blk cUnQKe']|.//div[@class='kp-blk cUnQKe Wnoohf OJXvsb']|.//div[@jsname='N760b']");//08-07-2021//04-12-2020 //11-02-2020

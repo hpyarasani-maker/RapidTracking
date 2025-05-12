6caa6b79c7a515cff22ac7b9852e08075c38fd84
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-05-12 18:43:29 +0100

    updated

--- TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/iOS.cs
+++ TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/iOS.cs
@@ -16,7 +16,6 @@ namespace TrendingReceiving
     {
         int orgLinks;
         string html; string seid = string.Empty;//23-06-2023
-        public event KeywordDone OnKeywordDone;//30-10-2024
         public async Task<(string, int)> ProcessDocument(string seid, string keyword, string jobid, string htmlsource)//12-05-2025//30-10-2024
         {
             this.seid = seid;//23-06-2023
@@ -216,7 +215,7 @@ namespace TrendingReceiving
                             //if (nc == null || node.SelectNodes(".//div[@id='kp-wp-tab-overview']/div") != null)//07-10-2021 answer card and PAA blocks
                             if (nc == null)//02-09-2022
                                 nc = node.SelectNodes(".//div[@id='kp-wp-tab-overview']/div"); //15-12-2020
-                             //end of swapped
+                                                                                               //end of swapped
                             if (nc == null) //|.//div[@class='a3spGf WvKfwe']/div //23-05-2020
                                 nc = node.SelectNodes(".//div[@class='Kot7x eXEBMb Znsfnf']/div[@class='GhpATe pttBJc']"); //15-04-2020
                             if (nc == null)//|.//div[@class='kp-blk c2xzTb OJXvsb']//23-05-2020
@@ -245,13 +244,7 @@ namespace TrendingReceiving
                             break;
                         }
                     }
-                    catch (WebException ex)
-                    {
-                        if (ex.Message.Contains("answercard"))//05-11-2024
-                        {
-                            OnKeywordDone.Invoke("Error:  seid: " + seid + ",  keyword: " + keyword + ",  jobid: " + jobid + "\r\n\t" + ex.Message + "^0^0.0^0.0^0");
-                        }//05-11-2024 
-                    }
+                    catch { }
                 }
             }
 

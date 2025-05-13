3f3fb99c09b010e331173577ca110f362c71aeb8
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-05-13 19:12:11 +0100

    Revert "update"
    This reverts commit 98ec669e47c805ad5b1e4d2b479a7b7bb4bb431a.

--- AIOMultiThreadRequests/Form1.cs
+++ AIOMultiThreadRequests/Form1.cs
@@ -1174,45 +1174,39 @@ namespace AIOMultiThreadRequests
                     else if (cbUrl[2] == "faulted" && cbUrl[3] == "no")
                     {
                         cbUrl[3] = "yes";
-                        //cnt++;
-                        break;
+                        cnt++;
                     }
                     else if (cbUrl[2] == "pending" && cbUrl[3] == "no")
                     {
-                        //try
-                        //{
-                        //    HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri.ToString().Replace("/results", ""));
-                        //    httpWebRequest.Headers["Authorization"] = "Basic " + authInfo;
-                        //    HttpWebResponse res = (HttpWebResponse)await httpWebRequest.GetResponseAsync();
+                        try
+                        {
+                            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri.ToString().Replace("/results", ""));
+                            httpWebRequest.Headers["Authorization"] = "Basic " + authInfo;
+                            HttpWebResponse res = (HttpWebResponse)await httpWebRequest.GetResponseAsync();
 
-                        //    string doneresp = "";
-                        //    using (StreamReader reader = new StreamReader(res.GetResponseStream(), Encoding.UTF8))
-                        //    {
-                        //        doneresp = reader.ReadToEnd();
-                        //    }
-                        //    res.Close();
+                            string doneresp = "";
+                            using (StreamReader reader = new StreamReader(res.GetResponseStream(), Encoding.UTF8))
+                            {
+                                doneresp = reader.ReadToEnd();
+                            }
+                            res.Close();
 
-                        //    JObject job = JObject.Parse(doneresp);
-                        //    string status = job["status"].Value<string>();
-                        //    cbUrl[2] = status;
-                        //}
-                        //catch (Exception ex)
-                        //{
-                        //    txtError.Text = "Status Request: " + ex.Message;
-                        //    txtError.Text = ex.Message.ToString();
-                        //}
-                        txtError.Text = "Pending";
-                        break;
+                            JObject job = JObject.Parse(doneresp);
+                            string status = job["status"].Value<string>();
+                            cbUrl[2] = status;
+                        }
+                        catch (Exception ex)
+                        {
+                            txtError.Text= "Status Request: " + ex.Message;
+                            txtError.Text = ex.Message.ToString();
+                        }
                     }
                     else
-                        //cnt++;
-                        txtError.Text = "Faulted";
-                        break;
-
-                    //Task.Delay(200).Wait();
+                        cnt++;
+                    Task.Delay(200).Wait();
                 }
 
-                //if (lst.Count == cnt) break;
+                if (lst.Count == cnt) break;
 
             } while (true);
 

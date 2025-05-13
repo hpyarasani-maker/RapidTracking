9715f47c7be859999dbd123a2d94720ca38a19fc
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-05-13 18:44:45 +0100

    updated

--- AIOMultiThreadRequests/Form1.cs
+++ AIOMultiThreadRequests/Form1.cs
@@ -1212,12 +1212,10 @@ namespace AIOMultiThreadRequests
                     //Task.Delay(200).Wait();
                 }
 
-                //if (lst.Count == cnt) break;
-
+                //if (lst.Count == cnt) 
+                break;
             } while (true);
-
             return await Task.FromResult<ArrayList>(alResult);
-
         }
         private void txtError_TextChanged(object sender, EventArgs e)
         {

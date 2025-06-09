fd04519dc9d9238c95c43409183b327188dc5ed7
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-06-09 19:48:09 +0100

    updated selector for missing twitter item urls for mobile searches at line number 2447 and 2448 //09-06-2025

--- RapidTrackingLibrary/iOS.cs
+++ RapidTrackingLibrary/iOS.cs
@@ -2499,6 +2499,8 @@ namespace RapidTrackingLibrary
                     nds = node.SelectNodes(".//div[@class='agqCtf tw-res']/g-card-section/a|.//a[@class='WlydOe eR6uYd']");//11-09-2024//24-04-2024
                 if (nds == null)
                     nds = node.SelectNodes(".//div[@class='agqCtf tw-res']/g-image-section/a");//03-04-2024
+                if (nds == null)//09-06-2025
+                    nds = node.SelectNodes(".//div[@class='hRcGbc']/a");//09-06-2025
                 if (nds != null)
                     foreach (HtmlNode nd in nds)
                     {

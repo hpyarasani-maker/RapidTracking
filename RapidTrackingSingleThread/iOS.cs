75f453b82a654fd85939c41a67574fad2d963969
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-07-08 18:28:30 +0100

    updated selector for missing content in AIO block for the mobile searches at line number 3969 //07-08-2025

--- RapidTrackingSingleThread/iOS.cs
+++ RapidTrackingSingleThread/iOS.cs
@@ -3956,6 +3956,8 @@ namespace RapidTrackingSingleThread
                         ls = nd.SelectNodes(".//ol[@jscontroller='M2ABbc']/div");//23-04-2025
                     if (ls != null && string.IsNullOrEmpty(ls[0].InnerText))//01-05-2025
                         ls = nd.SelectNodes(".//ol[@jscontroller='M2ABbc']/li|.//ul[@jscontroller='M2ABbc']/li");//01-05-2025
+                    if (ls == null)//07-07-2025
+                        ls = nd.SelectNodes(".//ul[@jscontroller='M2ABbc']/li");//07-08-2025
                     if (ls != null)
                     {
                         foreach (HtmlNode nd1 in ls)

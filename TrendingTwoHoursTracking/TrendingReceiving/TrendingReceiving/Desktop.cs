7aef3f8db88c37ea4c6e00add4db0bf3a65054a3
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-07-30 18:07:41 +0100

    Updated Desktop line number 50 //30-07-2025

--- TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/Desktop.cs
+++ TrendingTwoHoursTracking/TrendingReceiving/TrendingReceiving/Desktop.cs
@@ -50,7 +50,7 @@ namespace TrendingReceiving
                 nodeCol = rso.SelectNodes(".//div[contains(@class,'TzHB6b cLjAic')]");//02-11-2023
             //if (nodeCol == null || nodeCol.Count <= 3)//02-11-2023
             //    nodeCol = rso.SelectNodes(".//div[@class='WvKfwe a3spGf']/div|.//div[@class='aviV4d']");//01-08-2024//01-02-2024//02-11-2023
-            if (nodeCol == null || (nodeCol.Count >= 1 && nodeCol.Count <= 10))//15-07-2025//09-02-2024
+            if (nodeCol == null || (nodeCol.Count >= 1 && nodeCol.Count <= 12))//29-07-2025//15-07-2025//09-02-2024
             {//08-02-2024
                 if (rso.SelectNodes("//div[contains(@id,'kp-wp-tab-')]") != null)
                     nodeCol = rso.SelectNodes(".//div[contains(@class,'TzHB6b cLjAic')]|.//div[contains(@class,'TzHB6b j8lBAb p7kDMc')]|.//div[contains(@class,'g Ww4FFb')]|.//div[@class='g PmEWq']" +

5ce62c41cabfcb4770f6a275ce86f3b45373c52e
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-09-29 19:35:41 +0100

    updated

--- RapidTrackingProxiesAndServerIPs/RapidTrackingMultithread/RapidTrackingMultithread/OxylabsProxies.cs
+++ RapidTrackingProxiesAndServerIPs/RapidTrackingMultithread/RapidTrackingMultithread/OxylabsProxies.cs
@@ -289,7 +289,7 @@ namespace RapidTrackingMultithread
                 {
                     seresults = GetTop100Desktop(keyword, seid, out sip, value.domain, value.locale, value.uule);
                 }
-                else if (value.device == "mobile_android")
+                else if (value.device == "mobile")
                 {
                     seresults = GetTop100Mobile(keyword, seid, out sip, value.domain, value.locale, value.uule);
                 }

75f2d22c02d4ca16bed6053615e775154bb5aad4
hemachanderpyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-10-28 18:49:02 +0000

    Updated IOS //28-10-2025

--- RapidTrackingResSingleThread/iOS.cs
+++ RapidTrackingResSingleThread/iOS.cs
@@ -914,6 +914,11 @@ namespace RapidTrackingResSingleThread
                             if (nd.SelectSingleNode(".//div[@class='" + nd.Attributes["class"].Value + "']") != null)
                                 continue;
                         }//12-11-2021
+                        if (nd.SelectSingleNode(".//div[@class='rWBqlb']") != null)//28-10-2025
+                        {
+                            s.Append(GetFlights(nd));
+                            continue;
+                        }//28-10-2025
                         if (nd.SelectSingleNode(".//div[@jscontroller='i5z2Rc']") != null
                             || nd.SelectSingleNode(".//div[@class='Lgnr0e J88qA vgnU9e BmP5tf']") != null //13-12-2019
                              || nd.SelectSingleNode(".//div[@class='MUxGbd v0nnCb lyLwlc']") != null   //16-12-
@@ -1638,6 +1643,8 @@ namespace RapidTrackingResSingleThread
                 HtmlNodeCollection nds = node.SelectNodes(".//a[@class='dM1Yyd']|.//div[@class='HiHjCd']/a");
                 if (nds == null)
                     nds = node.SelectNodes(".//div[@class='EDblX JpOecb']/a");
+                if (nds == null)//28-10-2025 Missing ClassicLinkSiteLinks
+                    nds = node.SelectNodes(".//li[@class='KTAFWb']/a");//28-10-2025
                 if (nds != null)
                 {
                     s.Append("<block type=\"classicLinkSiteLinks\" url=\"" + SetUrl(n.Attributes["href"].Value) + "\" title=\"" + SetTitle(t.InnerText) + "\" >");

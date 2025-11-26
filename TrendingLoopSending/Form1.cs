7912641dca88755d3db951906b0c64ff09cd20f2
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2025-11-26 16:36:06 +0000

    updated

--- TrendingLoopSending/Form1.cs
+++ TrendingLoopSending/Form1.cs
@@ -101,13 +101,13 @@ namespace TrendingLoopSending
             {
                 worklist.Items.Clear();
                 
-                worklist.Items.Add("106:iphone,laptop,credit card,trading acount");
+                //worklist.Items.Add("106:iphone,laptop,credit card,trading acount");
                 worklist.Refresh();
                 date_picker.Format = DateTimePickerFormat.Custom;
                 date_picker.CustomFormat = "yyyy-MM-dd";
             });
 
-           return;
+           //return;
 
             Cursor.Current = Cursors.WaitCursor;
             string myDate = date_picker.Text;
